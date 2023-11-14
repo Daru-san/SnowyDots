80b6c8fe1eed68111101c8568bb697657e716409
0LaMica <mica.tui@proton.me>
2023-11-14 15:24:07 +0200

    Made home-manager system managed

--- nixos/Modules/users.nix
+++ nixos/Modules/users.nix
@@ -1,4 +1,17 @@
-{pkgs, ...}:{
+{ inputs, outputs, pkgs ... }: {
+  imports = [
+    # Import home-manager's NixOS module
+    inputs.home-manager.nixosModules.home-manager
+  ];
+  #This is to allow home manager to be managed by the system and not on it's own
+  home-manager = {
+    extraSpecialArgs = { inherit inputs outputs; };
+    users = {
+      # Home configuration
+      daru = import ../../home-manager/home.nix;
+    };
+  };
+
   #User configurations
   users = {
     users = {
