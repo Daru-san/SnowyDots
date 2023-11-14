1419fd0120aaf69f1919552316da478ee5e51d1b
0LaMica <mica.tui@proton.me>
2023-11-14 15:24:21 +0200

    Update system shell aliases

--- nixos/Modules/shell.nix
+++ nixos/Modules/shell.nix
@@ -10,19 +10,27 @@
     shellAliases = let     
     ranger = "${pkgs.ranger}/bin/ranger";
     w3m = "${pkgs.w3m}/bin/w3m";
+    rsync = "${pkgs.rsync}/bin/rsync";
   in {
+    #Useful shell aliases
     ll = "ls -l";
     ex = "exit";   
-    logout = "loginctl terminate-user $USER";
-    rsync-copy = "rsync -avzhe --progress";
-    rsync-move = "rsync --remove-source-files -zvh --progress";
-    nix-rebuild = "sudo nixos-rebuilt";
     rs = "reset";
     cl = "clear";
-    r = "${ranger}";
-    nix-upgrade = "sudo nixos-rebuild switch --upgrade-all";
     rebootF = "sudo systemctl reboot -f";
+    logout = "loginctl terminate-user $USER";
+
+    #Userful rsync aliases
+    rsync-copy = "${rsync} -avzhe --progress";
+    rsync-move = "${rsync} --remove-source-files -zvh --progress";
+
+    #Nix aliases
+    nix-upgrade = "nixos-rebuild --flake .#AspireNixRebuilt --upgrade-all";
+    nix-rebuild = "nixos-rebuild --flake .#AspireNixRebuilt";
     nix-manual = "${w3m}/bin/w3m file:///nix/store/cvnacwjawy7fi41q5ffklvxcjnhnib3k-nixos-manual-html/share/doc/nixos/index.html";
+    
+    #Program specific aliases
+    r = "${ranger}";
   };
 
   #Enable neovim and set as main editor
@@ -38,9 +46,9 @@
       #Default neovim plugins
       packages.myVimPackage = with pkgs.vimPlugins; {
         start = [ 
-        vim-nix
-        vim-clap
-        barbecue-nvim
+          vim-nix
+          vim-clap
+          barbecue-nvim
         ];
       };
     };
