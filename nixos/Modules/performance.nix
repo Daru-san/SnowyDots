0ef9d09e1525cbebd4caddbc984ac37f9546324f
0LaMica <mica.tui@proton.me>
2023-11-14 15:24:35 +0200

    Add configurations for performance

--- nixos/Modules/performance.nix
+++ nixos/Modules/performance.nix
@@ -1,4 +1,21 @@
 {...}:{
-
+  #Enable throttled service
   services.throttled.enable = true;
+
+  #Enable tlp for power management
+  services.tlp.enable = true;
+
+  #Disable power-profiles-daemon for GNOME (if enabled)
+  services.power-profiles-daemon.enable = false;
+
+  #Set performance to max
+  powerManagement = {
+  	enable = true;
+	  cpuFreqGovernor = "performance";
+
+    #Enable powertop for system monitoring
+    powetop = {
+      enable = true;
+    };
+  };
 }
