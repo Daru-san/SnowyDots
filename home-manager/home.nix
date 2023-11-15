c09fb6060adda01412e52959bcff4da29a63d73f
0LaMica <mica.tui@proton.me>
2023-11-15 13:58:11 +0200

    Fix neovim-nightly-overlay issue

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -29,7 +29,7 @@
       outputs.overlays.unstable-packages
 
       # You can also add overlays exported from other flakes:
-      neovim-nightly-overlay.overlays.default
+      inputs.neovim-nightly-overlay.overlays.default
 
       # Or define it inline, for example:
       # (final: prev: {
