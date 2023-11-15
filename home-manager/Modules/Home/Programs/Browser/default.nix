cc1b75a0e888c7a4838f994e2fb3dea1ff8a9ff4
Daru-san <mica.tui@proton.me>
2023-11-15 23:04:56 +0000

    refactor: remove another 'pkgs.unstable'

--- home-manager/Modules/Home/Programs/Browser/default.nix
+++ home-manager/Modules/Home/Programs/Browser/default.nix
@@ -3,7 +3,7 @@
     ./Firefox/default.nix
     ./Extra/default.nix
   ];
-  home.packages = with pkgs.unstable; [
+  home.packages = with pkgs; [
     lynx #Terminal browser
     w3m #Also a terminal browser and terminal image viewer
   ];
