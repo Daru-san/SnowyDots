412f48685d1cf0f3b43b4dd72c4488c27cef93a5
Daru-san <mica.tui@proton.me>
2023-11-15 23:06:29 +0000

    refactor: remove one more 'pkgs.unstable'

--- home-manager/Modules/Home/Desktop/Wallpapers/default.nix
+++ home-manager/Modules/Home/Desktop/Wallpapers/default.nix
@@ -11,7 +11,7 @@ in {
   imports = [
     ./wpaperd/default.nix
   ];
-  home.packages = with pkgs.unstable; [
+  home.packages = with pkgs; [
     swww #Wallpaper support for wayland
     #swaybg #uncomment to enable swaybg
 
