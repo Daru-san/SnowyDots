9238489a88969045f3f7da45ff6cc798eb7ac29a
Daru-san <mica.tui@proton.me>
2023-11-15 22:50:15 +0000

    bugfix: add stable pkgs to configs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -38,7 +38,7 @@
       # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
-      outputs.overlays.unstable-packages
+      outputs.overlays.stable-packages
 
       # Or define it inline, for example:
       # (final: prev: {
