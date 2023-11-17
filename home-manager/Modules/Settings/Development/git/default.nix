39d004cf05a0d2e22acf4a4d5fe04e53103790a3
Daru-san <mica.tui@proton.me>
2023-11-17 23:13:57 +0200

    hotfix: fix git aliases

--- home-manager/Modules/Settings/Development/git/default.nix
+++ home-manager/Modules/Settings/Development/git/default.nix
@@ -41,8 +41,8 @@
         ranger
      '')
       (pkgs.writeShellScriptBin "git-alias"''
-        print "These are the available git aliases:"
-        printf "ci = commit\nco = checkout\ncl = clone\nst = status\nad = add\nps = push\npl = pull\nre = remote\nbr = branch\nfe = fetch\ncp = cherry-pick\nrs = reset\ndf = diff\nin = init\n"
+        printf "These are the available git aliases:"
+        printf "ci = commit\nco = checkout\ncl = clone\nst = status\nad = add\nps = push\npl = pull\nre = remote\nbr = branch\nfe = fetch\ncp = cherry-pick\nrs = reset\ndf = diff\nini = init\n"
       '')
   ];
 }
