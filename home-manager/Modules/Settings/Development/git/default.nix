94d025b6267d9a9b3450d2cb0f92a7335fb77d09
Daru-san <mica.tui@proton.me>
2023-11-17 17:21:57 +0200

    feat: add more git aliases;chore: clean git config

--- home-manager/Modules/Settings/Development/git/default.nix
+++ home-manager/Modules/Settings/Development/git/default.nix
@@ -12,29 +12,37 @@
     aliases = {
       ci = "commit";
       co = "checkout";
-      s = "status";
-      a = "add";
-      p = "push";
-      pu = "pull";
-      r = "remote";
-      b = "branch";
+      cl = "clone";
+      st = "status";
+      ad = "add";
+      ps = "push";
+      pl = "pull";
+      re = "remote";
+      br = "branch";
       fe = "fetch";
       cp = "cherry-pick";
+      rs = "reset";
+      df = "diff";
+      in = "init";
     };
   };
   home.packages = with pkgs; [
       #Git scripts to make editing from any directory easier
       (pkgs.writeShellScriptBin "git-push" ''
-         cd ~/Nix-Dots 
-           git push 
+        cd ~/snowflake
+        git push 
       '')
       (pkgs.writeShellScriptBin "git-ui" ''
-         cd ~/Nix-Dots
-         gitui
+        cd ~/snowflake/
+        gitui
      '')
       (pkgs.writeShellScriptBin "git-r" ''
-         cd ~/Nix-Dots/ 
-         ranger
+        cd ~/snowflake/ 
+        ranger
      '')
+      (pkgs.writeShellScriptBin "git-alias"''
+        print "These are the available git aliases:"
+        printf "ci = commit\nco = checkout\ncl = clone\nst = status\nad = add\nps = push\npl = pull\nre = remote\nbr = branch\nfe = fetch\ncp = cherry-pick\nrs = reset\ndf = diff\nin = init\n"
+      '')
   ];
 }
