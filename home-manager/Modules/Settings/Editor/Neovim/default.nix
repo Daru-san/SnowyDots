4af874506959cde4d674131eb064858e759e9d46
Daru-san <mica.tui@proton.me>
2023-11-22 17:35:49 +0200

    feat: switch from nvchad to nix-based config

--- home-manager/Modules/Settings/Editor/Neovim/default.nix
+++ home-manager/Modules/Settings/Editor/Neovim/default.nix
@@ -1,49 +1,52 @@
 {config, pkgs, lib, ...}:{
-    programs.neovim = {
-      package = pkgs.neovim-nightly;
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-  };
-#  home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink ./config;
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
-  #Add my fork of NvChad to the config
-  home.file.".config/nvim" = {
-    recursive = true;
-    source = pkgs.fetchFromGitHub {
-      owner = "0LaMica";
-      repo = "Nix-NvChad";
-      rev = "a8413849cf9d648df0ae81a9349c8b125e628919";
-      sha256 = "u5U7caaHq9lsyYTZ4Gx+TshHIxgqnx03vWVWzKBqBhI=";
+    programs = {
+      neovim = {
+        #Enable neovim, set to nightly and make it the default editor
+        package = pkgs.neovim-nightly;
+        enable = true;
+        defaultEditor = true;
+
+        #Vi and vim aliases
+        viAlias = true;
+        vimAlias = true;  
+
+        coc = {
+          enable = true;
+        };
+        plugins = with pkgs.vimPlugins; [
+        #Coc plugins
+          coc-sh
+          coc-git
+          coc-css
+          coc-json
+          coc-toml
+          coc-html
+          coc-java
+          coc-cmake
+          coc-pairs
+          coc-python
+          coc-prettier
+          coc-highlight
+          coc-markdownlint
+          coc-snippets
+          coc-lua
+          coc-spell-checker
+          coc-ltex
+
+          #Telescope plugins
+          telescope-nvim
+          telescope-coc-nvim
+          telescope-undo-nvim
+          telescope-symbols-nvim
+          telescope-media-files-nvim
+          telescope-fzf-native-nvim
+          telescope-fzf-writer-nvim
+          telescope-cheat-nvim
+          telescope-file-browser-nvim
+          
+          #Barbecue for VSCode like winbar
+          barbecue-nvim
+        ];
+      };
     };
   };
-  home.file.".config/nvim/custom/chadrc.lua".text = ''
-  ---@type ChadrcConfig
-  local M = {}
-
-  -- Path to overriding theme and highlights files
-  local highlights = require "custom.highlights"
-
-  M.ui = {
-    theme = "nightfox",
-    theme_toggle = { "nightfox", "one_light" },
-    transparency = true,
-
-    statusline = {
-      theme = "vscode_colored",
-    },
-
-    hl_override = highlights.override,
-    hl_add = highlights.add,
-  }
-
-  M.plugins = "custom.plugins"
-
-  -- check core.mappings for table structure
-  M.mappings = require "custom.mappings"
-
-  return M
-  '';
-}
