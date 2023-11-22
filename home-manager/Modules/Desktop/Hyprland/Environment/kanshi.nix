587c8e34f95b08f43492cbd31b5466721a154afb
Daru-san <mica.tui@proton.me>
2023-11-22 12:36:33 +0200

    refactor: small changes

--- home-manager/Modules/Desktop/Hyprland/Environment/kanshi.nix
+++ home-manager/Modules/Desktop/Hyprland/Environment/kanshi.nix
@@ -19,7 +19,7 @@
       home = {
         outputs = [
         {
-          criteria = "Samsung Electric Company SyncMaster 0x45463233";
+          criteria = "Samsung Electric Company SyncMaster HVJZB00655";
           mode = "1920x1080@60";
           position = "0,0";
           scale = 1.0;
--- home-manager/Modules/Programs/Music/cava/default.nix
+++ home-manager/Modules/Programs/Music/cava/default.nix
@@ -2,21 +2,21 @@
   programs.cava = {
     enable = true;
     settings = {
-      general = {
-        framerate = 60;
-      };
-      input = {
-        method = "pipewire";
-      };
-      smoothing = {
-        noise_reduction = 88;
-      };
-      color = {
-        background = "'#000000'";
-        foreground = "'#58D196'";
-      };
-      sensitivity = 70;
-      bars = 128;
+      # general = {
+        # framerate = 60;
+      # };
+      # input = {
+        # method = "pipewire";
+      # };
+      # smoothing = {
+        # noise_reduction = 88;
+      # };
+      # color = {
+        # background = "'#000000'";
+        # foreground = "'#58D196'";
+      # };
+      # sensitivity = 70;
+      # bars = 128;
     };
   };
 }
