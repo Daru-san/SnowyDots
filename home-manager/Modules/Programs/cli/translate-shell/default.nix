59ba02165ee4b78bf5e274257c4381bde1aa0532
Daru-san <mica.tui@proton.me>
2023-11-22 17:34:17 +0200

    refactor: update translate-shell config

--- home-manager/Modules/Programs/cli/translate-shell/default.nix
+++ home-manager/Modules/Programs/cli/translate-shell/default.nix
@@ -1,11 +1,14 @@
 {pkgs, ...}:{
-  programs.translate-shell = {
-    enable = true;
-    settings = {
-      hl = "en";
-      view = true;
-      engine = "bing";
-      verbose = true;
+  programs = {
+    translate-shell = {
+      enable = true;
+      settings = {
+        hl = "en";
+        view = true;
+        show-original-dictionary = true;
+        show-alternatives = true;
+        verbose = true;
+      };
     };
   };
 }
