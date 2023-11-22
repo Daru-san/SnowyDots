519657afbc9a8203f36fab6ed55d31eabce9364f
Daru-san <mica.tui@proton.me>
2023-11-22 19:20:18 +0200

    feat/refactor: change up neovim plugins

--- home-manager/Modules/Settings/Editor/Neovim/default.nix
+++ home-manager/Modules/Settings/Editor/Neovim/default.nix
@@ -10,28 +10,24 @@
         viAlias = true;
         vimAlias = true;  
 
-        coc = {
-          enable = true;
-        };
         plugins = with pkgs.vimPlugins; [
-        #Coc plugins
-          coc-sh
-          coc-git
-          coc-css
-          coc-json
-          coc-toml
-          coc-html
-          coc-java
-          coc-cmake
-          coc-pairs
-          coc-python
-          coc-prettier
-          coc-highlight
-          coc-markdownlint
-          coc-snippets
-          coc-lua
-          coc-spell-checker
-          coc-ltex
+	  vim-prettier
+	  FTerm-nvim
+	  ChatGPT-nvim
+	  bufferline-nvim
+	  cheatsheet-nvim
+	  dashboard-nvim
+	  nvim-lspconfig
+	  nvim-colorizer-lua
+	  nvim-comment
+	  nvim-autopairs
+	  indent-blankline-nvim
+	  gitsigns-nvim
+	  lazy-nvim
+	  lazygit-nvim
+	  luasnip
+	  friendly-snippets
+	  galaxyline-nvim
 
           #Telescope plugins
           telescope-nvim
@@ -44,8 +40,35 @@
           telescope-cheat-nvim
           telescope-file-browser-nvim
           
-          #Barbecue for VSCode like winbar
-          barbecue-nvim
+	  #Treesitter
+	  nvim-treesitter-textobjects
+	  nvim-treesitter-refactor
+	  nvim-treesitter-parsers.vim
+	  nvim-treesitter-parsers.vimdoc
+	  nvim-treesitter-parsers.scss
+	  nvim-treesitter-parsers.rasi
+	  nvim-treesitter-parsers.python
+	  nvim-treesitter-parsers.passwd
+	  nvim-treesitter-parsers.nix
+	  nvim-treesitter-parsers.markdown
+	  nvim-treesitter-parsers.markdown_inline
+	  nvim-treesitter-parsers.make
+	  nvim-treesitter-parsers.lua
+	  nvim-treesitter-parsers.java
+	  nvim-treesitter-parsers.html
+	  nvim-treesitter-parsers.go
+	  nvim-treesitter-parsers.git_config
+	  nvim-treesitter-parsers.gitcommit
+	  nvim-treesitter-parsers.gitignore
+	  nvim-treesitter-parsers.git_rebase
+	  nvim-treesitter-parsers.css
+	  nvim-treesitter-parsers.json
+	  nvim-treesitter-parsers.comment
+	  nvim-treesitter-parsers.c
+	  nvim-treesitter-parsers.bash
+	  nvim-treesitter-parsers.ini
+	  cmp-treesitter 
+	  nvim-treesitter-parsers.lua
         ];
       };
     };
