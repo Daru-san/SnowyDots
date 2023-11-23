dfcf3fb0c755b57d00294223c742544951f8e293
Daru-san <mica.tui@proton.me>
2023-11-23 18:38:15 +0200

    refactor: update fuzzel configs

--- home-manager/Modules/Desktop/Hyprland/Environment/fuzzel.nix
+++ home-manager/Modules/Desktop/Hyprland/Environment/fuzzel.nix
@@ -7,19 +7,22 @@
         layer = "overlay";
         # anchor = "center";
         font = "Jetbrains Mono Nerd Font";
-        width = 56;
-        lines = 7;
+        width = 60;
+        lines = 8;
+	line-height = 24;
         icon-theme = "Papirus-Dark";
         tabs = 10;
+
       };
       border = {
         width = 3;
         radius = 4;
       };
       colors = {
-        background = "${config.colorScheme.colors.base07}ff";
-        border = "${config.colorScheme.colors.base03}ff";
-        selection = "${config.colorScheme.colors.base04}aa";
+        background = "1E2127FF";
+        border = "2F343FFF";
+        selection = "7FBBB3FF";
+	text = "FFFFFF";
       };
     };
   };
