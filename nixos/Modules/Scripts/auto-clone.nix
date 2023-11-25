aa5baf065a2eab89d260714ab7345d31cb3a4d81
Daru-san <mica.tui@proton.me>
2023-11-25 07:42:38 +0200

    hotfix: fix broken auto-clone script

--- nixos/Modules/Scripts/auto-clone.nix
+++ nixos/Modules/Scripts/auto-clone.nix
@@ -1,15 +1,14 @@
 {pkgs, ...}:{
   #Custom system activation script that runs whenever I use 'nix-rebuild'
   system.userActivationScripts = {
-    home-setup-daru.text = ''
-
+    home-setup-daru.text = '' 
       #If the '~/.config/nvim' folder doesn't exist it'll clone, otherwise it won't
       #It won't clone if the folder already exists, this would be useful incase there is another configuration that I don't want overwritten
       #Just delete the folder to get my custom configs from my repo
 
-      if [ ! -d $HOME/.config/nvim ];
+      if [ ! -d /home/daru/.config/nvim ];
       then
-        ${pkgs.git}/bin/git clone https://github.com/Daru-san/NeoLaze $HOME/.config/nvim/
+        ${pkgs.git}/bin/git clone https://github.com/Daru-san/NeoLaze /home/daru/.config/nvim/
       fi
 
 
@@ -18,12 +17,13 @@
       #
       #This is to make using a custom script for nix-rebuild easier
 
-      if [! -d $HOME/Projects/repo ];
+      if [ ! -d /home/daru/Projects/repo ];
       then
-        ${pkgs.git}/bin/git clone https://github.com/Daru-san/Snowflake-dots $HOME/Projects/repo
+        ${pkgs.git}/bin/git clone https://github.com/Daru-san/Snowflake-dots /home/daru/Projects/repo
       else
-        cd $HOME/Projects/repo
+        cd /home/daru/Projects/repo
         ${pkgs.git}/bin/git pull
+      fi
     '';
   }; 
 }
