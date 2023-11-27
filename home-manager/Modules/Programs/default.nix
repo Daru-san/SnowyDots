d5de64550004c9f14c10268ee75bdf55ad4e6f2a
Daru-san <mica.tui@proton.me>
2023-11-27 14:34:05 +0200

    feat: add wayland support for electron apps

--- home-manager/Modules/Programs/default.nix
+++ home-manager/Modules/Programs/default.nix
@@ -8,6 +8,23 @@
     ./Browser/default.nix
     ./Images/default.nix
   ];
+
+  #overlays to enable wayland support for freetube and logseq
+  nixpkgs.overlays = [ 
+    (final: prev: {
+      freetube = prev.freetube.override {
+        commandLineArgs =
+         "--enable-features=UseOzonePlatform --ozone-platform=wayland";
+      };
+    };)
+    (final: prev: {
+      logseq = prev.logseq.override {
+        commandLineArgs =
+         "--enable-features=UseOzonePlatform --ozone-platform=wayland";
+      };
+    };)
+  ]; 
+
   home.packages = with pkgs; [
     libreoffice-fresh
     logseq
