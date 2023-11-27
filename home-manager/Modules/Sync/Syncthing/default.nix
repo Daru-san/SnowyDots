3448ac357ed24f9d2cb3f59625cca4ef4c36c12f
Daru-san <mica.tui@proton.me>
2023-11-27 15:21:55 +0200

    feat: add syncthingtray service

--- home-manager/Modules/Sync/Syncthing/default.nix
+++ home-manager/Modules/Sync/Syncthing/default.nix
@@ -1,7 +1,12 @@
-{...}:{
+{pkgs, ...}:{
   services = {
     syncthing = {
       enable = true;
+      tray = {
+        enable = true;
+        command = "stc-tray";
+        package = pkgs.syncthingtray;
+      };
     };
   };
 }
