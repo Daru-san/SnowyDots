565d76b6bf9c1e28503b1cab5f90e5b8c361aef8
Daru-san <mica.tui@proton.me>
2023-11-27 14:33:56 +0200

    feat: update spotify theme & enable wayland

--- home-manager/Modules/Programs/Music/Spotify/spotify.nix
+++ home-manager/Modules/Programs/Music/Spotify/spotify.nix
@@ -3,57 +3,33 @@ let
   spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
 in
 {
-  # imports = [ inputs.spicetify-nix.homeManagerModules.default ];
   # allow spotify to be installed if you don't have unfree enabled already
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "spotify"
   ];
 
+  #Overlay to enable wayland support
+  nixpkgs.overlays = [ 
+    (final: prev: {
+      spotify = prev.spotify.override {
+        commandLineArgs =
+         "--enable-features=UseOzonePlatform --ozone-platform=wayland";
+      };
+    };)
+  ]; 
+
   # configure spicetify :)
-  programs.spicetify = let
-    #Import officialThemes since 'text isn't provided by default
-    officialThemes = pkgs.fetchgit {
-      url = "https://github.com/spicetify/spicetify-themes";
-      sha256 = "0sab8jh4867fh66zdz7msg1bwwqpm0zc6y20hw7328p7agcr7xlk";
-      rev = "2e124a90561649a8aa410e89c6893e5b5d4816b9";
-    };
-  in 
-    {
+  programs.spicetify = {
 
     #Enable spicetify
     enable = true;
-#Spicetify theme
-    theme = {
-      name = "text";
-        src = officialThemes;
-        appendName = true;
-        injectCss = true;
-        replaceColors = true;
-        overwriteAssets = true;
-        sidebarConfig = true;
-        patches = {
-          "xpui.js_find_0880" = "COLLAPSED\?64:32";
-          "xpui.js_repl_0880" = "COLLAPSED?32:32";
-          "xpui.js_find_8008" = ",(\w+=)56,";
-          "xpui.js_repl_8008" = ",$1}32,";
-        };
-      };
-    colorScheme = "Spotify";
-      # customColorScheme = {
-      #   accent = "76946A";
-      #   accent-active = "98BB6C";
-      #   accent-inactive = "1F1F28";
-      #   banner = "98BB6C";
-      #   border-active = "98BB6C";
-      #   border-inactive = "2A2A37";
-      #   header = "54546D";
-      #   highlight = "363646";
-      #   main = "1F1F28";
-      #   notification = "7E9CD8";
-      #   notification-error = "E82424";
-      #   subtext = "C8C093";
-      #   text = "DCD7BA";
-      # };
+
+    #Package 
+    spotifyPackage = spotify;
+    
+    #Spicetify theme
+    theme = "Comfy";
+    colorScheme = "Forest"; 
 
     #Spicetify extensions
     enabledExtensions = with spicePkgs.extensions; [
