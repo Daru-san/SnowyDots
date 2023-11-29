18d393587cdff586c0e1c208d262914ff01ae739
Daru-san <mica.tui@proton.me>
2023-11-29 20:31:32 +0200

    refactor: re-enable KDE

--- nixos-configs/AspireLaptop/Modules/desktop.nix
+++ nixos-configs/AspireLaptop/Modules/desktop.nix
@@ -2,7 +2,7 @@
   imports = [
     #Comment a specific desktop to disable it, this is just to prevent them from messing with each other
 
-    #./Desktop/kde.nix
+    ./Desktop/kde.nix
     #./Desktop/gnome.nix
     ./Desktop/Hyprland.nix
   ];
