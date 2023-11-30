bc5c7c5f196752ab43095659483d58b12de082d0
Daru-san <mica.tui@proton.me>
2023-11-30 22:27:58 +0200

    hotfix

--- home/daru/Modules/Desktop/Hyprland/Settings/default.nix
+++ home/daru/Modules/Desktop/Hyprland/Settings/default.nix
@@ -15,7 +15,7 @@
       extraCommands = [
         "systemctl --user start kanshi.service"
         "systemctl --user start easyeffects.service"
-        "systemctl --user start syncthing-tray.service"
+        "systemctl --user start syncthingtray.service"
       ];
     };
     settings = {
