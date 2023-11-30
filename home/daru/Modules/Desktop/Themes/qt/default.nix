052990507154ec29822513c1d920868f7474390e
Daru-san <mica.tui@proton.me>
2023-11-30 13:14:43 +0200

    hotfix: fix syntax error

--- home/daru/Modules/Desktop/Themes/qt/default.nix
+++ home/daru/Modules/Desktop/Themes/qt/default.nix
@@ -5,7 +5,7 @@
     #Allow qt applications to follow KDE theme 
     platformTheme = "gnome";
 
-    Set qt style to adwaita for a gtk feel
+    #Set qt style to adwaita for a gtk feel
     style = {
       package = pkgs.adwaita-qt;
       name = "adwaita-dark";
