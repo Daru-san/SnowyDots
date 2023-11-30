48c51c8c6c5e12c1095351686065a51cb5719baf
Daru-san <mica.tui@proton.me>
2023-11-30 13:11:34 +0200

    feat: update hardware-configuration.nix and remove encryption

--- nixos-configs/AspireLaptop/hardware-configuration.nix
+++ nixos-configs/AspireLaptop/hardware-configuration.nix
@@ -1,84 +1,63 @@
+#Config file containing filesystems
+#No encryption for the time being
 { config, lib, pkgs, modulesPath, ... }:
-let
-  impermanence = builtins.fetchTarball "https://github.com/nix-community/impermanence/archive/master.tar.gz";
-in
+
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
-	    "${impermanence}/nixos.nix"
     ];
-  #Root on tmpfs 
+
   fileSystems."/" =
     { device = "none";
       fsType = "tmpfs";
     };
 
-
-  #Boot
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/5BC9-5E1D";
-      fsType = "vfat";
-    };
-
-  #Persisted directories mounted as btrfs subvolumes
   fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/6e78ed62-ceaa-460d-9966-5a5114ff11fa";
+    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
       fsType = "btrfs";
       options = [ "subvol=nix" ];
     };
-  #Encryption
-  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/e7ddebc8-055e-4477-b02f-aa509be37fa2";
+
+  fileSystems."/etc" =
+    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
+      fsType = "btrfs";
+      options = [ "subvol=etc" ];
+    };
+
+  fileSystems."/var/log" =
+    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
+      fsType = "btrfs";
+      options = [ "subvol=log" ];
+    };
+
+  fileSystems."/root" =
+    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
+      fsType = "btrfs";
+      options = [ "subvol=root" ];
+    };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/6e78ed62-ceaa-460d-9966-5a5114ff11fa";
+    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
       fsType = "btrfs";
       options = [ "subvol=home" ];
     };
 
-  fileSystems."/persist" =
-    { device = "/dev/disk/by-uuid/6e78ed62-ceaa-460d-9966-5a5114ff11fa";
-      fsType = "btrfs";
-      options = [ "subvol=persist" ];
-      neededForBoot = true;
-    };
-
-  fileSystems."/etc/nixos" =
-    { device = "/dev/disk/by-uuid/6e78ed62-ceaa-460d-9966-5a5114ff11fa";
-      fsType = "btrfs";
-      options = [ "subvol=nixos-config" ];
-    };
   fileSystems."/tmp" =
-    { device = "/dev/disk/by-uuid/6e78ed62-ceaa-460d-9966-5a5114ff11fa";
+    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
       fsType = "btrfs";
       options = [ "subvol=tmp" ];
     };
 
-  #Swap file
-  swapDevices = [
-  {
-   device = "/var/lib/swapfile";
-   size = 2048;
-   }
-  ];
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/D04A-9AA9";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/78d797f0-5db6-4f7c-8030-685e2e381935"; }
+    ];
 
 
-  #Directories and files persisted by Nix
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  environment.persistence."/persist" = {
-    directories = [
-      "/etc/ssh"
-      "/etc/NetworkManager"	
-      "/var/log"
-      "/var/tmp"
-      
-      "/var/lib"
-      "/var/db/sudo/lectured"
-    ];
-   files = [
-     "/etc/machine-id"
-     "/etc/nix/id_rsa"
-     # "/etc/passwd"
-     # "/etc/shadow"
-    ];
- };
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
