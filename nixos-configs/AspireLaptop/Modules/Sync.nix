6e5d38cfa25761c01cd0119a2a5cc22cd5f2aec3
Daru-san <mica.tui@proton.me>
2023-11-30 17:55:35 +0200

    refactor: update syncthing config

--- nixos-configs/AspireLaptop/Modules/Sync.nix
+++ nixos-configs/AspireLaptop/Modules/Sync.nix
@@ -2,6 +2,22 @@
   services = {
     syncthing = {
       enable = true;
+      dataDir = "/home/daru";
+      user = "daru";
+      configDir = "/home/daru/.sync";
+      settings = {
+        gui = {
+          theme = "black";
+        };
+        devices = {
+          "Redmi 9C" = {
+            addresses = [
+              "tcp://fd11:2bad:3f46:0:c0e0:6ce4:3f2:76e4:22000"
+            ];
+            id = "XTPRGH6-GZ6N4LL-FFOIXMP-QKMDKKR-2CPWNZT-GDGM3MJ-OAHTTFA-I2NFKAK";
+          };
+        };
+      };
     };
   };
 }
