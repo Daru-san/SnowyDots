9e50e9fe0f52827435426824b5d69804716252ae
Daru-san <mica.tui@proton.me>
2023-12-14 11:32:30 +0200

    refactor: update swaylock colors

--- home/daru/Modules/Desktop/Hyprland/Environment/swaylock.nix
+++ home/daru/Modules/Desktop/Hyprland/Environment/swaylock.nix
@@ -7,28 +7,28 @@
       font = "Jetbrains Mono Nerd Font";
       clock = true;
       timestr = "%R";
-      datestr= "%a, %e of %B";
+      datestr= "%a, %d of %B";
       screenshots =  true;
-      fade-in = 0.2;
+      fade-in = 0.7;
 
       effect-blur = "20x2";
-      effect-scale = 0.3;
+      effect-scale = 0.5;
       indicator = true;
-      indicator-radius = 240;
-      indicator-thickness = 20;
+      indicator-radius = 230;
+      indicator-thickness = 14;
       indicator-caps-lock =  true;
 
-      key-hl-color = "880033";
+      key-hl-color = "336633";
 
       separator-color =  "00000000";
 
-      inside-color = "00000099";
+      inside-color = "111B1B52";
       inside-clear-color = "ffd20400";
       inside-caps-lock-color = "009ddc00";
       inside-ver-color = "d9d8d800";
       inside-wrong-color = "ee2e2400";
 
-      ring-color = "231f20D9";
+      ring-color = "A6E3A1D9";
       ring-clear-color = "231f20D9";
       ring-caps-lock-color = "231f20D9";
       ring-ver-color = "231f20D9";
@@ -41,14 +41,14 @@
       line-wrong-color = "ee2e24FF";
 
       text-clear-color = "ffd20400";
-      text-ver-color = "d9d8d800";
+      text-ver-color = "d9f8d800";
       text-wrong-color = "ee2e2400";
 
-      bs-hl-color = "ee2e24FF";
+      bs-hl-color = "A6E3A1FF";
       caps-lock-key-hl-color  = "ffd204FF";
       caps-lock-bs-hl-color = "ee2e24FF";
       disable-caps-lock-text = true;
-      text-caps-lock-color = "009ddc";
+      text-caps-lock-color = "00fddc";
     };
   };
 }
