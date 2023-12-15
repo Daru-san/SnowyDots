b121231c70052a911bac0fa8b50a3c346cceaeec
Daru-san <mica.tui@proton.me>
2023-12-15 20:34:30 +0200

    refactor: update directories

--- home/daru/Modules/Settings/Directories/default.nix
+++ home/daru/Modules/Settings/Directories/default.nix
@@ -1,27 +1,26 @@
-#Settings folders that will be present everytime the config is built
+# Settings folders that will be present everytime the config is built
 {config, ...}:{
   xdg.userDirs = {
     enable = true;
 
-    #Make it auto create generate the directories if they don't exist
+    # Make it auto create generate the directories if they don't exist
     createDirectories = true;
 
-    #Default directories
+    # Default directories
     desktop = "${config.home.homeDirectory}/Desktop";
     music = "${config.home.homeDirectory}/Music";
     pictures = "${config.home.homeDirectory}/Pictures";
     videos = "${config.home.homeDirectory}/Videos";
     download = "${config.home.homeDirectory}/Downloads";
     templates = "${config.home.homeDirectory}/Templates";
-    #Special directories
+    # Special directories
     extraConfig = {
       Emulation = "${config.home.homeDirectory}/Emulation";
       Anime = "${config.home.homeDirectory}/Anime";
-      screenshots = "${config.home.homeDirectory}/screenshots";
       Projects = "${config.home.homeDirectory}/Projects";
       mango = "${config.home.homeDirectory}/.mango";
       Scripts = "${config.home.homeDirectory}/Scripts";
       Wallpapers = "${config.home.homeDirectory}/Wallpapers";
-      };
+    };
   };
 }
