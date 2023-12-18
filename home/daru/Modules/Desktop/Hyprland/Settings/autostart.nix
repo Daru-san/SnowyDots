b0cd718df239add02c7cb7f9dbddd5bfbd4e8a4e
Daru-san <mica.tui@proton.me>
2023-12-18 20:52:21 +0200

    refactor

--- home/daru/Modules/Desktop/Hyprland/Settings/autostart.nix
+++ home/daru/Modules/Desktop/Hyprland/Settings/autostart.nix
@@ -6,7 +6,6 @@
     "[workspace name:1] ${config.programs.kitty.package}/bin/kitty -T Terminal"
     "[workspace name:2 silent] ${config.programs.kitty.package}/bin/kitty -T Git --session git.conf"
     "[workspace name:F12 silent;fullscreen;noanim] ${config.programs.kitty.package}/bin/kitty -T Clock --hold tty-clock -bscBrn"
-    "${pkgs.networkmanagerapplet}/bin/nm-applet"
     "${config.services.copyq.package}/bin/copyq"
     "${config.programs.waybar.package}/bin/waybar"
     ];
