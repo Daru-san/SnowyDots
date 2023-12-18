6a62850f17601bbea0d3a1dae59880463dfa3d93
Daru-san <mica.tui@proton.me>
2023-12-18 20:52:40 +0200

    feat: completely redo hyprland binds

--- home/daru/Modules/Desktop/Hyprland/Settings/binds.nix
+++ home/daru/Modules/Desktop/Hyprland/Settings/binds.nix
@@ -5,39 +5,31 @@
   ] ++ [
     ./extra-binds.nix
   ];
-  wayland.windowManager.hyprland.settings = {
+  wayland.windowManager.hyprland.settings = let
+    h = "${pkgs.hdrop}/bin/hdrop";
+    e = "exec";
+  in {
     bind = let
-      swayosd = "${config.services.swayosd.package}/bin/swayosd";
-      playerctl = "${config.services.playerctld.package}/bin/playerctl";
-      playerctld = "${config.services.playerctld.package}/bin/playerctld";
       terminal = "${config.programs.kitty.package}/bin/kitty";
       browser = "${config.programs.firefox.package}/bin/firefox-nightly";
       file-manager = "${pkgs.libsForQt5.dolphin}/bin/dolphin";
-      hyprshot = "${pkgs.hyprshot}/bin/hyprshot";
       editor = "${config.programs.neovim.package}/bin/nvim";
       ranger = "${pkgs.ranger}/bin/ranger";
       image-editor = "${pkgs.krita}/bin/krita";
-      hdrop = "${pkgs.hdrop}/bin/hdrop";
       swaylock = "${config.programs.swaylock.package}/bin/swaylock";
       copyq = "${config.services.copyq.package}/bin/copyq";
       wlogout = "${config.programs.wlogout.package}/bin/wlogout";
-      hyprpicker = "${pkgs.hyprpicker}/bin/hyprpicker";
-      wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy";
-      wl-paste = "${pkgs.wl-clipboard}/bin/wl-paste";
-      notify-send = "${pkgs.libnotify}/bin/notify-send";
-      easyeffects = "${config.services.easyeffects.package}/bin/easyeffects";
-      anyrun = "${config.programs.anyrun.package}/bin/anyrun";
     in [
-      #Basic binds
-      "SUPER,space,exec, ${anyrun}"
-      "SUPER, Return, exec, ${terminal}"
-      "SUPER, e, exec, ${hdrop} '${file-manager}'"
-      "SUPERALT, b, exec, ${hdrop} '${browser}'"
-      "SUPER, x, exec, ${wlogout}"
-      "SUPER, r, exec, ${terminal} --hold ${ranger}"
-      "SUPER, z, exec, ${terminal} --hold ${editor}"
-      "SUPER, i, exec, ${hdrop} '${image-editor}'"
-      "SUPER, a, exec, ${hdrop} '${easyeffects}'"
+      # Launching programs
+      "SUPER, e, ${e}, ${h} '${file-manager}'"
+      "SUPERALT, b, ${e}, ${h} '${browser}'"
+      "SUPER, x, ${e}, ${h} '${wlogout}'"
+      "SUPER, i, ${e}, ${h} '${image-editor}'"
+
+      # Terminal stuff
+      "SUPER, Return, ${e}, ${terminal}"
+      "SUPER, r, ${e}, ${terminal} --hold ${ranger}"
+      "SUPER, z, ${e}, ${terminal} --hold ${editor}"
 
       #Window bings
       "alt,q,killactive"
@@ -48,63 +40,96 @@
 
 
       #Lock screen
-      "SUPER, l ,exec , ${swaylock} -Ff"
+      "SUPER, l ,${e} , ${swaylock} -Ff"
 
       #Clipboard menu
-      "SUPERSHIFT, v, exec, ${copyq} menu"
-      
-      #Suspend
-      "SUPERALT, F12, exec, systemctl suspend"
-
-      #Color picker
-      "SUPERSHIFT,c,exec,${notify-send} 'Color copied to clipboard' `${hyprpicker} | ${wl-copy}` `${wl-paste}`"
+      "SUPERSHIFT, v, ${e}, ${copyq} menu"
 
       #'Task manager'
-      "SUPERSHIFT,P,exec,${terminal} -T SystemMonitor --session SystemMonitor.conf"
+      "SUPERSHIFT,P,${e},${terminal} -T SystemMonitor --session SystemMonitor.conf"
+    ];
 
-      ##Controls##
-      ############
+    bindle = let
+      s = "${config.services.swayosd.package}/bin/swayosd";
+      volup = "--output-volume=raise";
+      voldown = "--output-volume=lower";
+      volmute = "--output-volume=mute-toggle";
+      brightup = "--brightness=raise";
+      brightdown = "--brightness=lower";
+    in [
+     ### Controls using swayosd ###
 
-      #Brightness controll#
+      #// Might switch to ags //#
+
+      ## Brightness control##
 
       # Brightness control using swayosd
-      ",XF86MonBrightnessUp, exec,${swayosd} --brightness=raise 5"
-      ",XF86MonBrightnessDown, exec,${swayosd} --brightness=lower 5"
+      ",XF86MonBrightnessUp, ${e},${s} ${brightup} 5"
+      ",XF86MonBrightnessDown, ${e},${s} ${brightdown} 5"
 
       ##Volume Control##
+
       # Volume using swayosd
-      ",XF86AudioRaiseVolume, exec, ${swayosd} --output-volume=raise 5"
-      ",XF86AudioLowerVolume, exec, ${swayosd} --output-volume=lower 5"
-      ",XF86AudioMute, exec, ${swayosd} --output-volume=mute-toggle"
+      ",XF86AudioRaiseVolume, ${e}, ${s} ${volup} 5"
+      ",XF86AudioLowerVolume, ${e}, ${s} ${voldown} 5"
+      ",XF86AudioMute, ${e}, ${s} ${volmute}"
 
       #Same but for keyboards without media keys
-      "ALT,F8, exec, ${swayosd} --output-volume=raise 5"
-      "ALT,F6, exec, ${swayosd} --output-volume=lower 5"
-      "ALT,F7, exec, ${swayosd} --output-volume=mute-toggle"
+      "ALT,F8, ${e}, ${s} ${volup} 5"
+      "ALT,F6, ${e}, ${s} ${volup} 5"
+      "ALT,F7, ${e}, ${s} ${volmute}"
+    ];
 
+    bindl = let 
+      p = "${config.services.playerctld.package}/bin/playerctl";
+      nx = "next";
+      pv = "previous";
+      pl = "play-pause";
+      st = "stop";
+    in [
+      ### Media Control using playerctl ###
 
-      #Show when caps lock is pressed
-      ",caps_lock,exec,${swayosd} --caps-lock"
+      ",XF86AudioNext,${e},${p} ${nx}"
+      ",XF86AudioPrev,${e},${p} ${pv}"
+      ",XF86AudioPlay,${e},${p} ${pl}"
+      ",XF86AudioStop,${e},${p} ${st}"
+
+      #Same but for keyboards without media keys
+      "ALT,F12,${e},${p} ${nx}"
+      "ALT,F9,${e},${p} ${pv}"
+      "ALT,F10,${e},${p} ${pl}"
+      "ALT,F11,${e},${p} ${st}"
+    ];
+
+    bindr = let
+      any = "${config.programs.anyrun.package}/bin/anyrun";
+      easy = "${config.programs.easyeffects.package}/bin/easyeffects";
+      cl = "${pkgs.nix-scripts}/bin/color-picker";
+      s = "${config.services.swayosd.package}/bin/swayosd";
+      pk = "pkill";
+
+      # Screenshots 
+      hs = "pkill hyprshot && ${pkgs.hyprshot}/bin/hyprshot";
+      scr-dir = "~/Pictures/Screenshots";
+      r = "region";
+      o = "output";
+    in [
+      # Launch the launcher - anyrun
+      "super, space, ${e}, ${pk} ${any} || ${any}"
+
+      # Launch easyeffects
+      "super, a, ${e}, ${pk} ${easy} || ${h} '${easy}'"
+
+      # Show when caps lock is pressed
+      ",caps_lock,${e},${s} --caps-lock"
+
+      # Color picker
+      "supershift, c, ${e}, ${pk} ${cl} || ${h} '${cl}'"
 
       # Screenshotting
-      ", print, exec, ${hyprshot} -m region -o ~/Pictures/Screenshots"
-      "shift, print, exec, ${hyprshot} -m output -c -o ~/Pictures/Screenshots"
-      "alt, print, exec, ${hyprshot} -m output -o ~/Pictures/Screenshots"
-
-      # Media control
-      ",XF86AudioNext,exec,${playerctl} next"
-      ",XF86AudioPrev,exec,${playerctl} previous"
-      ",XF86AudioPlay,exec,${playerctl} play-pause"
-      ",XF86AudioStop,exec,${playerctl} stop"
-      "ALT,XF86AudioNext,exec,${playerctld} shift"
-      "ALT,XF86AudioPrev,exec,${playerctld} unshift"
-      "ALT,XF86AudioPlay,exec,systemctl --user restart playerctld"
-
-      #Same but for keyboards without media keys
-      "ALT,F12,exec,${playerctl} next"
-      "ALT,F9,exec,${playerctl} previous"
-      "ALT,F10,exec,${playerctl} play-pause"
-      "ALT,F11,exec,${playerctl} stop"
+      ", print, ${e}, ${hs} -m ${r} -o ${scr-dir}"
+      "shift, print, ${e}, ${hs} -m ${o} -c -o ${scr-dir}"
+      "alt, print, ${e}, ${hs} -m ${o} -o ${scr-dir}"     
     ];
   };
 }
