ddc6cebe3e4b7f5e4f734368a9f8bc99714cb1a2
Daru-san <mica.tui@proton.me>
2023-12-20 13:40:06 +0200

    update home packages

--- home/daru/Modules/Programs/default.nix
+++ home/daru/Modules/Programs/default.nix
@@ -10,37 +10,23 @@
     ./Video/default.nix
     ./Games/default.nix
     ./Browser/default.nix
+    ./desktop-entries.nix
   ];
 
 
   home.packages = with pkgs; 
-  let
-    #Package overides to allow electron apps to run in wayland natively
-    logseq-wayland = pkgs.logseq.overrideAttrs (old: rec {
-      commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
-    }); 
-    freetube-wayland = pkgs.freetube.overrideAttrs (old: rec {
-      commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
-    });
-    github-desktop-wayland = pkgs.github-desktop.overrideAttrs (old: rec {
-      commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
-    });
-  in
     [
-    logseq-wayland
+    logseq
     krita
     rssguard
     libsForQt5.okular
     libsForQt5.dolphin
-    libsForQt5.plasma-disks
-    partition-manager
-    freetube-wayland
+    freetube
     komikku
     scrcpy
     localsend
     libsForQt5.discover
     keepassxc
-    github-desktop-wayland
     pqiv
     transmission-qt
   ];
