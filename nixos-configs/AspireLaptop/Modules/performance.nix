34bd2f32a7d5a6eb3242bc06880d33ee25c00476
Daru-san <mica.tui@proton.me>
2023-12-20 18:05:34 +0200

    small tweak

--- nixos-configs/AspireLaptop/Modules/performance.nix
+++ nixos-configs/AspireLaptop/Modules/performance.nix
@@ -4,9 +4,6 @@
 
   #Enable tlp for power management
   services.tlp.enable = true;
-  #
-  # #Disable power-profiles-daemon for GNOME (if enabled)
-  services.power-profiles-daemon.enable = false;
 
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="[sv]d[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
