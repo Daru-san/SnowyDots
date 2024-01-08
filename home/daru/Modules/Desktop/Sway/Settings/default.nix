550ec37a83e63e49c175dbf593aac057415eb077
Daru-san <mica.tui@proton.me>
2024-01-08 13:04:24 +0200

    update sway config

--- home/daru/Modules/Desktop/Sway/Settings/default.nix
+++ home/daru/Modules/Desktop/Sway/Settings/default.nix
@@ -17,6 +17,21 @@
       terminal = "${config.programs.kitty.package}/bin/kitty";
       menu = "${config.programs.fuzzel.package}/bin/fuzzel";
     };
+    extraConfig = ''
+      # Applies all generated settings
+      include ~/.config/sway/.generated_settings/*.conf
+
+      # Launches sway-wallpaper when setting wallpaper from swaymsg.
+      # Without this, swaybg would launch instead...
+      swaybg_command sway-wallpaper
+
+      # To apply the selected wallpaper
+      exec_always sway-wallpaper -i ~/.cache/wallpaper -m fill
+
+      # Start all of the non-hidden applications in ~/.config/autostart
+      # This executable is included in the swaysettings package
+      exec sway-autostart
+    '';
   };
 }
 
