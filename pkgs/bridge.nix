889a632be85dea089c8b9c36b9e1e9696e600f5a
Daru-san <mica.tui@proton.me>
2024-01-09 13:51:18 +0200

    update bridge

--- pkgs/bridge.nix
+++ pkgs/bridge.nix
@@ -2,7 +2,7 @@
   appimageTools.wrapType2 { 
     name = "bridge-core";
     src = builtins.fetchurl {
-      url = "https://github.com/bridge-core/editor/releases/download/v2.7.8/bridge_2.7.8_amd64.AppImage";
+      url = "https://github.com/bridge-core/editor/releases/download/v2.7.9/bridge_2.7.9_amd64.AppImage";
     };
     extraPkgs = pkgs: with pkgs; [ ];
 }
