469aadad4f7e694678441ecd97a54df574333199
Daru-san <mica.tui@proton.me>
2024-01-16 20:28:53 +0200

    update hardware-configuration.nix: switch to ext4

--- systems/AspireLaptop/hardware-configuration.nix
+++ systems/AspireLaptop/hardware-configuration.nix
@@ -9,56 +9,32 @@
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")];
 
-  fileSystems."/" =
-    { device = "none";
-      fsType = "tmpfs";
-    };
-
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
-      fsType = "btrfs";
-      options = [ "subvol=nix" ];
-    };
-
-  fileSystems."/etc" =
-    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
-      fsType = "btrfs";
-      options = [ "subvol=etc" ];
-    };
-
-  fileSystems."/var/log" =
-    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
-      fsType = "btrfs";
-      options = [ "subvol=log" ];
-    };
-
-  fileSystems."/root" =
-    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
-      fsType = "btrfs";
-      options = [ "subvol=root" ];
-    };
-
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
-      fsType = "btrfs";
-      options = [ "subvol=home" ];
-    };
-
-  fileSystems."/tmp" =
-    { device = "/dev/disk/by-uuid/186fce87-42a0-4b38-8222-f3fe8661cf9f";
-      fsType = "btrfs";
-      options = [ "subvol=tmp" ];
-    };
-
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/D04A-9AA9";
       fsType = "vfat";
+    };
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/3bad4adb-b55b-4913-907f-fe78cd309314";
+      fsType = "ext4";
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/56681e75-adf4-4a3d-8226-4f92dc6eb990";
+      fsType = "ext4";
     };
 
   swapDevices =
     [ { device = "/dev/disk/by-uuid/78d797f0-5db6-4f7c-8030-685e2e381935"; }
     ];
 
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp2s0f1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
