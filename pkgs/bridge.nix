194ad82addaa8862be453128e07ebc894b93311d
Daru-san <mica.tui@proton.me>
2024-01-19 18:24:02 +0200

    update bridge hash

--- pkgs/bridge.nix
+++ pkgs/bridge.nix
@@ -1,12 +1,12 @@
 {appimageTools, ...}:
 let
   version = "v2.7.10";
-in 
-  appimageTools.wrapType2 { 
+in
+  appimageTools.wrapType2 {
     name = "bridge-core";
     src = builtins.fetchurl {
       url = "https://github.com/bridge-core/editor/releases/download/${version}/bridge_${version}_amd64.AppImage";
-      sha256 = "1ayq75rk0wbvl2apzm8906m3jyh7xl3xbrx460v430wjwpg4d7nx";
+      sha256 = "sbA+c5FJa7xTqgqwzKlGvjqjN79L+tCWRgiFH25PCuY=";
     };
 }
 
