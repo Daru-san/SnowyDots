7e7d18fc3bc8ef389d9d362390b3a3b8df9b13f2
Daru-san <mica.tui@proton.me>
2024-01-30 16:43:05 +0200

    clean swaync module

--- modules/home-manager/wayland/swaync/default2.nix
+++
@@ -1,9 +0,0 @@
-{
-  pkgs,
-  ...
-}:
-{
-  home.packages = with pkgs; [swaynotificationcenter];
-  xdg.configFile."swaync/config.json".text = (builtins.readFile ./config.json);
-  xdg.configFile."swaync/style.css".text = (builtins.readFile ./style.css);
-}
