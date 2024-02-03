1526ff43e3db4db21510a9503fb7297e88b9a81c
Daru-san <mica.tui@proton.me>
2024-02-03 18:25:22 +0200

    updates

--- home/daru/programs/bat.nix
+++ home/daru/programs/bat.nix
@@ -1,6 +1,10 @@
 {pkgs,...}:{
   programs.bat = {
     enable = true;
+    config = {
+      theme = "ansi";
+      pager = "less -FR";
+    };
     extraPackages = with pkgs.bat-extras; [batman batdiff batgrep batwatch prettybat batpipe];
   };
 }
