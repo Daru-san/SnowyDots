04210005512c096411670c6e14612364a5b52f9e
Daru-san <mica.tui@proton.me>
2024-02-06 18:14:07 +0200

    remove redundant lines from git.nix

--- modules/home-manager/git/git.nix
+++ modules/home-manager/git/git.nix
@@ -26,23 +26,4 @@
       ini = "init";
     };
   };
-  home.packages = with pkgs; [
-      #Git scripts to make editing from any directory easier
-      (pkgs.writeShellScriptBin "git-push" ''
-        cd ~/snowflake
-        git push 
-      '')
-      (pkgs.writeShellScriptBin "git-ui" ''
-        cd ~/snowflake/
-        gitui
-     '')
-      (pkgs.writeShellScriptBin "git-r" ''
-        cd ~/snowflake/ 
-        ranger
-     '')
-      (pkgs.writeShellScriptBin "git-alias"''
-        printf "These are the available git aliases:"
-        printf "\nci = commit\nco = checkout\ncl = clone\nst = status\nad = add\nps = push\npl = pull\nre = remote\nbr = branch\nfe = fetch\ncp = cherry-pick\nrs = reset\ndf = diff\nini = init\n"
-      '')
-  ];
 }
