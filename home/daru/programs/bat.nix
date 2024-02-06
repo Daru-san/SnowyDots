4b03af1095425b541a651713e00149e9e5ec0301
Daru-san <mica.tui@proton.me>
2024-02-06 18:34:57 +0200

    bat: use page instead

--- home/daru/programs/bat.nix
+++ home/daru/programs/bat.nix
@@ -3,7 +3,7 @@
     enable = true;
     config = {
       theme = "ansi";
-      pager = "nvim -R";
+      pager = "${pkgs.page}/bin/page";
     };
     extraPackages = with pkgs.bat-extras; [batman batdiff batgrep batwatch prettybat batpipe];
   };
