6e9577f06de10b1e93589e0ffe81728ceca69819
Daru-san <mica.tui@proton.me>
2024-02-12 23:46:58 +0200

    zsh: add plugin

--- modules/home-manager/shell/zsh/default.nix
+++ modules/home-manager/shell/zsh/default.nix
@@ -1,42 +1,50 @@
 ##Configuration for ZSH shell##
 
-{config, pkgs, ...}: {
+{config, pkgs, ...}:{
 
-  #Enable command-not-found
+  # Enable command-not-found
   programs.command-not-found = {
     enable = true;
   };
 
-  programs = { 
+  programs = {
     # Zsh configs
     zsh = {
       enable = true;
-
+      enableCompletion = true;
       autocd = true;
 
-      #History file
+      # History file
       history = {
         size = 10000;
         path = "${config.xdg.dataHome}/zsh/history";
       };
 
-    #  initExtra = "${config.programs.tmux.package}/bin/tmux";
-
-      #Syntax highlighting
+      # Syntax highlighting
       syntaxHighlighting = {
         enable = true;
       };
 
-      #Enable autosuggestions
+      # Enable autosuggestions
       enableAutosuggestions = true;
-      
-      #Oh-my-zsh for plugins, themes are set by oh-my-posh
+
+      plugins = [
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+      ];
+      # Oh-my-zsh for plugins, themes are set by oh-my-posh
       oh-my-zsh = {
         enable = true;
 
-        # theme = "minimal";
-
-        #Plugins
+        # Plugins
         plugins = [ 
           "git" 
           "sudo" 
