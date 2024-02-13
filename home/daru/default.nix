7efc9dcc2b6b0d185926b9d347e917cb30530ced
Daru-san <mica.tui@proton.me>
2024-02-13 21:45:38 +0200

    gc: add

--- home/daru/default.nix
+++ home/daru/default.nix
@@ -42,4 +42,9 @@
       permittedInsecurePackages = ["electron-25.9.0"];
     };
   };
+  nix.gc = {
+    automatic = true;
+    frequency = "weekly";
+    options = "--delete-older-than 7d";
+  };
 }
