3046258614abc1fad9c526934c48d2549e401c70
Daru-san <mica.tui@proton.me>
2024-02-17 18:34:24 +0200

    programs: update desktopEntries

--- home/daru/programs/desktop-entries.nix
+++ home/daru/programs/desktop-entries.nix
@@ -1,35 +1,8 @@
 {
   pkgs,
-  config,
   ...
 }: {
   xdg.desktopEntries = {
-    # spotify = {
-    #   name = "Spotify";
-    #   genericName = "Music Player";
-    #   comment = "Customized music player, stream all your favourite music, for free and wayland native!";
-    #   exec = "spotify %U --enable-features=UseOzonePlatform --ozone-platform=wayland";
-    #   icon = "spotify";
-    #   type = "Application";
-    #   settings = {
-    #     Keywords = "music";
-    #   };
-    # };
-    waypaper = {
-      name = "Waypaper";
-      genericName = "Wallpaper Browser";
-      comment = "Browse wallpapers and apply them whenever you want!";
-      exec = "${pkgs.waypaper}/bin/waypaper %U";
-      icon = "livewallpaper";
-      type = "Application";
-      settings = {Keywords = "wallpaper";};
-    };
-    logseq = {
-      name = "Logseq";
-      exec = "${pkgs.logseq}/bin/logseq %U --enable-features=UseOzonePlatform --ozone-platform=wayland";
-      icon = "logseq";
-      type = "Application";
-    };
     freetube = {
       name = "FreeTube";
       genericName = "YouTube";
