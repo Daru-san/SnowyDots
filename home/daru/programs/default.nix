9dcf11413e75852ccca070db648fb5c2a89147ae
Daru-san <mica.tui@proton.me>
2024-02-18 19:33:45 +0200

    pkgs: add standardnotes

--- home/daru/programs/default.nix
+++ home/daru/programs/default.nix
@@ -2,6 +2,35 @@
 {pkgs, ...}: {
   imports = [./desktop-entries.nix ./music/default.nix ./firefox/default.nix];
 
+  # Overlay for standardnotes from https://github.com/NixOS/nixpkgs/issues/278191#issuecomment-1910865477
+  nixpkgs.overlays = [
+    (final: prev: {
+      standardnotes-patch = let
+        pname = "standardnotes";
+        version = "3.183.22";
+        src = prev.fetchurl {
+          url = "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%403.183.22/standard-notes-3.183.22-linux-x86_64.AppImage";
+          hash = "sha256-tPbTgM13+c+wyhCAPawr3U71ofIHFN96x8QYes7RhfE=";
+        };
+        appimageContents = prev.appimageTools.extract {inherit pname version src;};
+      in
+        prev.appimageTools.wrapType2 {
+          inherit pname version src;
+
+          extraPkgs = pkgs: [pkgs.libsecret];
+
+          extraInstallCommands = ''
+            chmod -R +w $out
+            mv $out/bin/${pname}-${version} $out/bin/${pname}
+
+            ${prev.desktop-file-utils}/bin/desktop-file-install --dir $out/share/applications \
+              --set-key Exec --set-value ${pname} ${appimageContents}/standard-notes.desktop
+            ln -s ${appimageContents}/usr/share/icons $out/share
+          '';
+        };
+    })
+  ];
+
   # GUI
   home.packages = with pkgs;
     [
@@ -20,6 +49,7 @@
       audacity
       overskride
       pavucontrol
+      standardnotes-patch
     ]
     # GNOME specific packages
     ++ (with gnome; [nautilus dconf-editor gnome-clocks])
