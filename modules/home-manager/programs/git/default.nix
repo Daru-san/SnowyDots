46081c6e5a26f8c2d81f205fc8626430dcb73da9
Daru-san <mica.tui@proton.me>
2024-02-21 16:17:37 +0200

    git: remove gitui; update config; add lazygit
    Move git main config to git/default.nix
    Remove gitui in favour of lazygit

--- modules/home-manager/programs/git/default.nix
+++ modules/home-manager/programs/git/default.nix
@@ -1,5 +1,31 @@
 {...}: {
-  imports = [./git.nix];
-  programs.gitui = {enable = true;};
+  imports = [./lazygit.nix];
   programs.git-cliff = {enable = true;};
+  programs.git = {
+    enable = true;
+    userName = "Daru-san";
+    userEmail = "mica.tui@proton.me";
+
+    # Git highlighting
+    diff-so-fancy.enable = true;
+
+    # Git aliases
+    aliases = {
+      ci = "commit";
+      co = "checkout";
+      cl = "clone";
+      st = "status";
+      ad = "add";
+      ps = "push";
+      pl = "pull";
+      re = "remote";
+      br = "branch";
+      fe = "fetch";
+      cp = "cherry-pick";
+      rs = "reset";
+      df = "diff";
+      ini = "init";
+    };
+  };
+
 }
---
+++ modules/home-manager/programs/git/lazygit.nix
@@ -0,0 +1,43 @@
+{...}: {
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui = {
+        windowSize = "half";
+        mainPanelSplitMode = "horizontal";
+        expandFocusedSidePanel = true;
+        mouseEvents = true;
+        commitLength.show = true;
+        showFileTree = true;
+        showBottomLine = false;
+        nerdFontsVersion = 3;
+        portaitMode = "always";
+        showFileIcons = true;
+        showRandomTip = true;
+      };
+      git = {
+        autoFetch = true;
+        autoRefresh = true;
+        fetchAll = false;
+        mainBranches = ["master" "main"];
+        paging.pager = "diff-so-fancy";
+      };
+      os = {
+      };
+      refresher = {
+        refreshInterval = 7;
+        fetchInterval = 60;
+      };
+      keybinding = {
+        status = {
+          checkForUpdate = "u";
+          recentRepos = "<enter>";
+        };
+      };
+      update.method = "never";
+      notARepository = "prompt";
+      disableStartupPopups = true;
+      confirmOnQuit = true;
+    };
+  };
+}
