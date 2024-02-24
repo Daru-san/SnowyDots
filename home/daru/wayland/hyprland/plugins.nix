935e51936daf8f800a0ab685ae5e859d432b597b
Daru-san <mica.tui@proton.me>
2024-02-24 17:19:00 +0200

    hyprland: temporarily disable hyprfocus
    Hyprfocus does not build for some reason, so for the time being, it will remain disabled

--- home/daru/wayland/hyprland/plugins.nix
+++ home/daru/wayland/hyprland/plugins.nix
@@ -6,7 +6,7 @@
   wayland.windowManager.hyprland = {
     plugins = [
       inputs.hycov.packages.${pkgs.system}.hycov
-      inputs.hyprfocus.packages.${pkgs.system}.hyprfocus
+      # inputs.hyprfocus.packages.${pkgs.system}.hyprfocus
     ];
     extraConfig = ''
       plugin {
@@ -16,35 +16,35 @@
           hotarea_size = 10 #hotarea size in bottom left,10x10
           enable_hotarea = 1 # enable mouse cursor hotarea
         }
-        hyprfocus {
-          enabled = yes
-
-          keyboard_focus_animation = flash
-          mouse_focus_animation = flash
-
-          bezier = bezIn, 0.5,0.0,1.0,0.5
-          bezier = bezOut, 0.0,0.5,0.5,1.0
-
-          flash {
-            flash_opacity = 0.7
-
-            in_bezier = bezIn
-            in_speed = 0.5
-
-            out_bezier = bezOut
-            out_speed = 3
-          }
-
-          shrink {
-            shrink_percentage = 0.8
-
-            in_bezier = bezIn
-            in_speed = 0.5
-
-            out_bezier = bezOut
-            out_speed = 3
-          }
-        }
+        # hyprfocus {
+        #   enabled = yes
+        #
+        #   keyboard_focus_animation = flash
+        #   mouse_focus_animation = flash
+        #
+        #   bezier = bezIn, 0.5,0.0,1.0,0.5
+        #   bezier = bezOut, 0.0,0.5,0.5,1.0
+        #
+        #   flash {
+        #     flash_opacity = 0.7
+        #
+        #     in_bezier = bezIn
+        #     in_speed = 0.5
+        #
+        #     out_bezier = bezOut
+        #     out_speed = 3
+        #   }
+        #
+        #   shrink {
+        #     shrink_percentage = 0.8
+        #
+        #     in_bezier = bezIn
+        #     in_speed = 0.5
+        #
+        #     out_bezier = bezOut
+        #     out_speed = 3
+        #   }
+        # }
       }
     '';
   };
