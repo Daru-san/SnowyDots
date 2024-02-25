e2a4b788cc08f74b92d09bb6dfabc1bcc9c24cee
Daru-san <mica.tui@proton.me>
2024-02-25 19:24:19 +0200

    clean config files

--- home/daru/programs/default.nix
+++ home/daru/programs/default.nix
@@ -7,35 +7,6 @@
 }: {
   imports = [./desktop-entries.nix ./music/default.nix ./firefox/default.nix];
 
-  # Overlay for standardnotes from https://github.com/NixOS/nixpkgs/issues/278191#issuecomment-1910865477
-  nixpkgs.overlays = [
-    (final: prev: {
-      standardnotes-patch = let
-        pname = "standardnotes";
-        version = "3.183.22";
-        src = prev.fetchurl {
-          url = "https://github.com/standardnotes/app/releases/download/%40standardnotes%2Fdesktop%403.183.22/standard-notes-3.183.22-linux-x86_64.AppImage";
-          hash = "sha256-tPbTgM13+c+wyhCAPawr3U71ofIHFN96x8QYes7RhfE=";
-        };
-        appimageContents = prev.appimageTools.extract {inherit pname version src;};
-      in
-        prev.appimageTools.wrapType2 {
-          inherit pname version src;
-
-          extraPkgs = pkgs: [pkgs.libsecret];
-
-          extraInstallCommands = ''
-            chmod -R +w $out
-            mv $out/bin/${pname}-${version} $out/bin/${pname}
-
-            ${prev.desktop-file-utils}/bin/desktop-file-install --dir $out/share/applications \
-              --set-key Exec --set-value ${pname} ${appimageContents}/standard-notes.desktop
-            ln -s ${appimageContents}/usr/share/icons $out/share
-          '';
-        };
-    })
-  ];
-
   home.packages = with lib;
     mkMerge [
       (with pkgs; [
@@ -43,7 +14,6 @@
         obsidian
         krita
         newsflash
-        evince
         freetube
         scrcpy
         keepassxc
@@ -54,7 +24,6 @@
         audacity
         overskride
         pavucontrol
-        standardnotes-patch
         valent
         iwgtk
 
@@ -73,6 +42,7 @@
         nitch
         pulsemixer
         ani-cli
+        manga-cli
         pokeshell
         du-dust
         fzf
