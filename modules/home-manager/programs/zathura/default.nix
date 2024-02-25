741331e937d8582cfc91ccb5e4c558a347f70a2c
Daru-san <mica.tui@proton.me>
2024-02-25 19:23:30 +0200

    zathura: update theme

--- modules/home-manager/programs/zathura/default.nix
+++ modules/home-manager/programs/zathura/default.nix
@@ -1,27 +1,31 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.zathura = {
     enable = true;
     options = {
       recolor = true;
-      recolor-darkcolor = "#161616";
+      recolor-lightcolor = "rgba(36, 40, 59, 0.6)";
+      recolor-darkcolor = "rgba(0, 0, 0, 0.3)";
       font = config.fonts.serif.name;
       page-padding = 3;
       window-title-basename = true;
     };
     mappings = {
       m = "set recolor true";
-      M = "set recolor false";
     };
     extraConfig = let
-      base16-zathura = fetchFromGitHub {
-        owner = "HaoZeke";
-        repo = "base16-zathura";
-        rev = "9f148b4001dc67d399e645919225943d47e50125";
-        hash = "sha256-nVJoygnytYN40VHXpAFwO7ruVbaSydeR547MU45VSuY=";
-      };
-      theme = "onedark";
-    in ''
-      ${builtins.readFile ./${base16-zathura}/build_schemes/colors/base16-${theme}.config}
-    '';
+      base16-zathura = with pkgs;
+        fetchFromGitHub {
+          owner = "HaoZeke";
+          repo = "base16-zathura";
+          rev = "9f148b4001dc67d399e645919225943d47e50125";
+          hash = "sha256-nVJoygnytYN40VHXpAFwO7ruVbaSydeR547MU45VSuY=";
+        };
+      theme = "material-darker";
+    in
+      with builtins; readFile "${base16-zathura}/build_schemes/colors/base16-${theme}.config";
   };
 }
