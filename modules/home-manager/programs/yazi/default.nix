0b0026b1652c575e65bdb87f26cd7d2f18800331
Daru-san <mica.tui@proton.me>
2024-02-25 20:45:33 +0200

    yazi: update binds

--- modules/home-manager/programs/yazi/default.nix
+++ modules/home-manager/programs/yazi/default.nix
@@ -1,4 +1,4 @@
-{...}:{
+{pkgs, ...}: {
   programs.yazi = {
     enable = true;
     settings = {
@@ -9,32 +9,29 @@
       show_symlink = true;
 
       opener = {
-        archive = [{exec = ''extract "$1"''; desc = "Extract here";} ];
-        text = [{exec = ''vi "$@"''; block = true;}];
+        archive = [
+          {
+            exec = ''extract "$1"'';
+            desc = "Extract here";
+          }
+        ];
+        text = [
+          {
+            exec = ''vi "$@"'';
+            block = true;
+          }
+        ];
         document = [{exec = ''${pkgs.zathura}/bin/zathura "$@"'';}];
-        image = [{exec = ''pqiv "@"''}];
+        image = [{exec = ''pqiv "@"'';}];
       };
       open = {
         rules = [
-          	{ name = "*.pdf"; use = "document"; }
+          {
+            name = "*.pdf";
+            use = "document";
+          }
         ];
       };
     };
-    keymaps = {
-      input.keymap = [
-        {on = ["<C-h>"]; exec = "hidden toggle";}
-        {on = ["<C-n"]; exec = "cd --interactive";}
-        {on = ["<C-o>"]; exec = "open --interactive";}
-        {on = ["<C-c>"]; exec = "yank";}
-        {on = ["<C-x>"]; exec = "yank --cut";}
-        {on = ["<C-v>"]; exec = "paste";}
-        {on = ["<C-b>"]; exec = "unyank";}
-        {on = ["<C-l>"]; exec = "link";}
-        {on = ["<n>"]; exec = "create";}
-        {on = ["<F12>"]; exec = "tab_create";}
-        {on = ["<F11>"]; exec = "tab_close";}
-        {on = ["<F10>"]; exec = "tab_switch";}
-      ];
-    }
-  }
+  };
 }
