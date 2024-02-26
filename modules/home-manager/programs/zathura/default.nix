27b7e49a5ca53fb11a364b024a1f29143462b694
Daru-san <mica.tui@proton.me>
2024-02-26 20:44:34 +0200

    zathura: patch attempt

--- modules/home-manager/programs/zathura/default.nix
+++ modules/home-manager/programs/zathura/default.nix
@@ -5,18 +5,20 @@
 }: {
   programs.zathura = {
     enable = true;
+    package = pkgs.zathura.overrideAttrs (o: {
+          patches = (o.patches or [ ]) ++ [
+            
+          ];
+        });
     options = {
       recolor = true;
       recolor-keephue = true;
-      default-bg = "rgba(255,255,255,0.7)";
+      default-bg = "rgba(255,255,255,0)";
       recolor-lightcolor = "rgba(255,255,255,0)";
       recolor-darkcolor = "black";
       font = config.fonts.serif.name;
       page-padding = 3;
       window-title-basename = true;
-    };
-    mappings = {
-      m = "set recolor true";
     };
   };
 }
