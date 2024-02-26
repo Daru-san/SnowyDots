8bea48057ef969b99d5c9c86b24a0ff893789ea5
Daru-san <mica.tui@proton.me>
2024-02-26 19:23:16 +0200

    waybar: remove unnecessesary patch

--- home/daru/wayland/waybar/waybar.nix
+++ home/daru/wayland/waybar/waybar.nix
@@ -5,19 +5,6 @@
   ...
 }: {
   programs.waybar = {
-    #Fix for waybar hyprland scrolling
-    package = pkgs.waybar.overrideAttrs (oa: {
-      mesonFlags = (oa.mesonFlags or []) ++ ["-Dexperimental=true"];
-      patches =
-        (oa.patches or [])
-        ++ [
-          (pkgs.fetchpatch {
-            name = "fix waybar hyprctl";
-            url = "https://aur.archlinux.org/cgit/aur.git/plain/hyprctl.patch?h=waybar-hyprland-git";
-            sha256 = "sha256-pY3+9Dhi61Jo2cPnBdmn3NUTSA8bAbtgsk2ooj4y7aQ=";
-          })
-        ];
-    });
 
     enable = true;
 
