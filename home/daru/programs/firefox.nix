3945c4158724af9fa1d69199dfe37659e5fcb852
Daru-san <mica.tui@proton.me>
2024-02-26 18:05:45 +0200

    firefox: clean config, move css to main file

--- home/daru/programs/firefox.nix
+++ home/daru/programs/firefox.nix
@@ -3,9 +3,14 @@
   config,
   inputs,
   ...
-}: {
-  imports = [./theme.nix];
-
+}: let
+  # Fetch css from bjesus/fireside on github
+  src = pkgs.fetchgit {
+    url = "https://github.com/bjesus/fireside";
+    rev = "ae26321db68d2cefe44fd74a8678aa13b83d36f5";
+    sha256 = "mwlw8PNurozOBvfG6D809fxQmqiutakWq89h4UGbcXo=";
+  };
+in {
   programs.firefox = {
     enable = true;
 
@@ -15,6 +20,9 @@
     profiles.daru = {
       # Name
       name = "darumaka";
+
+      # Custom css
+      userChrome = with builtins; readFile "${src}/userChrome.css";
 
       # Extensions(from NUR)
       extensions = with config.nur.repos.rycee.firefox-addons;
