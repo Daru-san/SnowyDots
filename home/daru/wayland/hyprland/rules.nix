c97890057298b9ed80ae08b52f67e2f469fd5b08
Daru-san <mica.tui@proton.me>
2024-03-09 15:34:39 +0200

    hypr: update window and workspace rules

--- home/daru/wayland/hyprland/rules.nix
+++ home/daru/wayland/hyprland/rules.nix
@@ -9,37 +9,35 @@
       windowrule = let
         f = regex: "float, ^(${regex})$";
         c = regex: "center, ^(${regex})$";
-        w = regex: "workspace audio, ^(${regex})$";
       in
         [
           # Floating windows
           (f "com.github.hluk.copyq")
-          (f "com.github.wwmm.easyeffects")
           (f "file-roller")
+          (f "org.kde.kdeconnect.daemon")
 
           # Center them
           (c "file-roller")
           (c "com.github.hluk.copyq")
-          (c "com.github.wwmm.easyeffects")
+          (c "org.kde.kdeconnect.daemon")
         ]
         ++ [
           #Window specific worspaces
           "workspace name:7,^(krita)$"
           "workspace name:8,^(pqiv)$"
-          "workspace name:8,^(waypaper)$"
+          "workspace name:9,^(com.github.wwmmasyeffects)$"
+
           #Productivity workspaces
           "workspace name:F1,^(firefox-nightly)$"
           "workspace name:F2,^(osidian)$"
           "workspace name:F4,^(org.gnome.Nautilus)$"
 
           #Entertainment workspaces
-          "workspace name:F5,^(info.febvre.Komikku)$"
           "workspace name:F6,^(mpv)$"
           "workspace name:F7,^(FreeTube)$"
           "workspace name:F8,title:^(Spotify)(.*)$"
 
           #Others
-          "workspace name:F9,^(io.gitlab.news_flash.NewsFlash)$"
           "workspace name:F10,^(org.pwmt.zathura)$"
         ]
         ++
