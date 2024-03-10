fb6d7356ad3e3aa318c942f64bfe8bab4be80661
Daru-san <mica.tui@proton.me>
2024-03-10 17:32:30 +0200

    bat: update theme to catppuccin
    Nice right?

--- modules/home-manager/programs/bat/default.nix
+++ modules/home-manager/programs/bat/default.nix
@@ -2,8 +2,20 @@
   programs.bat = {
     enable = true;
     config = {
-      theme = "ansi";
+      theme = "catppuccin";
       pager = "less -FR";
+      paging = "always";
+    };
+    themes = {
+      catppuccin = {
+        src = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "bat";
+          rev = "a875b71dc5514b2eae9174b0591ef421887aa99f";
+          hash = "sha256-PWX8LR3xTgNBUpk+xBIGpEBQCNJHre8cRX4OTSWkZvs=";
+        };
+        file = "themes/Catppuccin\ Mocha.tmTheme";
+      };
     };
     extraPackages = with pkgs.bat-extras; [
       batman
