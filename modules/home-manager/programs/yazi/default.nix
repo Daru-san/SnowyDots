53b9f4e0e2a5b4a9cae020591b2a511b30fde2dd
Daru-san <mica.tui@proton.me>
2024-03-10 18:03:54 +0200

    yazi: completely revamp configs
    Update the ui
    Update the theme
    Add Reledia/glow.yazi, Sonico98/exifaudio.yazi and DreamMaoMao/mime.yazi plugins
    Update a lot of the config
    Add custom lua options for better customized experience (for me that is)
    Add custom previewers
    General cleanup

--- modules/home-manager/programs/yazi/default.nix
+++ modules/home-manager/programs/yazi/default.nix
@@ -1,21 +1,95 @@
-{pkgs, ...}: let
-  theme = pkgs.fetchFromGitHub {
-    owner = "Reledia";
-    repo = "flexoki.yazi";
-    rev = "2987ef67b15683d041d708275ff4625a478ff18d";
-    hash = "sha256-qAg0BUO8jh0R1LIg4UwFLKFr247ZLi5mmwrSzEQDf1Q=";
-  };
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  theme = with pkgs;
+    fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "yazi";
+      rev = "0846aed69b2a62d29c98e100af0cf55ca729723d";
+      hash = "sha256-2T41qWMe++3Qxl9veRNHMeRI3eU4+LAueKTss02gYNk=";
+    }
+    + "themes/mocha.toml";
+  keymaps = with pkgs;
+    fetchFromGitHub {
+      owner = "sxyazi";
+      repo = "yazi";
+      rev = "78b98a98c356b2846d902aa349b8474d8fa60cd6";
+      hash = "sha256-LhGaQi2VbH6yZ2ujLeD66BeB2E7oO1/KnLnkZ9YKKKc=";
+    }
+    + "yazi-config/preset/keymap.toml";
 in {
-  xdg.configFile."yazi/theme.toml".source = "${theme}/theme.toml";
+  imports = [./lua.nix];
+  xdg.configFile = {
+    "yazi/theme.toml".source = theme;
+    "yazi/keymap.toml".text = with lib; mkDefault (mkBefore "${with builtins; readFile keymaps}");
+    "yazi/plugins/exifaudio.yazi".source = with pkgs;
+      fetchFromGitHub {
+        owner = "Sonico98";
+        repo = "exifaudio.yazi";
+        rev = "94329ead8b3a6d3faa2d4975930a3d0378980c7a";
+        hash = "sha256-jz6fVtcLHw9lsxFWECbuxE7tEBttE08Fl4oJSTifaEc=";
+      };
+    "yazi/plugins/glow.yazi".source = with pkgs;
+      fetchFromGitHub {
+        owner = "Reledia";
+        repo = "glow.yazi";
+        rev = "54b34c2b337ab0933130a06f61038398efa9ce65";
+        hash = "sha256-YTe76i5pdw0GhzWGBU41FDAKy3PJR7aPQgn+dbXHPNg=";
+      };
+    "yazi/plugins/mime.yazi".source = with pkgs;
+      fetchFromGitHub {
+        owner = "DreamMaoMao";
+        repo = "mime.yazi";
+        rev = "0ba4bb22e452c287daaf67fe87e218dc12205dba";
+        hash = "sha256-K+JXBfJPPl/scLeMCq4+OiyGjYKM7bJgdZf8q8O+2zk=";
+      };
+  };
   programs.yazi = {
     enable = true;
+    enableZshIntegration = true;
     settings = {
-      ratio = [1 4 3];
+      ratio = [0 4 4];
       sort_by = "natural";
       sort_dir_first = true;
       show_hidden = false;
-      show_symlink = true;
-
+      show_symlink = false;
+      keymap = {
+        input.keymap = [
+          {
+            on = ["l"];
+            run = "plugin --sync smart-enter";
+            desc = "Enter the child directory, or open the file";
+          }
+          {
+            on = ["<C-s>"];
+            run = ''shell "$SHELL" --block --confirm'';
+            desc = "Open shell here";
+          }
+          {
+            on = ["<Esc>"];
+            run = "close";
+            desc = "Cancel input";
+          }
+        ];
+      };
+      plugin = with lib; {
+        prepend_previewers = [
+          {
+            mime = "audio/*";
+            exec = "${getExe pkgs.exiftool}";
+          }
+          {
+            mime = "*.md";
+            exec = "${getExe pkgs.glow}";
+          }
+          {
+            mime = "text/*";
+            exec = "${getExe pkgs.bat}";
+          }
+        ];
+      };
       opener = {
         archive = [
           {
@@ -25,12 +99,12 @@ in {
         ];
         text = [
           {
-            exec = ''vi "$@"'';
+            exec = ''${with lib; getExe config.programs.neovim.package} "$@"'';
             block = true;
           }
         ];
-        document = [{exec = ''${pkgs.zathura}/bin/zathura "$@"'';}];
-        image = [{exec = ''pqiv "@"'';}];
+        document = [{exec = ''${with lib; getExe config.programs.zathura.package}"$@"'';}];
+        image = [{exec = ''${with lib; getExe config.programs.pqiv.package} "@"'';}];
       };
       open = {
         rules = [
---
+++ modules/home-manager/programs/yazi/lua.nix
@@ -0,0 +1,73 @@
+{...}: {
+  xdg.configFile = {
+    "yazi/init.lua".text = ''
+        function Manager:render(area)
+        local chunks = self:layout(area)
+
+        local bar = function(c, x, y)
+          x, y = math.max(0, x), math.max(0, y)
+          return ui.Bar(ui.Rect { x = x, y = y, w = ya.clamp(0, area.w - x, 1), h = math.min(1, area.h) }, ui.Bar.TOP)
+              :symbol(c)
+        end
+
+        return ya.flat {
+          -- Borders
+          ui.Border(area, ui.Border.ALL):type(ui.Border.ROUNDED),
+          ui.Bar(chunks[1], ui.Bar.RIGHT),
+          ui.Bar(chunks[3], ui.Bar.LEFT),
+
+          bar("┬", chunks[1].right - 1, chunks[1].y),
+          bar("┴", chunks[1].right - 1, chunks[1].bottom - 1),
+          bar("┬", chunks[2].right, chunks[2].y),
+          bar("┴", chunks[2].right, chunks[1].bottom - 1),
+
+          -- Parent
+          Parent:render(chunks[1]:padding(ui.Padding.xy(1))),
+          -- Current
+          Current:render(chunks[2]:padding(ui.Padding.y(1))),
+          -- Preview
+          Preview:render(chunks[3]:padding(ui.Padding.xy(1))),
+        }
+      end
+
+      function Status:name()
+        local h = cx.active.current.hovered
+        if not h then
+          return ui.Span("")
+        end
+
+        local linked = ""
+        if h.link_to ~= nil then
+          linked = " -> " .. tostring(h.link_to)
+        end
+        return ui.Span(" " .. h.name .. linked)
+      end
+
+      function Header:host()
+        if ya.target_family() ~= "unix" then
+          return ui.Line {}
+        end
+        return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+      end
+
+      function Header:render(area)
+        self.area = area
+
+        local right = ui.Line { self:count(), self:tabs() }
+        local left = ui.Line { self:host(), self:cwd(math.max(0, area.w - right:width())) }
+        return {
+          ui.Paragraph(area, { left }),
+          ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
+        }
+      end
+    '';
+    "yazi/plugins/smart-enter.yazi/init.lua".text = ''
+      return {
+        entry = function()
+          local h = cx.active.current.hovered
+          ya.manager_emit(h and h.cha.is_dir and "enter" or "open", {})
+        end,
+      }
+    '';
+  };
+}
