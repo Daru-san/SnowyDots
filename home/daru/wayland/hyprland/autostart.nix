ae82343056398b5fa97c956ac96dbe55a89f4052
Daru-san <mica.tui@proton.me>
2024-03-11 19:05:09 +0200

    hypr: clean autostart.nix

--- home/daru/wayland/hyprland/autostart.nix
+++ home/daru/wayland/hyprland/autostart.nix
@@ -8,8 +8,6 @@ with lib; {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "[workspace name:1] ${config.programs.kitty.package}/bin/kitty -T Terminal"
-      "[workspace name:2 silent] ${config.programs.kitty.package}/bin/kitty -T Git --session git.conf"
-      "[workspace name:F12 silent;fullscreen;noanim] ${config.programs.kitty.package}/bin/kitty -T Clock --hold tty-clock -bscBrn"
       "${config.services.copyq.package}/bin/copyq"
       "${config.programs.waybar.package}/bin/waybar"
       "hyprpm reload -n"
