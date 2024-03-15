2be2ad62bad0417daba4f93e262441b7636568b3
Daru-san <mica.tui@proton.me>
2024-03-15 20:17:16 +0200

    specialisation: hotfix

--- specialisations/default.nix
+++ specialisations/default.nix
@@ -1,16 +1,16 @@
 {...}: {
   specialisation = {
-    plasma.configuration = {
-      imports = [./plasma.nix];
-    };
+    # plasma.configuration = {
+    #   imports = [./plasma.nix];
+    # };
     plasma-ext-display.configuration = {
       imports = [./plasma.nix ./no-laptop-display.nix];
     };
     hyprland-ext-display.configuration = {
       imports = [./no-laptop-display.nix ./hyprland.nix];
     };
-    hyprland.configuration = {
-      imports = [./hyprland.nix];
-    };
+    # hyprland.configuration = {
+    #   imports = [./hyprland.nix];
+    # };
   };
 }
