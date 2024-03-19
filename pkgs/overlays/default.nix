837fd8cf529eec5de91045ed7b213e1edae5adca
Daru-san <mica.tui@proton.me>
2024-03-19 14:20:00 +0200

    overlays: update overlays file

--- pkgs/overlays/default.nix
+++ pkgs/overlays/default.nix
@@ -1,13 +1,12 @@
-# This file defines overlays
 {inputs, ...}: {
-  # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs {pkgs = final;};
+  # Take custom packages from pkgs/packages so they can be accessed using pkgs.(package)
+  additions = final: _prev: import ../packages {pkgs = final;};
 
   # When applied, the stable (23.05) nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.stable'
   stable-packages = final: _prev: {
     stable = import inputs.nixpkgs-stable {
-      system = final.system;
+      inherit (final) system;
       config.allowUnfree = true;
     };
   };
