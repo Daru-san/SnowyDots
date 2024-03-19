ade97fa41bca47f50210c013c2db2df02759bdf0
Daru-san <mica.tui@proton.me>
2024-03-19 10:23:39 +0200

    shell/oh-my-posh: add custom theme

--- modules/home-manager/shell/extras.nix
+++ modules/home-manager/shell/extras.nix
@@ -1,40 +1,43 @@
 {
-  # Unified theming for bash and zsh
-  programs.oh-my-posh = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-  };
+  programs = {
+    # Unified theming for bash and zsh
+    oh-my-posh = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      settings = builtins.fromJSON (builtins.unsafeDiscardStringContext (builtins.readFile ./oh-my-posh-theme.json));
+    };
 
-  # Coloured ls with cool features
-  programs.eza = {
-    enable = true;
-    git = true;
-    icons = true;
-    extraOptions = ["--group-directories-first"];
-    enableZshIntegration = true;
-    enableBashIntegration = true;
-  };
+    # Coloured ls with cool features
+    eza = {
+      enable = true;
+      git = true;
+      icons = true;
+      extraOptions = ["--group-directories-first"];
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+    };
 
-  # cd on steriods
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-    enableBashIntegration = true;
-  };
+    # cd on steriods
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+    };
 
-  # Searching through directories with ease
-  programs.broot = {
-    enable = true;
-    enableZshIntegration = true;
-    enableBashIntegration = true;
-    settings.modal = true;
-  };
+    # Searching through directories with ease
+    broot = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      settings.modal = true;
+    };
 
-  # Find files fuzzily
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-    enableBashIntegration = true;
+    # Find files fuzzily
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+    };
   };
 }
