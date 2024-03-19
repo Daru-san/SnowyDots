7a33d441f77cc69655602621d60823e6a7373135
Daru-san <mica.tui@proton.me>
2024-03-19 13:48:02 +0200

    modules/wayland: remove tschuss for wlogout

---
+++ modules/home-manager/wayland/wlogout/default.nix
@@ -0,0 +1,71 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.wlogout = {
+    enable = true;
+    layout = [
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "s";
+      }
+      {
+        label = "reboot";
+        action = "shutdown -r now";
+        text = "Reboot";
+        keybind = "r";
+      }
+      {
+        label = "logout";
+        action = "loginctl terminate-user $USER";
+        text = "Logout/Exit";
+        keybind = "e";
+      }
+      {
+        label = "Lock";
+        action = "hyprlock";
+        text = "Lock";
+        keybind = "l";
+      }
+      {
+        label = "Suspend";
+        action = "systemctl suspend";
+        text = "Suspend";
+        keybind = "s";
+      }
+      {
+        label = "hibernate";
+        action = "systemctl hibernate";
+        text = "Hibernate";
+        keybind = "h";
+      }
+    ];
+    style = let
+      icons =
+        pkgs.fetchFromGitHub {
+          owner = "ArtsyMacaw";
+          repo = "wlogout";
+          rev = "bc76e5d2433d234cce0e923fd386429759ef8f7a";
+          hash = "sha256-5ikvElaMYdg7jtSsn26FH1SvhEUhqgw9J6V9y/aPzIc=";
+        }
+        + "/icons";
+    in
+      builtins.concatLines "\n" [
+        ''
+          root: {
+            --shutdown: url(${icons}/shutdown.png)
+            --reboot: url(${icons}/reboot.png)
+            --lock: url(${icons}/lock.png)
+            --logout: url(${icons}/logout.png)
+            --suspend: url(${icons}/suspend.png)
+            --hibernate: url(${icons}/hibernate.png)
+          }
+        ''
+        (builtins.readFile config.colorScheme.css)
+        (builtins.readFile ./style.css)
+      ];
+  };
+}
