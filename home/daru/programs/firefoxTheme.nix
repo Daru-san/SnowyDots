bd5499ceff28ae0b793890859e5612107eeefd40
Daru-san <mica.tui@proton.me>
2024-03-21 16:25:10 +0200

    programs/firefox: change theme to https://github.com/christorange/VerticalFox

--- home/daru/programs/firefoxTheme.nix
+++ home/daru/programs/firefoxTheme.nix
@@ -2,26 +2,14 @@
 let
   # Fetch css from Godiesc/firefox-one
   src = builtins.fetchGit {
-    url = "https://github.com/Godiesc/firefox-one";
-    rev = "33e012a4d9ac45342bf159bffb2779b6147ac5d2";
+    url = "https://github.com/christorange/VerticalFox";
+    rev = "29d7ee3f320bc52bb928a1b0ba82457a6583fbfe";
   };
 in {
-  home.file.".mozilla/firefox/daru/chrome" = {
-    source = "${src}/chrome";
-  };
+  home.file.".mozilla/firefox/daru/chrome" = {source = "${src}/windows";};
 
   # Custom settings for firefoxone
   programs.firefox.profiles.daru.settings = {
-    "firefoxone.without-default-colors" = true;
-    "firefoxone.tree_tabs_style" = true;
-    "firefoxone.rhythm_sound_tab" = true;
-    "firefoxone.selected_tab_gradient_border" = true;
-    "firefoxone.style_without_leftsidebar" = true;
     "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-    "svg.context-properties.content.enabled" = true;
-    "layout.css.color-mix.enabled" = true;
-    "layout.css.backdrop-filter.enabled" = true;
-    "browser.tabs.delayHidingAudioPlayingIconMS" = 0;
-    "browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar" = false;
   };
 }
