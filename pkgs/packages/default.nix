6ae416487c165a21611fba362c6f09e6135abc62
Daru-san <mica.tui@proton.me>
2024-03-23 19:19:05 +0200

    pkgs: add zaread

--- pkgs/packages/default.nix
+++ pkgs/packages/default.nix
@@ -8,4 +8,7 @@
 
   # An audio mixer widget
   mixxc = pkgs.callPackage ./mixxc.nix {};
+
+  # Zaread, a script that enables running zathura on with other file types
+  zaread = pkgs.callPackage ./zaread.nix {};
 }
---
+++ pkgs/packages/zaread.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  zathura,
+  makeWrapper,
+  libreoffice,
+  typst,
+  bash,
+}:
+stdenv.mkDerivation {
+  pname = "zaread";
+  version = "c2d45e";
+
+  src = fetchFromGitHub {
+    owner = "paoloap";
+    repo = "zaread";
+    rev = "c2d45e1bbaeb0a637b1747cc89b483f2c6d11cb3";
+    hash = "sha256-g4Xb6gGA09/rfv0myzHbc830BwtD37IOYuEIwoOigP8=";
+  };
+
+  nativeBuildInputs = [makeWrapper];
+
+  hardeningDisable = ["format"];
+
+  buildPhase = ''
+    mkdir -p $out/bin
+
+    cp $src/zaread $out/bin/zaread
+  '';
+
+  installPhase = ''
+    wrapProgram $out/bin/zaread \
+      --prefix PATH : ${
+      lib.makeBinPath [bash zathura libreoffice typst]
+    }  '';
+
+  makeFlags = ["PREFIX=$(out)"];
+
+  meta = with lib; {
+    description = "Simple and fast highly customisable dropdown power menu for Linux systems.";
+    homepage = "https://github.com/Lucad44/tschuss";
+    license = licenses.mit;
+    maintainers = with maintainers; [daru];
+    platforms = platforms.linux;
+  };
+}
