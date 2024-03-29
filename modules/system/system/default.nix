866659e5e3e3d17b59083ed2744e3484dd00392e
Daru-san <mica.tui@proton.me>
2024-03-29 18:53:55 +0200

    modules/system: add security modules

--- modules/system/system/default.nix
+++ modules/system/system/default.nix
@@ -5,5 +5,6 @@
     ./hardware
     ./fonts
     ./shell
+    ./security
   ];
 }
---
+++ modules/system/system/security/default.nix
@@ -0,0 +1,24 @@
+{
+  security = {
+    sudo-rs.enable = true;
+    polkit = {
+      enable = true;
+      extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if (
+            subject.isInGroup("users")
+              && (
+                action.id == "org.freedesktop.login1.reboot" ||
+                action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+                action.id == "org.freedesktop.login1.power-off" ||
+                action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+              )
+            )
+          {
+            return polkit.Result.YES;
+          }
+        })
+      '';
+    };
+  };
+}
