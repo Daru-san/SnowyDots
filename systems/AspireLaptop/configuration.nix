038a2c761133ba288697a04fd800f34138841ab9
Daru-san <mica.tui@proton.me>
2024-03-29 16:30:21 +0200

    system/laptop: add ntopng service

--- systems/AspireLaptop/configuration.nix
+++ systems/AspireLaptop/configuration.nix
@@ -42,12 +42,15 @@
     };
   };
 
-  services.syncthing = {
-    enable = true;
-    dataDir = "/home/daru";
-    user = "daru";
-    configDir = "/home/daru/.sync";
-    settings = {gui = {theme = "black";};};
+  services = {
+    syncthing = {
+      enable = true;
+      dataDir = "/home/daru";
+      user = "daru";
+      configDir = "/home/daru/.sync";
+      settings = {gui = {theme = "black";};};
+    };
+    ntopng.enable = true;
   };
 
   environment.systemPackages = with pkgs;
