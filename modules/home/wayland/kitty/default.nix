8efe55a18d6cf0457608074314614547d1a7dc77
Daru-san <mica.tui@proton.me>
2024-04-01 18:36:13 +0200

    wayland/kitty: update tab bar

--- modules/home/wayland/kitty/default.nix
+++ modules/home/wayland/kitty/default.nix
@@ -24,16 +24,19 @@
 
     #Settings
     settings = {
+      tab_title_template = "{fmt.fg._5c6370}{fmt.bg.default}{fmt.fg._abb2bf}{fmt.bg._5c6370}{index}: {title[:15] + (title[15:] and '…')}{fmt.fg._5c6370}{fmt.bg.default} ";
+      active_tab_title_template = "{fmt.fg._246993}{fmt.bg.default}{fmt.fg._282c34}{fmt.bg._246993}{index}: {title[:40] + (title[40:] and '…')}{fmt.fg._246993}{fmt.bg.default} ";
       tab_bar_edge = "top";
-      tab_bar_style = "powerline";
-      tab_powerline_style = "round";
-      tab_activity_symbol = "󰗖 ";
+      tab_bar_style = "separator";
+      tab_bar_min_tabs = 1;
+      tab_separator = ''""'';
+      tab_activity_symbol = ''""'';
       active_tab_font_style = "bold";
       inactive_tab_font_style = "italics";
       input_delay = 1;
       repaint_delay = 7;
-      window_margin_width = 8;
-      window_margin_height = 6;
+      window_margin_width = 9;
+      window_margin_height = 9;
       term = "kitty";
       placement_strategy = "center";
 
@@ -50,9 +53,9 @@
 
       #Scrollback pager
       scrollback_lines = 100000;
-      scrollback_pager = lib.getExe (with pkgs;
-        writeShellScriptBin "kitty-scroll" ''
-          nvim --noplugin -c "set signcolumn=no showtabline=0" -c "silent! write! /tmp/kitty_scrollback_buffer | te cat /tmp/kitty_scrollback_buffer - "
+      scrollback_pager = with lib;
+        getExe (pkgs.writeShellScriptBin "kitty-scroll" ''
+          ${getExe config.programs.snowvim.package} --noplugin -c "set signcolumn=no showtabline=0" -c "silent! write! /tmp/kitty_scrollback_buffer | te cat /tmp/kitty_scrollback_buffer - "
         '');
     };
     keybindings = let
