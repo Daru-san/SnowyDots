6fa094e63b01766ac3ba409311e71f1a930034a9
daru <mica.tui@proton.me>
2024-04-28 15:42:50 +0200

    wlsunset: remove extra module configs

--- modules/home/desktop/wayland/share/wlsunset/default.nix
+++ modules/home/desktop/wayland/share/wlsunset/default.nix
@@ -1,13 +1,7 @@
 {
-  imports = [./wlsunset.nix];
   services.wlsunset = {
     gamma = "0.9";
     latitude = "25.99";
     longitude = "28.13";
-  };
-  services.wlsunset-test = {
-    enable = true;
-    latitude = "24.2";
-    longitude = "15.7";
   };
 }
