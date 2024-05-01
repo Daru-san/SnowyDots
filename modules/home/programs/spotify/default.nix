a99ba3e9345379d3e7250bd3a3f93d5ab45b23b4
daru <mica.tui@proton.me>
2024-05-01 08:04:49 +0200

    programs/spicetify: clean nix file

--- modules/home/programs/spotify/default.nix
+++ modules/home/programs/spotify/default.nix
@@ -2,20 +2,16 @@
   pkgs,
   system,
   inputs,
-  lib,
   ...
 }: let
   spicePkgs = inputs.spicetify-nix.packages.${system}.default;
 in {
   imports = with inputs.spicetify-nix.homeManagerModules; [spicetify ./spotify-player.nix];
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) ["spotify"];
   programs.spicetify = {
     enable = true;
-    spotifyPackage = pkgs.spotify;
     theme = spicePkgs.themes.text;
-    enabledExtensions = with spicePkgs.extensions; [
-      fullAppDisplayMod
+    tyenabledExtensions = with spicePkgs.extensions; [
+      fullAppDisplay
       powerBar
       bookmark
       autoVolume
@@ -28,20 +24,19 @@ in {
       shuffle
       playlistIcons
     ];
-    enabledCustomApps = with pkgs; let
+    enabledCustomApps = let
       better-library = pkgs.fetchzip {
         url = "https://github.com/Sowgro/betterLibrary/archive/refs/tags/3.0.zip";
         hash = "sha256-8xdhNvL2yF9bFzEH4JUAcOxexdoif9czpUHI3K7cPWk=";
       };
     in
-      with spicePkgs.apps; [
-        new-releases
-        lyrics-plus
+      [
         {
           name = "betterLibrary";
           src = better-library + "/CustomApps/betterLibrary";
           appendName = false;
         }
-      ];
+      ]
+      ++ (with spicePkgs.apps; [new-releases lyrics-plus]);
   };
 }
