bd84e870c3ab62619d3011e87b750e52ffe48dde
daru <mica.tui@proton.me>
2024-05-02 18:37:34 +0200

    programs/mpv: update configuration

--- modules/home/programs/mpv/default.nix
+++ modules/home/programs/mpv/default.nix
@@ -5,18 +5,40 @@
     config = {
       hwdec = "auto-safe";
       vo = "gpu";
+      gpu-api = "vulkan";
+      taskbar-progress = false;
+      keep-open = "always";
       profile = "gpu-hq";
       gpu-context = "wayland";
       force-window = true;
+      save-position-on-quit = true;
+      deband = false;
+      osd-bar = false;
+      border = false;
+      osc = false;
+      osd-font = "NotoMono Nerd Font";
+      sub-auto = "fuzzy";
     };
-
+    scriptOpts = {
+      osc = {
+        scalewindowed = 0.8;
+        hidetimeout = 300;
+        showonpause = false;
+      };
+      thumbfast = {
+        hwdec = true;
+        network = true;
+        spawn_first = true;
+        max_height = 250;
+        max_width = 250;
+      };
+    };
     scripts = with pkgs.mpvScripts; [
       sponsorblock
-      convert
-      uosc
+      modernx
+      mpv-cheatsheet
       quality-menu
-      mpris
-      autocrop
+      quack
       thumbfast
     ];
   };
