a05e9021b3681a09d3e8c445c1d0ab256bd37b11
daru <mica.tui@proton.me>
2024-05-05 14:11:55 +0200

    wayland/hypridle: update config

--- modules/home/desktop/wayland/share/hypridle/default.nix
+++ modules/home/desktop/wayland/share/hypridle/default.nix
@@ -7,27 +7,31 @@
 }: let
   inherit (lib) getExe getExe' mkIf;
   cfg = config.wayland;
-  lockscreen =
-    if (cfg.compositor == "hyprland")
-    then (getExe config.programs.hyprlock.package)
-    else (getExe pkgs.gtklock);
-  beforeSleepCmd = getExe (pkgs.writeShellScriptBin "sleep" ''
+  hyprlock = getExe config.programs.hyprlock.package;
+  pausemusic = getExe (pkgs.writeShellScriptBin "sleep" ''
     ${getExe' config.services.playerctld.package "playerctl"} pause
-    ${lockscreen}
   '');
 in {
   services.hypridle = mkIf cfg.enable {
+    general = {
+      before_sleep_cmd = "${pausemusic} && loginctl lock-session";
+      lock_cmd = ''pidof hyprlock || ${hyprlock}'';
+      after_sleep_cmd = "hyprctl dispatch dpms on";
+    };
     listeners = [
       {
-        timeout = 1200;
-        onTimeout = lockscreen;
+        timeout = 300;
+        on-timeout = "loginctl lock-session";
       }
       {
-        timeout = 1800;
-        onTimeout = "systemctl suspend";
+        timeout = 600;
+        on-timeout = "hyprctl dispatch dpms off";
+        on-resume = "hyprctl dispatch dpms on";
+      }
+      {
+        timeout = 1200;
+        on-timeout = "systemctl suspend";
       }
     ];
-    lockCmd = lockscreen;
-    inherit beforeSleepCmd;
   };
 }
