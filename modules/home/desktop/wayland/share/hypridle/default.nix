6324570aeafe99e3306cbcafb462a470e5d93c73
daru <mica.tui@proton.me>
2024-05-05 14:52:48 +0200

    programs/hypridle: fix config errors

--- modules/home/desktop/wayland/share/hypridle/default.nix
+++ modules/home/desktop/wayland/share/hypridle/default.nix
@@ -1,37 +1,37 @@
 {
-  inputs,
   lib,
   config,
   pkgs,
   ...
 }: let
-  inherit (lib) getExe getExe' mkIf;
-  cfg = config.wayland;
+  inherit (lib) getExe getExe';
   hyprlock = getExe config.programs.hyprlock.package;
   pausemusic = getExe (pkgs.writeShellScriptBin "sleep" ''
     ${getExe' config.services.playerctld.package "playerctl"} pause
   '');
 in {
-  services.hypridle = mkIf cfg.enable {
-    general = {
-      before_sleep_cmd = "${pausemusic} && loginctl lock-session";
-      lock_cmd = ''pidof hyprlock || ${hyprlock}'';
-      after_sleep_cmd = "hyprctl dispatch dpms on";
+  services.hypridle = {
+    settings = {
+      general = {
+        before_sleep_cmd = "${pausemusic} && loginctl lock-session";
+        lock_cmd = ''pidof hyprlock || ${hyprlock}'';
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+      listeners = [
+        {
+          timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 600;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          timeout = 1200;
+          on-timeout = "systemctl suspend";
+        }
+      ];
     };
-    listeners = [
-      {
-        timeout = 300;
-        on-timeout = "loginctl lock-session";
-      }
-      {
-        timeout = 600;
-        on-timeout = "hyprctl dispatch dpms off";
-        on-resume = "hyprctl dispatch dpms on";
-      }
-      {
-        timeout = 1200;
-        on-timeout = "systemctl suspend";
-      }
-    ];
   };
 }
