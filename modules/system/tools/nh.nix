4629b25f894eb23d6f810d27f32688f1ee64ead7
daru <mica.tui@proton.me>
2024-05-05 14:14:57 +0200

    modules/nh: fix error

--- modules/system/tools/nh.nix
+++ modules/system/tools/nh.nix
@@ -3,7 +3,7 @@
     enable = true;
     clean = {
       enable = true;
-      extraArgs = "all -K 3d -k 3";
+      extraArgs = "-K 3d -k 3";
       dates = "03:50";
     };
     flake = "/home/daru/snow/";
