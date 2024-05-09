ae4304d5d923912e64c511e322d4ca58c2daf8d9
daru <mica.tui@proton.me>
2024-05-09 18:39:05 +0200

    hyprland/plugins: remove hyprfocus configuration

--- modules/home/desktop/wayland/hyprland/config/plugins.nix
+++ modules/home/desktop/wayland/hyprland/config/plugins.nix
@@ -31,22 +31,6 @@
           hight_of_titlebar = 0
           show_special = 0
         }
-        # hyprfocus {
-        #   enabled = yes
-        #   keyboard_focus_animation = flash
-        #   mouse_focus_animation = flash
-        #   bezier = bezIn, 0.5,0.0,1.0,0.5
-        #   bezier = bezOut, 0.0,0.5,0.5,1.0
-        #   flash {
-        #     flash_opacity = 0.7
-        #
-        #     in_bezier = bezIn
-        #     in_speed = 0.5
-        #
-        #     out_bezier = bezOut
-        #     out_speed = 3
-        #   }
-        # }
       }
     '';
   };
