37f38ab39a1a11e53976e1d3aa2123c17321c0fa
daru <mica.tui@proton.me>
2024-05-14 20:36:28 +0200

    home/env: add qmmp music player

--- home/daru/packages.nix
+++ home/daru/packages.nix
@@ -51,6 +51,7 @@
       du-dust
       fzf
       musikcube
+      qmmp
       mediainfo
       unrar
       onefetch
--- modules/home/xdg/mimetypes.nix
+++ modules/home/xdg/mimetypes.nix
@@ -3,5 +3,6 @@
     "application/pdf" = ["org.pwmt.zathura-pdf-mupdf.desktop"];
     "image/*" = ["oculante.desktop"];
     "video/*" = ["mpv.desktop"];
+    "audio/*" = ["qmmp.desktop"];
   };
 }
