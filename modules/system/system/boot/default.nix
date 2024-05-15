1d9398aca9786b588c3735e7858b744519761857
daru <mica.tui@proton.me>
2024-05-15 20:23:32 +0200

    system/boot: migrate to systemd-boot

--- modules/system/system/boot/default.nix
+++ modules/system/system/boot/default.nix
@@ -1,52 +1,14 @@
-{
-  pkgs,
-  config,
-  lib,
-  system,
-  inputs,
-  ...
-}: let
-  inherit (lib) mkEnableOption mkMerge mkIf;
-  cfg = config.os.boot;
-in {
-  options = {
-    os.boot = {
-      enableEfiGrub = mkEnableOption "Enable the grub config for EFI";
+{pkgs, ...}: {
+  boot = {
+    consoleLogLevel = 0;
+    kernelParams = ["quiet"];
+    kernelPackages = pkgs.linuxPackages_xanmod_stable;
+    initrd.verbose = false;
+    initrd.systemd.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    loader.systemd-boot = {
+      enable = true;
+      configurationLimit = 7;
     };
   };
-  config.boot = mkMerge [
-    {
-      consoleLogLevel = 0;
-      kernelParams = ["quiet"];
-      kernelPackages = pkgs.linuxPackages_xanmod_stable;
-      initrd.verbose = false;
-      loader.grub = {
-        enable = true;
-        configurationLimit = 3;
-        default = "saved";
-        useOSProber = true;
-        timeoutStyle = "menu";
-      };
-      plymouth = rec {
-        enable = true;
-        theme = "blockchain";
-        themePackages = [
-          (pkgs.adi1090x-plymouth-themes.override {
-            selected_themes = [theme];
-          })
-        ];
-      };
-      initrd.systemd.enable = true;
-    }
-    (mkIf cfg.enableEfiGrub {
-      loader = {
-        efi.canTouchEfiVariables = true;
-        grub = {
-          device = "nodev";
-          theme = inputs.grub-themes.packages.${system}.hyperfluent;
-          efiSupport = true;
-        };
-      };
-    })
-  ];
 }
