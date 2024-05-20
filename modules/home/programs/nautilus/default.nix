5280833d017a2f5c6105f1f12f236ed5740d4206
daru <mica.tui@proton.me>
2024-05-20 18:29:10 +0200

    Revert "programs/nautilus: add gstreamer plugins; add git icons"
    This reverts commit c3fcb40190425f32c2c9eb80cdd146e9d0733b1c.

--- modules/home/programs/nautilus/default.nix
+++ modules/home/programs/nautilus/default.nix
@@ -6,7 +6,6 @@
       gnome.nautilus
       gnome.nautilus-python
       nautilus-open-any-terminal
-      git-nautilus-icons
     ];
   };
 in {
@@ -14,19 +13,6 @@ in {
     packages = [nautEnv];
     sessionVariables.NAUTILUS_4_EXTENSION_DIR = "${nautEnv}/lib/nautilus/extensions-4";
   };
+
   dconf.settings."com/github/stunkymonkey/nautilus-open-any-terminal".terminal = "kitty";
-  nixpkgs.overlays = [
-    (self: super: {
-      gnome = super.gnome.overrideScope' (gself: gsuper: {
-        nautilus = gsuper.nautilus.overrideAttrs (nsuper: {
-          buildInputs =
-            nsuper.buildInputs
-            ++ (with super.gst_all_1; [
-              gst-plugins-good
-              gst-plugins-bad
-            ]);
-        });
-      });
-    })
-  ];
 }
