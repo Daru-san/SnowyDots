17e799106ce5949458290d3ab6b633fde1446e88
daru <mica.tui@proton.me>
2024-05-20 17:54:14 +0200

    programs/nautilus: add gstreamer plugins; add git icons

--- modules/home/programs/nautilus/default.nix
+++ modules/home/programs/nautilus/default.nix
@@ -6,6 +6,7 @@
       gnome.nautilus
       gnome.nautilus-python
       nautilus-open-any-terminal
+      git-nautilus-icons
     ];
   };
 in {
@@ -13,6 +14,19 @@ in {
     packages = [nautEnv];
     sessionVariables.NAUTILUS_4_EXTENSION_DIR = "${nautEnv}/lib/nautilus/extensions-4";
   };
-
   dconf.settings."com/github/stunkymonkey/nautilus-open-any-terminal".terminal = "kitty";
+  nixpkgs.overlays = [
+    (self: super: {
+      gnome = super.gnome.overrideScope' (gself: gsuper: {
+        nautilus = gsuper.nautilus.overrideAttrs (nsuper: {
+          buildInputs =
+            nsuper.buildInputs
+            ++ (with super.gst_all_1; [
+              gst-plugins-good
+              gst-plugins-bad
+            ]);
+        });
+      });
+    })
+  ];
 }
