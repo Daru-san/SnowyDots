9463ea3dee451cd70d556bdc62b69d11dba195be
daru <mica.tui@proton.me>
2024-05-21 09:27:39 +0200

    system/ssh: update ksshaskpass to qt6

--- modules/nixos/tools/ssh.nix
+++ modules/nixos/tools/ssh.nix
@@ -2,15 +2,14 @@
   pkgs,
   lib,
   ...
-}:
-with lib; {
-  services.openssh = mkDefault {
+}: {
+  services.openssh = lib.mkDefault {
     enable = true;
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
   };
   programs.ssh = {
     enableAskPassword = true;
-    askPassword = "${pkgs.libsForQt5.ksshaskpass}/bin/ksshaskpass";
+    askPassword = "${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass";
   };
 }
