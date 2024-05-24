16454eea20b56594b19cc8b2aee48a908e8472d9
daru <mica.tui@proton.me>
2024-05-24 15:20:34 +0200

    programs/spotify: fix hyprland class issues

--- modules/home/programs/spotify/default.nix
+++ modules/home/programs/spotify/default.nix
@@ -9,6 +9,7 @@ in {
   programs.spicetify = {
     enable = true;
     theme = spicePkgs.themes.text;
+    windowManagerPatch = true;
     enabledExtensions = with spicePkgs.extensions; [
       fullAppDisplay
       powerBar
