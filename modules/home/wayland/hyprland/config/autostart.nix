b049c45ba1362ebda36dc73295eda752a88725d6
daru <mica.tui@proton.me>
2024-05-28 15:23:17 +0200

    wayland/matcha: start with matcha off
    Rather have the idle inhibitor off when the entering Hyprland and toggle
    it on when I need it to be on

--- modules/home/wayland/hyprland/config/autostart.nix
+++ modules/home/wayland/hyprland/config/autostart.nix
@@ -10,7 +10,7 @@
       "[workspace name:1] ${config.programs.kitty.package}/bin/kitty -T Terminal"
       "${config.programs.waybar.package}/bin/waybar"
       "${pkgs.copyq}/bin/copyq"
-      "${inputs.matcha.packages.${system}.default}/bin/matcha -d"
+      "${inputs.matcha.packages.${system}.default}/bin/matcha -d --off"
     ];
     exec = [
       "systemctl --user restart kanshi.service"
