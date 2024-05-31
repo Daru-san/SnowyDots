c2458fa9cc65025385b4eae5538e542cde787003
daru <mica.tui@proton.me>
2024-05-31 21:28:21 +0200

    xdg/desktop-entries: fix swayimg mimetypes

--- modules/home/xdg/desktop-entries.nix
+++ modules/home/xdg/desktop-entries.nix
@@ -1,9 +1,31 @@
 {
   swayimg = {
     name = "swayimg";
-    exec = "swayimg";
-    mimeType = ["image/*"];
+    exec = "swayimg %U";
     noDisplay = true;
     type = "Application";
+    mimeType = [
+      "image/bmp"
+      "image/gif"
+      "image/vnd.microsoft.icon"
+      "image/jpeg"
+      "image/png"
+      "image/pnm"
+      "image/avif"
+      "image/tiff"
+      "image/webp"
+      "image/svg+xml"
+      "image/exr"
+      "image/x-dcraw"
+      "image/x-nikon-nef"
+      "image/x-canon-cr2"
+      "image/x-adobe-dng"
+      "image/x-epson-erf"
+      "image/x-fuji-raf"
+      "image/x-sony-arw"
+      "image/x-sony-srf"
+      "image/x-sony-sr2"
+      "image/x-panasonic-raw"
+    ];
   };
 }
