8cca44d03f28ac8f05be6ecce315ef2f2192ffc8
daru <mica.tui@proton.me>
2024-05-31 19:39:48 +0200

    programs/yazi: use home-manager options for plugins and themes
    This change came to my surprise but the timing was phenominal

--- modules/home/programs/yazi/plugins.nix
+++ modules/home/programs/yazi/plugins.nix
@@ -1,19 +1,17 @@
 let
   yazi-plugins = import ./nix/sources.nix;
   getPlugin = x: yazi-plugins.${x}.outPath;
-  setPlugin = x: "yazi/plugins/${x}.yazi";
 in {
-  xdg.configFile = {
-    "yazi/init.lua".source = ./lua/init.lua;
-    "yazi/theme.toml".source = "${getPlugin "tokyo-night.yazi"}/flavor.toml";
-    "${setPlugin "smart-enter"}/init.lua".source = ./lua/smart-enter.lua;
-    ${setPlugin "exifaudio"}.source = getPlugin "exifaudio.yazi";
-    ${setPlugin "glow"}.source = getPlugin "glow.yazi";
-    ${setPlugin "mime"}.source = getPlugin "mime.yazi";
-    ${setPlugin "bat"}.source = getPlugin "yazi-plugin-bat";
-    ${setPlugin "fg"}.source = getPlugin "fg.yazi";
-    ${setPlugin "ouch"}.source = getPlugin "ouch.yazi";
-    ${setPlugin "eza-preview"}.source = getPlugin "eza-preview.yazi";
-    ${setPlugin "torrent-preview"}.source = getPlugin "torrent-preview.yazi";
+  programs.yazi.flavors.tokyonight = "tokyo-night.yazi";
+  programs.yazi.plugins = {
+    smart-enter = ./smart-enter;
+    exifaudio = getPlugin "exifaudio.yazi";
+    glow = getPlugin "glow.yazi";
+    mime = getPlugin "mime.yazi";
+    bat = getPlugin "yazi-plugin-bat";
+    fg = getPlugin "fg.yazi";
+    ouch = getPlugin "ouch.yazi";
+    eza-preview = getPlugin "eza-preview.yazi";
+    torrent-preview = getPlugin "torrent-preview.yazi";
   };
 }
