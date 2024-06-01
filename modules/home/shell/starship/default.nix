e151ad0274c669b6b138f4356fe3115844840a87
daru <mica.tui@proton.me>
2024-06-01 17:56:30 +0200

    shell/starship: add config

--- modules/home/shell/starship/default.nix
+++ modules/home/shell/starship/default.nix
@@ -1,9 +1,48 @@
-{
-  programs.startship = {
+{lib, ...}: {
+  programs.starship = {
     enable = true;
     enableZshIntegration = true;
     enableBashIntegration = true;
     enableNushellIntegration = true;
     enableFishIntegration = true;
+    settings = {
+      add_newline = false;
+      format = lib.concatStrings [
+        "directory"
+        "git_branch"
+        "git_commit"
+        "git_state"
+        "git_metrics"
+        "git_status"
+        "java"
+        "nix_shell"
+        "sudo"
+        "character"
+        "shell"
+      ];
+      right_format = lib.concatStrings [
+        "$cmd_duration"
+      ];
+      java = {
+        format = ''java:[(''${version} )]($style)'';
+        symbol = "";
+      };
+      git_branch = {
+        format = ''git:([$branch(:$remote_branch)])($style)'';
+        symbol = "";
+      };
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+      package.disabled = true;
+
+      nix_shell = {
+        impure_msg = "[impure shell](bold red)";
+        pure_msg = "[pure shell](bold green)";
+        unknown_msg = "[unknown shell](bold yellow)";
+        format = "nix:([$state( \($name\))])(bold blue) ";
+      };
+    };
   };
 }
