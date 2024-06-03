29e6fd10b8819a62d123c48e6c55bcda5dcf8835
daru <mica.tui@proton.me>
2024-06-03 07:33:40 +0200

    system/hardware: add tuxclocker and cpupower-gui tools
    Some useful tools to help me in my hardware monitoring

--- modules/nixos/system/hardware.nix
+++ modules/nixos/system/hardware.nix
@@ -1,8 +1,10 @@
 {
+  programs.tuxclocker.enable = true;
   services = {
     fstrim.enable = true;
     earlyoom.enable = true;
     thermald.enable = true;
+    cpupower-gui.enable = true;
   };
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="[sv]d[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
