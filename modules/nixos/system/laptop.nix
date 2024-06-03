627c6d4474e619ee9b5f8691d62783bc5b955c3d
daru <mica.tui@proton.me>
2024-06-03 07:22:02 +0200

    system/laptop: scale to performance on ac

--- modules/nixos/system/laptop.nix
+++ modules/nixos/system/laptop.nix
@@ -17,13 +17,14 @@
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
       PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "balanced";
-      CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      RUNTIME_PM_ON_AC = "auto";
+      RUNTIME_PM_ON_AC = "on";
       RUNTIME_PM_ON_BAT = "auto";
-      INTEL_GPU_MIN_FREQ_ON_AC = "600";
+      INTEL_GPU_MIN_FREQ_ON_AC = "650";
       INTEL_GPU_MIN_FREQ_ON_BAT = "300";
       USB_AUTOSUSPEND = 0;
+      NMI_WATCHDOG = 0;
     };
   };
   boot.kernelParams = ["snd-intel-dspcfg.dsp_driver=1"];
