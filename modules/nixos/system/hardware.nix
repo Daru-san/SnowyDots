27cff0a773f0c74431d6e5de912d2edfda9b3249
daru <mica.tui@proton.me>
2024-06-03 08:27:51 +0200

    system: undervolting laptop
    Temps actually went down!

--- modules/nixos/system/hardware.nix
+++ modules/nixos/system/hardware.nix
@@ -9,6 +9,21 @@
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="[sv]d[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
   '';
+  services.undervolt = {
+    enable = true;
+    tempAc = 71;
+    tempBat = 55;
+    turbo = 0;
+    useTimer = true;
+    p1 = {
+      window = 10;
+      limit = 5;
+    };
+    p2 = {
+      window = 30;
+      limit = 6;
+    };
+  };
   zramSwap = {
     enable = true;
     memoryPercent = 200;
