e0d8e261a11b530c552a9ef33ce6356608deadcd
Daru <mica.tui@proton.me>
2024-06-07 21:33:57 +0200

    system/laptop: move undervolt config to laptop module

--- modules/nixos/system/hardware.nix
+++ modules/nixos/system/hardware.nix
@@ -13,21 +13,6 @@
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="[sv]d[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
   '';
-  services.undervolt = {
-    enable = false;
-    tempAc = 77;
-    tempBat = 64;
-    turbo = 0;
-    useTimer = true;
-    p1 = {
-      window = 10;
-      limit = 5;
-    };
-    p2 = {
-      window = 30;
-      limit = 6;
-    };
-  };
   zramSwap = {
     enable = true;
     memoryPercent = 200;
--- modules/nixos/system/laptop.nix
+++ modules/nixos/system/laptop.nix
@@ -8,34 +8,51 @@
 in {
   options.system.laptop = mkEnableOption "Enable laptop configuration";
   config = mkIf cfg {
-    services.upower = {
-      enable = true;
-      percentageCritical = 20;
-      percentageLow = 30;
-      percentageAction = 15;
-      criticalPowerAction = "HybridSleep";
-      ignoreLid = true;
-    };
-    powerManagement.powertop.enable = true;
-    services.tlp = {
-      enable = true;
-      settings = {
-        CPU_DRIVER_OPMODE_ON_AC = "active";
-        CPU_DRIVER_OPMODE_ON_BAT = "passive";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-        PLATFORM_PROFILE_ON_AC = "performance";
-        PLATFORM_PROFILE_ON_BAT = "balanced";
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        RUNTIME_PM_ON_AC = "on";
-        RUNTIME_PM_ON_BAT = "auto";
-        INTEL_GPU_MIN_FREQ_ON_AC = "650";
-        INTEL_GPU_MIN_FREQ_ON_BAT = "300";
-        USB_AUTOSUSPEND = 0;
-        NMI_WATCHDOG = 0;
+    services = {
+      upower = {
+        enable = true;
+        percentageCritical = 20;
+        percentageLow = 30;
+        percentageAction = 15;
+        criticalPowerAction = "HybridSleep";
+        ignoreLid = true;
+      };
+      tlp = {
+        enable = true;
+        settings = {
+          CPU_DRIVER_OPMODE_ON_AC = "active";
+          CPU_DRIVER_OPMODE_ON_BAT = "passive";
+          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+          PLATFORM_PROFILE_ON_AC = "performance";
+          PLATFORM_PROFILE_ON_BAT = "balanced";
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
+          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+          RUNTIME_PM_ON_AC = "on";
+          RUNTIME_PM_ON_BAT = "auto";
+          INTEL_GPU_MIN_FREQ_ON_AC = "650";
+          INTEL_GPU_MIN_FREQ_ON_BAT = "300";
+          USB_AUTOSUSPEND = 0;
+          NMI_WATCHDOG = 0;
+        };
+      };
+      undervolt = {
+        enable = true;
+        tempAc = 77;
+        tempBat = 64;
+        turbo = 0;
+        useTimer = true;
+        p1 = {
+          window = 10;
+          limit = 5;
+        };
+        p2 = {
+          window = 30;
+          limit = 6;
+        };
       };
     };
+    powerManagement.powertop.enable = true;
     boot.kernelParams = ["snd-intel-dspcfg.dsp_driver=1"];
   };
 }
