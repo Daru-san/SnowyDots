007610fbd78516774cfa31385b7c0ffa8e4f2c45
Daru <mica.tui@proton.me>
2024-06-07 09:57:55 +0200

    home/services: add udiskie service

--- modules/home/services/common.nix
+++ modules/home/services/common.nix
@@ -3,6 +3,7 @@
     playerctld.enable = true;
     easyeffects.enable = true;
     kdeconnect.enable = true;
+    udiskie.enable = true;
     syncthing = {
       enable = true;
       tray.enable = true;
