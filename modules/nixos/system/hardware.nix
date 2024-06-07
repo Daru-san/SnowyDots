ed83b1dd53dd2597ca04db025df6213f5d039c25
Daru <mica.tui@proton.me>
2024-06-07 21:34:14 +0200

    system/hardware: disable cpupower-gui

--- modules/nixos/system/hardware.nix
+++ modules/nixos/system/hardware.nix
@@ -2,13 +2,12 @@
   environment.systemPackages = with pkgs; [
     linuxPackages.cpupower
   ];
-  services.fwupd.enable = true;
   programs.tuxclocker.enable = true;
   services = {
+    fwupd.enable = true;
     fstrim.enable = true;
     earlyoom.enable = true;
     thermald.enable = true;
-    cpupower-gui.enable = true;
   };
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="[sv]d[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
