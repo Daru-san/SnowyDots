9ee5225d116d36ea6088a859fcff19e9d0188c40
Daru <mica.tui@proton.me>
2024-06-09 18:28:09 +0200

    wayland/kitty: simplify configuration

--- modules/home/wayland/common/kitty/default.nix
+++ modules/home/wayland/common/kitty/default.nix
@@ -24,17 +24,10 @@
 
     #Settings
     settings = {
-      tab_title_template = " {fmt.fg._5c6370}{fmt.bg.default}{fmt.fg._abb2bf}{fmt.bg._5c6370}{index}: {title[:15] + (title[15:] and '…')}{fmt.fg._5c6370}{fmt.bg.default} ";
-      active_tab_title_template = " {fmt.fg._287373}{fmt.bg.default}{fmt.fg._282c34}{fmt.bg._287373}{index}: {title[:40] + (title[40:] and '…')}{fmt.fg._287373}{fmt.bg.default} ";
-      tab_bar_edge = "top";
-      tab_bar_style = "separator";
-      tab_bar_min_tabs = 1;
-      tab_separator = ''""'';
-      tab_activity_symbol = ''""'';
       active_tab_font_style = "bold";
       inactive_tab_font_style = "italics";
-      input_delay = 1;
-      repaint_delay = 7;
+      input_delay = 0;
+      repaint_delay = 4;
       window_margin_width = 9;
       window_margin_height = 9;
       term = "kitty";
@@ -42,7 +35,7 @@
 
       hide_window_decorations = true;
 
-      background_opacity = "0";
+      background_opacity = "100";
       dynamic_background_opacity = true;
 
       sync_to_monitor = true;
@@ -60,38 +53,12 @@
     keybindings = let
       c = "ctrl";
       a = "alt";
-      g = "goto_tab";
+      s = "shift";
     in {
       "${c}+c" = "copy_or_interrupt";
-      "${c}+${a}+1" = "${g} 1";
-      "${c}+${a}+2" = "${g} 2";
-      "${c}+${a}+3" = "${g} 3";
-      "${c}+${a}+4" = "${g} 4";
-      "${c}+${a}+5" = "${g} 5";
-      "${c}+${a}+6" = "${g} 6";
-      "${c}+${a}+7" = "${g} 7";
-      "${c}+${a}+8" = "${g} 8";
-      "${c}+${a}+9" = "${g} 9";
       "${c}+${a}+minus" = "set_background_opacity -0.1";
       "${c}+${a}+plus" = "set_background_opacity +0.1";
+      "${c}+${s}+t" = "no_op";
     };
   };
-
-  #Autostart session files
-  home.file.".config/kitty/git.conf".text = ''
-    title SnowyDots
-
-    new_tab LazyFlake
-    cd ~/snow
-    launch lazygit
-
-    new_tab ChilledEditor
-    cd ~/snow
-    launch vi
-  '';
-  xdg.configFile."kitty/musikcube.conf".text = ''
-    new_tab musikcube
-    title musikcube
-    launch musikcube
-  '';
 }
