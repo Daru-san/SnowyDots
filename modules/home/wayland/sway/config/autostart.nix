485845aea1dd1fde151d9b2341d61c166a1c0643
Daru <mica.tui@proton.me>
2024-06-10 10:12:57 +0200

    wayland/sway/autostart: update startup programs

--- modules/home/wayland/sway/config/autostart.nix
+++ modules/home/wayland/sway/config/autostart.nix
@@ -1,18 +1,16 @@
 {
   pkgs,
-  config,
+  lib,
   ...
 }: {
   wayland.windowManager.sway.config.startup = [
-    {command = "systemctl --user start swaync";}
-    {command = "${config.services.copyq.package}/bin/copyq";}
-    {command = "${pkgs.autotiling-rs}/bin/autotiling-rs";}
+    {command = lib.getExe pkgs.kitty;}
+    {command = lib.getExe pkgs.copyq;}
+    {command = lib.getExe pkgs.autotiling-rs;}
+    {command = lib.getExe pkgs.sway-assign-cgroups;}
     {
       command = "systemctl --user restart kanshi";
       always = true;
     }
-    {command = "systemctl --user start hypridle";}
-    {command = "systemctl --user start swayosd";}
-    {command = "systemctl --user start wlsunset";}
   ];
 }
