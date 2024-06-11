01a0954b99ba2e87a728d1b7a1a34164cb22bca9
Daru <mica.tui@proton.me>
2024-06-11 15:25:08 +0200

    virtualisation: attempt fix gpu passthrough

--- modules/nixos/tools/virtualization.nix
+++ modules/nixos/tools/virtualization.nix
@@ -13,10 +13,17 @@
     gnome.adwaita-icon-theme
   ];
   boot.kernelModules = ["kvm-intel"];
+  boot.extraModprobeConfig = "options i915 enable_guc=2";
   users.users.daru.extraGroups = ["libvirtd"];
   programs.virt-manager.enable = true;
   services.spice-vdagentd.enable = true;
   virtualisation = {
+    kvmgt.enable = true;
+    kvmgt.vgpus = {
+      i915-GVTg_V5_4 = {
+        uuid = ["6ecf0e14-27aa-11ef-abcb-77e6eb5999bc"];
+      };
+    };
     spiceUSBRedirection.enable = true;
     libvirtd = {
       enable = true;
