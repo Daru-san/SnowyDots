1e6190d2c4a384b79aaba2324a89038d2c423deb
Daru <mica.tui@proton.me>
2024-06-11 15:27:12 +0200

    system/graphics: update drivers

--- modules/nixos/system/graphics.nix
+++ modules/nixos/system/graphics.nix
@@ -1,10 +1,16 @@
 {pkgs, ...}: {
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override {enableHybridCodec = true;};
+  };
   hardware.opengl = {
     enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-    ];
     driSupport = true;
     driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      libvdpau-va-gl
+    ];
   };
+  environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";};
 }
