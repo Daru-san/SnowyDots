f75f99c27da94e3f61b4350ef097f973212229e8
Daru <mica.tui@proton.me>
2024-06-12 14:31:31 +0200

    wayland/plasma: add rc2nix

--- modules/home/wayland/plasma/defualt.nix
+++ modules/home/wayland/plasma/defualt.nix
@@ -1,4 +1,9 @@
-{inputs, ...}: {
+{
+  inputs,
+  system,
+  ...
+}: {
   imports = [inputs.plasma-manager.homeManagerModules.default];
   programs.plasma.enable = true;
+  home.packages = [inputs.plasma-manager.packages.${system}.rc2nix];
 }
