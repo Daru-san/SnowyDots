6356ac53cd5b242601515dbdd876e242f6344777
Daru <mica.tui@proton.me>
2024-06-14 18:05:14 +0200

    wayland/hyprland: fix xdg portals

--- modules/home/xdg/portal.nix
+++ modules/home/xdg/portal.nix
@@ -1,10 +1,8 @@
 {pkgs, ...}: {
   enable = true;
-  xdgOpenUsePortal = true;
   extraPortals = with pkgs; [
     xdg-desktop-portal-hyprland
-    # xdg-desktop-portal-wlr
-    # xdg-desktop-portal-kde
-    # xdg-desktop-portal-gtk
+    xdg-desktop-portal-kde
+    xdg-desktop-portal-gtk
   ];
 }
