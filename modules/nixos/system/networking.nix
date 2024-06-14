515500439822b9ec029200b521e652f8f134f64e
Daru <mica.tui@proton.me>
2024-06-14 10:54:03 +0200

    system/networking: use networkmanager with iwd
    It seems like this is the best configuration to use, no issues with wifi
    on my network adapter so far

--- modules/nixos/system/networking.nix
+++ modules/nixos/system/networking.nix
@@ -1,15 +1,30 @@
-{
+{pkgs, ...}: {
   networking = {
     nameservers = ["1.1.1.1" "1.0.0.1"];
     networkmanager = {
       enable = true;
       dns = "none";
       wifi = {
-        powersave = false;
+        backend = "iwd";
         macAddress = "random";
+        powersave = false;
+      };
+    };
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        Settings = {
+          AutoConnect = false;
+          Hidden = true;
+          AlwaysRandomizeAddress = true;
+        };
       };
     };
   };
+  environment.systemPackages = [pkgs.iw];
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="net", KERNEL=="wlan*" RUN+="${pkgs.iw}/bin/iw dev %k set power_save off"
+  '';
   networking.firewall = {
     enable = true;
     allowedUDPPorts = [51413];
