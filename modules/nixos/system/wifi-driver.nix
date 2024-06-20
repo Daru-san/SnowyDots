ed011539dedb4ec439c656d6b9038531eace65d7
Daru <mica.tui@proton.me>
2024-06-20 10:03:28 +0200

    system/desktop: enable concurrent mode on wifi adapter

--- modules/nixos/system/wifi-driver.nix
+++ modules/nixos/system/wifi-driver.nix
@@ -1,6 +1,14 @@
 {config, ...}: {
   boot = {
-    extraModulePackages = [config.boot.kernelPackages.rtl88x2bu];
+    extraModulePackages = [
+      (config.boot.kernelPackages.rtl88x2bu.overrideAttrs (old: {
+        prePatch =
+          old.prePatch
+          + ''
+            substituteInPlace Makefile --replace "CONFIG_CONCURRENT_MODE = n" "CONFIG_CONCURRENT_MODE = y"
+          '';
+      }))
+    ];
     kernelModules = ["88x2bu"];
     blacklistedKernelModules = [
       "rtw88_usb"
