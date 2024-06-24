5937f0d322ef147fac98fe89f2ad9db12f77b47c
Daru <mica.tui@proton.me>
2024-06-24 12:07:02 +0200

    system/desktop: remount partitions

--- systems/desktop/hardware-configuration.nix
+++ systems/desktop/hardware-configuration.nix
@@ -1,44 +1,80 @@
-{
-  lib,
-  pkgs,
-  ...
-}: {
-  boot = {
-    kernelParams = ["intel_pstate=passive"];
-    kernelModules = ["coretemp"];
-    initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc"];
-  };
+{lib, ...}: {
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc"];
+
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/30ccfaac-4d6a-4c46-82f6-074a297dddd1";
-      fsType = "ext4";
-      options = ["noatime" "nodiratime"];
+      device = "/dev/disk/by-uuid/38d10da5-6c7c-4eb2-b614-ad20a1a795e8";
+      fsType = "btrfs";
+      options = ["subvol=root" "compress=zstd"];
+    };
+
+    "/etc" = {
+      device = "/dev/disk/by-uuid/38d10da5-6c7c-4eb2-b614-ad20a1a795e8";
+      fsType = "btrfs";
+      options = ["subvol=etc" "compress=zstd"];
+    };
+
+    "/tmp" = {
+      device = "/dev/disk/by-uuid/38d10da5-6c7c-4eb2-b614-ad20a1a795e8";
+      fsType = "btrfs";
+      options = ["subvol=tmp" "compress=zstd"];
+    };
+
+    "/persist" = {
+      device = "/dev/disk/by-uuid/38d10da5-6c7c-4eb2-b614-ad20a1a795e8";
+      fsType = "btrfs";
+      options = ["subvol=persist" "compress=zstd"];
     };
 
     "/nix" = {
-      device = "/dev/disk/by-uuid/620efc74-668f-49a5-b824-9e4e297a3b91";
+      device = "/dev/disk/by-uuid/dbd1df32-d4d5-4e20-8da7-aa8df227c6ac";
       fsType = "ext4";
       options = ["noatime" "nodiratime"];
     };
 
+    "/home" = {
+      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
+      fsType = "btrfs";
+      options = ["subvol=home" "compress=zstd"];
+    };
+
+    "/home/daru/Music" = {
+      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
+      fsType = "btrfs";
+      options = ["subvol=music" "rw" "compress=zstd"];
+    };
+
+    "/home/daru/Documents" = {
+      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
+      fsType = "btrfs";
+      options = ["subvol=documents" "rw" "compress=zstd"];
+    };
+
+    "/home/daru/Development" = {
+      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
+      fsType = "btrfs";
+      options = ["subvol=repos" "rw" "compress=zstd"];
+    };
+
+    "/home/daru/Pictures" = {
+      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
+      fsType = "btrfs";
+      options = ["subvol=pictures" "rw" "compress=zstd"];
+    };
+
     "/boot" = {
-      device = "/dev/disk/by-uuid/EE38-B76E";
+      device = "/dev/disk/by-uuid/7ED4-6E7F";
       fsType = "vfat";
       options = ["fmask=0022" "dmask=0022"];
     };
-
-    "/home" = {
-      device = "/dev/disk/by-uuid/2c99252a-5281-4d2a-ba37-a25e0926b3e7";
-      fsType = "ext4";
-    };
   };
+
   swapDevices = [];
 
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware = {
-    cpu.intel.updateMicrocode = true;
     enableRedistributableFirmware = true;
-    firmware = [pkgs.rtw88-firmware];
+    cpu.intel.updateMicrocode = true;
   };
 }
