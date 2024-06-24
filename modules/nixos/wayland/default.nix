8a95636c696a1e53528f743c4f04dd2485aea759
Daru <mica.tui@proton.me>
2024-06-24 12:06:51 +0200

    system/wayland: switch to hyprland

--- modules/nixos/wayland/default.nix
+++ modules/nixos/wayland/default.nix
@@ -9,18 +9,7 @@
 in {
   options.wayland.enable = mkEnableOption "Enable wayland";
   config = mkIf cfg.enable {
-    programs = {
-      sway = {
-        enable = false;
-        extraPackages = [];
-      };
-      hyprland.enable = true;
-    };
-    xdg.portal.enable = true;
-    services.gnome = {
-      sushi.enable = true;
-      gnome-user-share.enable = true;
-    };
+    programs.hyprland.enable = true;
     programs = {
       dconf.enable = true;
       hyprlock.enable = true;
@@ -37,16 +26,6 @@ in {
               window-padding = 1;
               g = "Hello, Daru";
               c = "Hyprland";
-              theme = lib.concatStringsSep ";" [
-                "border=magenta"
-                "text=gray"
-                "prompt=green"
-                "time=red"
-                "action=blue"
-                "button=yellow"
-                "container=black"
-                "input=red"
-              ];
             };
           in "${pkgs.greetd.tuigreet}/bin/tuigreet ${flags}";
           user = "daru";
