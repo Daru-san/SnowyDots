a483237f2fd55b71f2f65327c3f492ff28d78f0a
Daru <mica.tui@proton.me>
2024-06-24 12:06:14 +0200

    system/networking: switch back to wpa supplicant

--- modules/nixos/system/networking.nix
+++ modules/nixos/system/networking.nix
@@ -5,15 +5,11 @@
       enable = true;
       dns = "none";
       wifi = {
-        backend = "iwd";
         macAddress = "random";
         powersave = false;
       };
     };
   };
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="net", KERNEL=="wlan*" RUN+="${pkgs.iw}/bin/iw dev %k set power_save off"
-  '';
   environment.systemPackages = [pkgs.networkmanagerapplet];
   networking.firewall = {
     enable = true;
