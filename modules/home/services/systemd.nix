c08fae83621f34511ef9e64a88553f26e704cd18
Daru <mica.tui@proton.me>
2024-06-24 12:03:15 +0200

    services: fix rqbit server not starting

--- modules/home/services/systemd.nix
+++ modules/home/services/systemd.nix
@@ -2,10 +2,9 @@
   systemd.user.services = {
     rqbit-server = {
       Service = {
-        Wants = ["network.target"];
         ExecStart = "${pkgs.rqbit}/bin/rqbit server start Downloads/Torrents";
       };
-      Install = {WantedBy = ["multi-user.target"];};
+      Install = {WantedBy = ["default.target"];};
     };
   };
 }
