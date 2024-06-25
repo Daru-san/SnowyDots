025950d095524d894a3a239a83b5f7818caec655
Daru <mica.tui@proton.me>
2024-06-25 13:04:15 +0200

    xdg/portals: attempt to fix kde file picker

--- modules/home/xdg/portal.nix
+++ modules/home/xdg/portal.nix
@@ -2,12 +2,10 @@
   enable = true;
   extraPortals = with pkgs; [
     xdg-desktop-portal-kde
-    xdg-desktop-portal-gtk
   ];
   config = {
     sway = {
-      default = ["wlr" "gtk"];
-      "org.freedesktop.impl.portal.FileChooser" = ["kde"];
+      default = ["wlr" "kde"];
     };
     hyprland = {
       default = ["hyprland" "gtk"];
