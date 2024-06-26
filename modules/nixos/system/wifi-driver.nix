fe333a09a78214e103bc78211db1251217c42b9b
Daru <mica.tui@proton.me>
2024-06-26 21:51:44 +0200

    system/desktop: hopefully fix wifi driver

--- modules/nixos/system/wifi-driver.nix
+++ modules/nixos/system/wifi-driver.nix
@@ -1,10 +1,22 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   boot = {
     extraModprobeConfig = ''
-      options 88x2bu rtw_switch_usb_mode=1 rtw_power_mgnt=0
+      options 88x2bu rtw_power_mgnt=0
     '';
     extraModulePackages = [
-      config.boot.kernelPackages.rtl88x2bu
+      (config.boot.kernelPackages.rtl88x2bu.overrideAttrs
+        (oldAttrs: {
+          src = pkgs.fetchFromGitHub {
+            owner = "cilynx";
+            repo = "rtl88x2bu";
+            rev = "620b1a12c8822ee7d340465fbdc9d5150b193189";
+            hash = "sha256-InpU6HFoa1vPzP88HdQo76i1s6zAHPQ05pzGiaQKgEw=";
+          };
+        }))
     ];
     kernelModules = ["88x2bu"];
     blacklistedKernelModules = [
