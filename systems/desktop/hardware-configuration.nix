0eb851deaf77262fde23c8828771f184281ea635
Daru <mica.tui@proton.me>
2024-07-01 15:33:07 +0200

    system/desktop: remount partitions

--- systems/desktop/hardware-configuration.nix
+++ systems/desktop/hardware-configuration.nix
@@ -11,49 +11,55 @@
     "/etc" = {
       device = "/dev/disk/by-uuid/38d10da5-6c7c-4eb2-b614-ad20a1a795e8";
       fsType = "btrfs";
-      options = ["subvol=etc" "compress=zstd"];
+      options = ["subvol=etc" "compress=zstd" "discard"];
+    };
+
+    "/var" = {
+      device = "/dev/disk/by-uuid/38d10da5-6c7c-4eb2-b614-ad20a1a795e8";
+      fsType = "btrfs";
+      options = ["subvol=tmp" "compress=zstd" "discard"];
     };
 
     "/persist" = {
       device = "/dev/disk/by-uuid/38d10da5-6c7c-4eb2-b614-ad20a1a795e8";
       fsType = "btrfs";
-      options = ["subvol=persist" "compress=zstd"];
+      options = ["subvol=persist" "compress=zstd" "discard"];
     };
 
     "/nix" = {
       device = "/dev/disk/by-uuid/dbd1df32-d4d5-4e20-8da7-aa8df227c6ac";
       fsType = "ext4";
-      options = ["noatime" "nodiratime"];
+      options = ["noatime" "nodiratime" "discard"];
     };
 
     "/home" = {
       device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
       fsType = "btrfs";
-      options = ["subvol=home" "compress=zstd"];
+      options = ["subvol=home" "compress=zstd" "discard"];
     };
 
     "/home/daru/Music" = {
       device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
       fsType = "btrfs";
-      options = ["subvol=music" "rw" "compress=zstd"];
+      options = ["subvol=music" "rw" "compress=zstd" "discard"];
     };
 
     "/home/daru/Documents" = {
       device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
       fsType = "btrfs";
-      options = ["subvol=documents" "rw" "compress=zstd"];
+      options = ["subvol=documents" "rw" "compress=zstd" "discard"];
     };
 
     "/home/daru/Development" = {
       device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
       fsType = "btrfs";
-      options = ["subvol=repos" "rw" "compress=zstd"];
+      options = ["subvol=repos" "rw" "compress=zstd" "discard"];
     };
 
     "/home/daru/Pictures" = {
       device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
       fsType = "btrfs";
-      options = ["subvol=pictures" "rw" "compress=zstd"];
+      options = ["subvol=pictures" "rw" "compress=zstd" "discard"];
     };
 
     "/boot" = {
