6fab8b9b7fad60e88c419f6caab012402de46519
Daru <mica.tui@proton.me>
2024-07-11 20:30:47 +0200

    home/services: add gnome-keyring

--- modules/home/services/common.nix
+++ modules/home/services/common.nix
@@ -8,6 +8,10 @@
     easyeffects.enable = true;
     kdeconnect.enable = true;
     udiskie.enable = true;
+    gnome-keyring = {
+      enable = true;
+      components = ["secrets" "ssh"];
+    };
     syncthing = {
       enable = true;
       tray = {
