a25442387cf0b28bf81194c369df36bbaae963ff
Daru <mica.tui@proton.me>
2024-08-17 14:04:59 +0200

    system/desktop: pin rtl88x2bu

--- modules/nixos/system/wifi-driver.nix
+++ modules/nixos/system/wifi-driver.nix
@@ -1,12 +1,17 @@
 {
+  pkgs,
   config,
-  inputs,
   ...
 }: {
   boot = {
     extraModulePackages = [
       (config.boot.kernelPackages.rtl88x2bu.overrideAttrs (oldAttrs: {
-        src = inputs.rtw-driver;
+        src = pkgs.fetchFromGitHub {
+          owner = "felipealfonsog";
+          repo = "rtl-88x2bu";
+          rev = "70b9a417db1fe49ab6240252cc61073c9601bc58";
+          hash = "sha256-ltyBcotI9I5295z6s2u9PLpYAyYpMQKZSKrkpeczI84=";
+        };
       }))
     ];
     blacklistedKernelModules = [
