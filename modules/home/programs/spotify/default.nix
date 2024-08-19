43ad34045818cffc9af14a37e9382a5db40d16b9
Daru <mica.tui@proton.me>
2024-08-19 18:57:38 +0200

    programs/spotify: fixes

--- modules/home/programs/spotify/default.nix
+++ modules/home/programs/spotify/default.nix
@@ -11,16 +11,16 @@ in {
     enable = true;
     theme = spicePkgs.themes.text;
     enabledSnippets = with spicePkgs.snippets; [
-      auto-hide-friends
-      hide-friends-activity-button
-      remove-popular
-      Hide-Made-For-YOU
-      remove-connect-bar
-      fix-liked-button
-      fix-liked-icon
-      hide-download-button
-      hide-now-playing-view-button
-      hide-profile-username
+      autoHideFriends
+      hideFriendsActivityButton
+      removePopular
+      hideMadeForYou
+      removeConnectBar
+      fixLikedButton
+      fixLikedIcon
+      hideDownloadButton
+      hideNowPlayingViewButton
+      hideProfileUsername
       ''
         section[data-testid='home-page'] .main-shelf-shelf:not([aria-label='Recently played'], [aria-label='Your playlists']) {
           display: none !important;
