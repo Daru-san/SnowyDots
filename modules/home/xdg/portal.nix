abfa7ee46e22ff5abe5bfa0629941f7a7ad874c5
Daru <mica.tui@proton.me>
2024-08-24 08:46:21 +0200

    home/xdg: add wlr portal

--- modules/home/xdg/portal.nix
+++ modules/home/xdg/portal.nix
@@ -1,13 +1,9 @@
-{
-  pkgs,
-  inputs,
-  system,
-  ...
-}: {
+{pkgs, ...}: {
   enable = true;
   extraPortals = with pkgs; [
     xdg-desktop-portal-kde
     xdg-desktop-portal-gtk
+    xdg-desktop-portal-wlr
   ];
   config = {
     sway = {
