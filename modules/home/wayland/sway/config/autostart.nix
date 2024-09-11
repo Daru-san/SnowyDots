a80df58cbc5f5dd717fd4e84190064c7a2cb5174
Daru <mica.tui@proton.me>
2024-09-11 18:41:43 +0200

    wayland/sway: remove unused autostart commands

--- modules/home/wayland/sway/config/autostart.nix
+++ modules/home/wayland/sway/config/autostart.nix
@@ -3,21 +3,16 @@
   lib,
   config,
   ...
-}: {
+}:
+{
   wayland.windowManager.sway.config.startup = [
-    {command = lib.getExe pkgs.copyq;}
-    {command = lib.getExe pkgs.autotiling-rs;}
-    {command = lib.getExe pkgs.sway-assign-cgroups;}
+    { command = lib.getExe pkgs.copyq; }
+    { command = lib.getExe pkgs.autotiling-rs; }
+    { command = lib.getExe pkgs.sway-assign-cgroups; }
     {
       command = "sleep 3 && " + lib.getExe pkgs.swww + " img ${config.wallpaperImage}";
       always = true;
     }
-    {command = lib.getExe pkgs.kitty + " --app-id btop --hold " + lib.getExe pkgs.btop;}
-    {command = "ags";}
-    {command = "systemctl --user import-environment";}
-    {
-      command = "systemctl --user restart kanshi";
-      always = true;
-    }
+    { command = lib.getExe pkgs.kitty + " --app-id btop --hold " + lib.getExe pkgs.btop; }
   ];
 }
