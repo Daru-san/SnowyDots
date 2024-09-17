c1ea58bea363eb706b3ecf6950bd8842eed0b5d4
Daru <mica.tui@proton.me>
2024-09-17 11:07:43 +0200

    wayland/hyprland: bring back window rules

--- modules/home/wayland/hyprland/config/rules.nix
+++ modules/home/wayland/hyprland/config/rules.nix
@@ -1,4 +1,5 @@
-{lib, ...}: {
+{ lib, ... }:
+{
   wayland.windowManager.hyprland = {
     settings = {
       windowrulev2 = [
@@ -7,46 +8,89 @@
         "float,class:(lazarus),title:(Completion)"
       ];
       windowrule = lib.flatten [
-        (let
-          window = [
-            "file-roller"
-            "foot"
-            "iwgtk"
-            "nm-connection-editor"
-            "org.kde.kdeconnect.daemon"
-            ".blueman-manager-wrapped"
-            "org.twosheds.iwgtk"
-            "com.github.hluk.copyq"
-            "com.github.wwmm.easyeffects"
-            "Generate Password"
-            "org.gnome.FileRoller"
-            "org.freedesktop.impl.portal.desktop.kde"
-            "nmtui"
-            "pulsemixer"
-            "valent"
-            "org.kde.rsibreak"
-            "xdg-desktop-portal-kde"
-          ];
-          resized-windows = [
-            "foot"
-            "nmtui"
-            "pulsemixer"
-            "org.freedesktop.impl.portal.desktop.kde"
-            "xdg-desktop-portal-kde"
-            "org.gnome.FileRoller"
-            "org.twosheds.iwgtk"
-            "valent"
-            "com.github.wwmm.easyeffects"
-          ];
-        in [
-          (map (c: "float,^(${c})(.*)$") window)
-          (map (d: "center,^(${d})(.*)$") window)
-          (map (e: "size 60%,^(${e})(.*)$") resized-windows)
-        ])
-        (let
-          rules = ["fullscreen" "noanim" "stayfocused" "pin" "dimaround"];
-        in
-          map (x: "${x},^(wlogout)$") rules)
+        (
+          let
+            window = [
+              "file-roller"
+              "foot"
+              "iwgtk"
+              "nm-connection-editor"
+              "org.kde.kdeconnect.daemon"
+              ".blueman-manager-wrapped"
+              "org.twosheds.iwgtk"
+              "com.github.hluk.copyq"
+              "com.github.wwmm.easyeffects"
+              "Generate Password"
+              "org.gnome.FileRoller"
+              "org.freedesktop.impl.portal.desktop.kde"
+              "nmtui"
+              "pulsemixer"
+              "valent"
+              "org.kde.rsibreak"
+              "xdg-desktop-portal-kde"
+            ];
+            resized-windows = [
+              "foot"
+              "nmtui"
+              "pulsemixer"
+              "org.freedesktop.impl.portal.desktop.kde"
+              "xdg-desktop-portal-kde"
+              "org.gnome.FileRoller"
+              "org.twosheds.iwgtk"
+              "valent"
+              "com.github.wwmm.easyeffects"
+            ];
+          in
+          [
+            (map (c: "float,^(${c})(.*)$") window)
+            (map (d: "center,^(${d})(.*)$") window)
+            (map (e: "size 60%,^(${e})(.*)$") resized-windows)
+          ]
+        )
+        (
+          let
+            w = x: y: "workspace ${toString x},^(${y})(.*)$";
+            wt = x: y: "workspace ${toString x},title:(${y})(.*)$";
+            g = x: "group,^(${x})$";
+          in
+          [
+            (g "oculante")
+            (w 8 "oculante")
+
+            (w 7 "libreoffice")
+
+            (w 2 "firefox")
+            (w 4 "org.gnome.Nautilus")
+
+            (w 6 "mpv")
+
+            (w 6 "FreeTube")
+            (wt 5 "Spotify")
+
+            (w 9 "virt-manager")
+            (g "virt-manager")
+
+            (w 9 "org.pwmt.zathura")
+
+            (w 7 "org.prismlauncher.PrismLauncher")
+            (w 7 "Minecraft")
+            (w 7 "com.obsproject.Studio")
+
+            (w 8 "org.kde.kdenlive")
+          ]
+        )
+        (
+          let
+            rules = [
+              "fullscreen"
+              "noanim"
+              "stayfocused"
+              "pin"
+              "dimaround"
+            ];
+          in
+          map (x: "${x},^(wlogout)$") rules
+        )
       ];
     };
   };
