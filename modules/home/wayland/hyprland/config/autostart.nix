946dbde09cde787318625e44586a84bca2774912
Daru <mica.tui@proton.me>
2024-09-17 13:24:09 +0200

    wayland/hyprland: btop starts on workspace 3

--- modules/home/wayland/hyprland/config/autostart.nix
+++ modules/home/wayland/hyprland/config/autostart.nix
@@ -12,7 +12,7 @@ in
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "[workspace 1] ${kitty} -T Terminal"
-      "[workspace 2] ${kitty} -T btop --hold ${getExe config.programs.btop.package}"
+      "[workspace 3] ${kitty} -T btop --hold ${getExe config.programs.btop.package}"
       (getExe pkgs.copyq)
       (getExe pkgs.swww + " img ${config.wallpaperImage}")
     ];
