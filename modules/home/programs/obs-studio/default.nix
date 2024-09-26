15fa2a4e6329661d85bd92d3faea7882f5b8d11c
Daru <mica.tui@proton.me>
2024-09-26 13:04:11 +0200

    programs/obs-studio: refactor plugins

--- modules/home/programs/obs-studio/default.nix
+++ modules/home/programs/obs-studio/default.nix
@@ -1,12 +1,14 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
       obs-vkcapture
       obs-vaapi
       obs-pipewire-audio-capture
       input-overlay
+      droidcam-obs
+      obs-websocket
     ];
   };
 }
