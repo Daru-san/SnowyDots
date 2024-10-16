054f617d23834360b32fff73560cb607081c6a4f
Daru <mica.tui@proton.me>
2024-10-16 05:57:40 +0200

    system/kernel: back to mainline

--- modules/nixos/system/kernel.nix
+++ modules/nixos/system/kernel.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   boot = {
-    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [ "usbcore.autosuspend=-1" ];
   };
   boot.kernel.sysctl = {
