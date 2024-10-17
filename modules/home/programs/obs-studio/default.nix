1c8056383601c10ab835494b9a8299844d5f0a28
Daru <mica.tui@proton.me>
2024-10-17 18:46:09 +0200

    programs/obs-studio: remove vkcapture

--- modules/home/programs/obs-studio/default.nix
+++ modules/home/programs/obs-studio/default.nix
@@ -3,7 +3,6 @@
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
-      obs-vkcapture
       obs-vaapi
       obs-pipewire-audio-capture
       input-overlay
