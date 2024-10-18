f04066a92459e28b0daea8eaf2250f58855d80cf
Daru <mica.tui@proton.me>
2024-10-18 20:33:00 +0200

    systems/aria: simplify partition layout

--- systems/desktop/hardware-configuration.nix
+++ systems/desktop/hardware-configuration.nix
@@ -1,163 +1,30 @@
-{ lib, pkgs, ... }:
-{
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "ahci"
-    "usbhid"
-    "usb_storage"
-    "sd_mod"
-    "sr_mod"
-    "rtsx_usb_sdmmc"
-  ];
+{ config, lib,... }:
 
-  fileSystems = {
-    "/" = {
-      device = "none";
-      fsType = "tmpfs";
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/50eefb2e-874f-4fa1-b03a-3c3c880b8f24";
+      fsType = "xfs";
     };
 
-    "/boot" = {
-      device = "/dev/disk/by-uuid/7ED4-6E7F";
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/07075934-d163-41a5-81f5-23cbd5528c95";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/7ED4-6E7F";
       fsType = "vfat";
-      options = [
-        "fmask=0022"
-        "dmask=0022"
-      ];
+      options = [ "fmask=0022" "dmask=0022" ];
     };
-
-    "/nix" = {
-      device = "/dev/disk/by-uuid/1de88fe7-5fe8-4e0b-ad00-fdc3cb452408";
-      fsType = "btrfs";
-      options = [
-        "subvol=nix"
-        "noatime"
-        "nodiratime"
-      ];
-    };
-
-    "/etc" = {
-      device = "/dev/disk/by-uuid/1de88fe7-5fe8-4e0b-ad00-fdc3cb452408";
-      fsType = "btrfs";
-      options = [ "subvol=etc" ];
-    };
-
-    "/var" = {
-      device = "/dev/disk/by-uuid/1de88fe7-5fe8-4e0b-ad00-fdc3cb452408";
-      fsType = "btrfs";
-      options = [ "subvol=var" ];
-    };
-
-    "/home" = {
-      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
-      fsType = "btrfs";
-      options = [ "subvol=home" ];
-    };
-
-    "/home/daru/Music" = {
-      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
-      fsType = "btrfs";
-      options = [
-        "subvol=music"
-        "rw"
-        "compress=zstd"
-        "discard"
-      ];
-    };
-
-    "/home/daru/snow" = {
-      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
-      fsType = "btrfs";
-      options = [
-        "subvol=snow"
-        "rw"
-        "compress=zstd"
-        "discard"
-      ];
-    };
-
-    "/home/daru/Videos" = {
-      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
-      fsType = "btrfs";
-      options = [
-        "subvol=Videos"
-        "rw"
-        "compress=zstd"
-        "discard"
-      ];
-    };
-
-    "/home/daru/Wallpapers" = {
-      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
-      fsType = "btrfs";
-      options = [
-        "subvol=Wallpapers"
-        "rw"
-        "compress=zstd"
-        "discard"
-      ];
-    };
-
-    "/home/daru/Misc" = {
-      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
-      fsType = "btrfs";
-      options = [
-        "subvol=Misc"
-        "rw"
-        "compress=zstd"
-        "discard"
-      ];
-    };
-
-    "/home/daru/Development" = {
-      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
-      fsType = "btrfs";
-      options = [
-        "subvol=repos"
-        "rw"
-        "compress=zstd"
-        "discard"
-      ];
-    };
-
-    "/home/daru/Documents" = {
-      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
-      fsType = "btrfs";
-      options = [
-        "subvol=documents"
-        "rw"
-        "compress=zstd"
-        "discard"
-      ];
-    };
-
-    "/home/daru/Pictures" = {
-      device = "/dev/disk/by-uuid/5bbf8784-92a9-4959-97a5-a7baec021d1c";
-      fsType = "btrfs";
-      options = [
-        "subvol=pictures"
-        "rw"
-        "compress=zstd"
-        "discard"
-      ];
-    };
-
-    "/home/daru/Documents/comp-sci" = {
-      device = "/dev/disk/by-label/work";
-      fsType = "ntfs-3g";
-      options = [
-        "rw"
-        "uid=1000"
-      ];
-    };
-  };
 
   swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault true;
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware = {
-    enableRedistributableFirmware = true;
-    cpu.intel.updateMicrocode = true;
-    firmware = [ pkgs.linux-firmware ];
-  };
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
