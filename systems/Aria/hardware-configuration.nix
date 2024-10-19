b93d182ce7e7924b668140db07af8eb7473a1861
Daru <mica.tui@proton.me>
2024-10-19 14:50:21 +0200

    system/desktop: clean hardware configuration

--- systems/Aria/hardware-configuration.nix
+++ systems/Aria/hardware-configuration.nix
@@ -1,30 +1,29 @@
-{ config, lib,... }:{
+{
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/50eefb2e-874f-4fa1-b03a-3c3c880b8f24";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/50eefb2e-874f-4fa1-b03a-3c3c880b8f24";
       fsType = "xfs";
     };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/07075934-d163-41a5-81f5-23cbd5528c95";
+    "/home" = {
+      device = "/dev/disk/by-uuid/07075934-d163-41a5-81f5-23cbd5528c95";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/7ED4-6E7F";
+    "/boot" = {
+      device = "/dev/disk/by-uuid/7ED4-6E7F";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
     };
+  };
 
   swapDevices = [ ];
-
-  networking.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware = {
+    cpu.intel.updateMicrocode = true;
+    enableRedistributableFirmware = true;
+  };
 }
