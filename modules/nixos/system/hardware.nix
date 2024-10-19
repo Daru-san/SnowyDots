83b47520ab55dab5d928a6cf23ecb98b282370f7
Daru <mica.tui@proton.me>
2024-10-19 11:08:25 +0200

    system/hardware: add thermald service

--- modules/nixos/system/hardware.nix
+++ modules/nixos/system/hardware.nix
@@ -8,6 +8,7 @@
     fwupd.enable = true;
     fstrim.enable = true;
     earlyoom.enable = true;
+    thermald.enable = true;
   };
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="[sv]d[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
