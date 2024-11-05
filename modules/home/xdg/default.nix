4b41343506e6dad87aef0b20d2d231bde815c092
Daru <mica.tui@proton.me>
2024-11-05 08:47:36 +0200

    home/xdg: do not use xdg-desktop-portal config

--- modules/home/xdg/default.nix
+++ modules/home/xdg/default.nix
@@ -1,13 +1,9 @@
 {
   config,
-  pkgs,
-  system,
-  inputs,
   ...
 }:
 {
   xdg = {
-    portal = import ./portal.nix { inherit pkgs system inputs; };
     userDirs = import ./dirs.nix { inherit config; };
     mimeApps = import ./mimetypes.nix;
     desktopEntries = import ./desktop-entries.nix;
