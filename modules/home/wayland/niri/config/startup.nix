dd891f2665691cb2af2d8467f4f47785312b0707
Daru <mica.tui@proton.me>
2024-11-12 21:46:36 +0200

    wayland/niri: update startup services

--- modules/home/wayland/niri/config/startup.nix
+++ modules/home/wayland/niri/config/startup.nix
@@ -7,11 +7,15 @@
 {
   programs.niri.settings = {
     spawn-at-startup = [
-      { command = [ "systemctl --user start swww-daemon" ]; }
-      { command = [ (lib.getExe pkgs.swww + " img ${config.wallpaperImage}") ]; }
       { command = [ (lib.getExe pkgs.copyq) ]; }
       { command = [ (lib.getExe config.programs.kitty.package) ]; }
       { command = [ (lib.getExe pkgs.xwayland-satellite) ]; }
+      { command = [ "systemctl --user restart snowy-shell" ]; }
+      { command = [ "systemctl --user restart swww-daemon" ]; }
+      { command = [ "systemctl --user restart hypridle" ]; }
+      { command = [ "systemctl --user restart avizo" ]; }
+      { command = [ "systemctl --user restart wlsunset" ]; }
+      { command = [ (lib.getExe pkgs.swww + " img ${config.wallpaperImage}") ]; }
     ];
   };
 }
