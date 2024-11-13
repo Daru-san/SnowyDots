8a6f95d42b56c9fd8f4fbc302c76b96aab766a3d
Daru <mica.tui@proton.me>
2024-11-13 19:35:26 +0200

    wayland/niri: get systemd services working properly

--- modules/home/wayland/niri/config/startup.nix
+++ modules/home/wayland/niri/config/startup.nix
@@ -7,11 +7,34 @@
 {
   programs.niri.settings = {
     spawn-at-startup = [
-      { command = [ "systemctl --user start niri-session.target" ]; }
+      {
+        command = [
+          "systemctl"
+          "--user"
+          "start"
+          "niri-session.target"
+        ];
+      }
       { command = [ (lib.getExe pkgs.copyq) ]; }
       { command = [ (lib.getExe config.programs.kitty.package) ]; }
       { command = [ (lib.getExe pkgs.xwayland-satellite) ]; }
       { command = [ (lib.getExe pkgs.swww + " img ${config.wallpaperImage}") ]; }
+      {
+        command = [
+          "systemctl"
+          "--user"
+          "restart"
+          "snowy-shell.service"
+        ];
+      }
+      {
+        command = [
+          "systemctl"
+          "--user"
+          "restart"
+          "syncthingtray.service"
+        ];
+      }
     ];
   };
 }
