664441ef56e875c73a61610d72aa7b62bb71924f
Daru <mica.tui@proton.me>
2024-12-04 16:22:45 +0200

    home/shell: clean aliases

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -26,28 +26,17 @@
   home.shellAliases =
     let
       bat = "${config.programs.bat.package}/bin/bat";
-      git = "${config.programs.git.package}/bin/git";
-      firefox = "${lib.getExe config.programs.firefox.package}";
-      lazygit = "${config.programs.lazygit.package}/bin/lazygit";
       tty-clock = "${pkgs.tty-clock}/bin/tty-clock";
       trashy = "${pkgs.trashy}/bin/trashy";
       fzf = "${config.programs.fzf.package}/bin/fzf";
     in
     {
-      g = git;
-      lg = lazygit;
       clock = "${tty-clock} -bscBrnS";
-      nb = "nix-rebuild";
-      hb = "hm-build";
       tp = "${trashy} put";
       te = "${trashy} empty";
-      jtp = ''find $1 -name "*.jpg" -exec mogrify -format png {} \;'';
-      man = "${pkgs.bat-extras.batman}/bin/batman";
-      bgrep = "${pkgs.bat-extras.batgrep}/bin/batgrep";
       cat = bat;
       trashy-empty = "${trashy} list | ${fzf} --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs ${trashy} empty --match=exact --force";
       trashy-restore = "${trashy} list | ${fzf} --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs ${trashy} restore --match=exact --force";
-      inherit firefox;
       zc = "clear && z";
     };
 }
