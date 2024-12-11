9dd28b203c8edf19055f2586597b3ca6d05e326c
Daru <mica.tui@proton.me>
2024-12-11 21:02:46 +0200

    system: add extra pipewire sinks

---
+++ modules/nixos/tools/pipewire.nix
@@ -0,0 +1,26 @@
+{
+  services.pipewire.extraConfig.pipewire."97-null-sink" = {
+    "context.objects" = [
+      {
+        factory = "adapter";
+        args = {
+          "factory.name" = "support.null-audio-sink";
+          "node.name" = "Null-Sink";
+          "node.description" = "Null Sink";
+          "media.class" = "Audio/Sink";
+          "audio.position" = "FL,FR";
+        };
+      }
+      {
+        factory = "adapter";
+        args = {
+          "factory.name" = "support.null-audio-sink";
+          "node.name" = "Null-Source";
+          "node.description" = "Null Source";
+          "media.class" = "Audio/Source";
+          "audio.position" = "FL,FR";
+        };
+      }
+    ];
+  };
+}
