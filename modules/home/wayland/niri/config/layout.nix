7dd3d1ac9d2442becb175eaef3fcb16dc6fe365e
Daru <mica.tui@proton.me>
2024-12-16 22:17:55 +0200

    wayland/niri: update layout config

--- modules/home/wayland/niri/config/layout.nix
+++ modules/home/wayland/niri/config/layout.nix
@@ -1,12 +1,15 @@
 {
   programs.niri.settings = {
     prefer-no-csd = true;
+    hotkey-overlay.skip-at-startup = true;
+    cursor.hide-when-typing = true;
     layout = {
       border = {
         enable = false;
       };
       focus-ring = {
-        enable = false;
+        enable = true;
+        width = 3;
       };
       gaps = 6;
     };
