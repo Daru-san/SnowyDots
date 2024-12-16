6355e53eae46a67dfa77cab0d9ce50a669b84b37
Daru <mica.tui@proton.me>
2024-12-16 22:26:14 +0200

    wayland/niri: update config

--- modules/home/wayland/niri/config/rules.nix
+++ modules/home/wayland/niri/config/rules.nix
@@ -2,10 +2,35 @@
   programs.niri.settings = {
     window-rules = [
       {
+        draw-border-with-background = false;
+        open-maximized = true;
+        clip-to-geometry = true;
+        geometry-corner-radius = {
+          top-left = 6.0;
+          top-right = 6.0;
+          bottom-left = 6.0;
+          bottom-right = 6.0;
+        };
+        variable-refresh-rate = false;
+      }
+      {
+        matches = [
+          { app-id = "^org\.keepassxc\.KeePassXC$"; }
+          { app-id = "^org\.gnome\.World\.Secrets$"; }
+          { app-id = "iwgtk"; }
+          { app-id = "com.github.hluk.copyq"; }
+          { app-id = "org.twosheds.iwgtk"; }
+          { app-id = "Generate Password"; }
+          { app-id = "^nmtui$"; }
+          { app-id = "valent"; }
+        ];
+        block-out-from = "screencast";
+      }
+      {
+        open-maximized = false;
         matches = [
           { app-id = "file-roller"; }
           { app-id = "foot"; }
-          { app-id = "iwgtk"; }
           { app-id = "nm-connection-editor"; }
           { app-id = ".blueman-manager-wrapped"; }
           { app-id = "org.twosheds.iwgtk"; }
