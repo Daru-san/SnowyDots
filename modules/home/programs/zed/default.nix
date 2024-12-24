9e3ea6c8dc61d2dd84961f8c92c2337da7f15f8a
Daru <mica.tui@proton.me>
2024-12-24 11:21:41 +0200

    programs/zed: update config

--- modules/home/programs/zed/default.nix
+++ modules/home/programs/zed/default.nix
@@ -5,7 +5,7 @@
 {
   programs.zed-editor = {
     enable = true;
-    extension = [
+    extensions = [
       "nix"
       "dart"
       "toml"
@@ -24,10 +24,22 @@
       zig
       nushell
       meson
+      mesonlsp
+      clang-tools
+      cargo
+      rust-analyzer
+      rustc
+      rustfmt
+      clippy
+      nil
+      nixfmt-rfc-style
     ];
     userSettings = {
       features = {
         copilot = false;
+      };
+      language_models = {
+        copilot_chat = null;
       };
       assistant = {
         default_model = {
@@ -53,6 +65,21 @@
         button = false;
         indent_size = 24;
       };
+      vim = {
+        toggle_relative_line_numbers = true;
+      };
+      languages = {
+        Nix = {
+          language_servers = [
+            "nil"
+            "nixd"
+          ];
+          formatter.external = {
+            command = "nixfmt";
+          };
+        };
+      };
+      load_direnv = "shell_hook";
       toolbar.quick_actions = false;
       outline_panel.button = false;
       collaboration_panel.button = false;
