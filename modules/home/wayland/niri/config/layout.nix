8ef5a920101b66945201a42aaf167ada75a8881a
Daru <mica.tui@proton.me>
2025-01-13 13:46:07 +0200

    wayland/niri: show cursor when typing

--- modules/home/wayland/niri/config/layout.nix
+++ modules/home/wayland/niri/config/layout.nix
@@ -2,7 +2,7 @@
   programs.niri.settings = {
     prefer-no-csd = true;
     hotkey-overlay.skip-at-startup = true;
-    cursor.hide-when-typing = true;
+    cursor.hide-when-typing = false;
     layout = {
       border = {
         enable = false;
