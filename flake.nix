3fe2543ed91297be7b90ea329aa575dccdf9bf8b
Daru <mica.tui@proton.me>
2025-01-15 22:36:51 +0200

    flake: use snowyvim nixpkgs input

--- flake.nix
+++ flake.nix
@@ -44,10 +44,7 @@
     # My own repos
     rimi.url = "github:Daru-san/rimi";
     color-picker.url = "github:Daru-san/color-picker-rs";
-    snowyvim = {
-      url = "sourcehut:~darumaka/SnowyVim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    snowyvim.url = "sourcehut:~darumaka/SnowyVim";
     snowpkgs.url = "github:Daru-san/Snowpkgs";
     walls = {
       url = "sourcehut:~darumaka/Wallpapers";
