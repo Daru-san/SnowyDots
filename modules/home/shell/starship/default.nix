dec01824cb8d76091e4c1beabe808bcc3e080178
Daru <mica.tui@proton.me>
2025-01-25 15:13:36 +0200

    shell/starship: update prompt style
    I think it looks way better in it's new form

--- modules/home/shell/starship/default.nix
+++ modules/home/shell/starship/default.nix
@@ -7,42 +7,95 @@
     enableNushellIntegration = true;
     settings = {
       add_newline = false;
+      continuation_prompt = "[▸▹ ](dimmed white)";
       format = lib.concatStrings [
+        "$cmd_duration"
+        "$shell"
+        "$sudo"
+        "$username"
         "$directory"
+        "$character"
+      ];
+      command_timeout = 50000;
+      right_format = lib.concatStrings [
+        "$nix_shell"
         "$git_branch"
         "$git_commit"
         "$git_state"
-        "$git_metrics"
-        "$git_status"
-        "$java"
-        "$nix_shell"
-        "$sudo"
-        "$character"
-        "$shell"
+        "$hg_branch"
+        "$cmake"
+        "$c"
+        "$dart"
+        "$lua"
+        "$rust"
+        "$meson"
+        "$status"
       ];
-      command_timeout = 5000;
-      right_format = lib.concatStrings [
-        "$cmd_duration"
-      ];
-      java = {
-        format = ''[java:(($version)) ]($style)'';
-        symbol = "";
+      cmd_duration = {
+        format = "[⏱︎   $duration]($style) ";
+        show_notifications = true;
+        min_time_to_notify = 300000;
       };
       git_branch = {
-        format = ''[git:($branch(:$remote_branch )) ]($style)'';
-        symbol = "";
+        format = ''[($branch)]($style)'';
+      };
+      git_status = {
+        format = "$all_status$ahead_behind ";
+        ahead = "[⬆](bold purple) ";
+        behind = "[⬇](bold purple) ";
+        staged = "[✚](green) ";
+        deleted = "[✖](red) ";
+        renamed = "[➜](purple) ";
+        stashed = "[✭](cyan) ";
+        untracked = "[◼](white) ";
+        modified = "[✱](blue) ";
+        conflicted = "[═](yellow) ";
+        diverged = "⇕ ";
+        up_to_date = "";
+      };
+      git_metrics = {
+        format = "([▴ $a dded]($added_style))([▿$del eted]($deleted_style))";
+        added_style = "italic dimmed green";
+        deleted_style = "italic dimmed red";
+        ignore_submodules = true;
+        disabled = false;
       };
       character = {
-        success_symbol = "[> ](bold green)";
-        error_symbol = "[> ](bold red)";
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+        vimcmd_symbol = "[❮](green)";
       };
-      package.disabled = true;
 
+      package.disabled = true;
+      sudo = {
+        format = "[$symbol]($style)";
+        style = "bold italic bright-purple";
+        symbol = "⋈┈";
+        disabled = false;
+      };
       nix_shell = {
-        impure_msg = "[impure](bold red)";
-        pure_msg = "[pure](bold green)";
-        unknown_msg = "[unknown](bold yellow)";
-        format = "[nix-shell:($state(( $name))) ](bold blue) ";
+        style = "bold italic dimmed blue";
+        symbol = "✶";
+        format = " [$symbol nix⎪$state⎪]($style) [$name](italic dimmed white) ";
+        impure_msg = "[⌽](bold dimmed red)";
+        pure_msg = "[⌾](bold dimmed green)";
+        unknown_msg = "[◌](bold dimmed ellow)";
+      };
+      lua = {
+        format = ''[lua](italic) [''${symbol}''${version}]($style)'';
+        version_format = ''''${raw}'';
+        symbol = "⨀ ";
+        style = "bold bright-yellow";
+      };
+      rust = {
+        format = " [rust](italic) [$symbol$version]($style)";
+        symbol = "⊃ ";
+        version_format = ''''${raw}'';
+        style = "bold red";
+      };
+      c = {
+        symbol = "C ";
+        format = " [$symbol($version(-$name))]($style)";
       };
     };
   };
