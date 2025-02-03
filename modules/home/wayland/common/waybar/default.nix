c82a3230b10621b4b61d767a11cfef4e4e29c391
Daru <mica.tui@proton.me>
2025-02-03 20:34:49 +0200

    programs/waybar: disable stylix css injection
    Lets me keep stylix enabled without the css additions, courtesy of
    stylix commit e81bd0cffadaffecd18b65301628cc4b9b0dbea3

--- modules/home/wayland/common/waybar/default.nix
+++ modules/home/wayland/common/waybar/default.nix
@@ -4,29 +4,10 @@
   ...
 }:
 let
-  colors = config.lib.stylix.colors.withHashtag;
-  color-names = [
-    "base00"
-    "base01"
-    "base02"
-    "base03"
-    "base04"
-    "base05"
-    "base06"
-    "base07"
-    "base08"
-    "base09"
-    "base0A"
-    "base0B"
-    "base0C"
-    "base0D"
-    "base0E"
-    "base0F"
-  ];
-  define-colors = name: value: "@define-color ${name} ${value};";
 in
 {
-  stylix.targets.waybar.enable = false;
+  stylix.targets.waybar.addCss = false;
+
   programs.waybar = {
     enable = true;
 
@@ -34,20 +15,6 @@ in
 
     settings = import ./setup.nix { inherit config lib; };
 
-    style = lib.concatLines (
-      lib.flatten [
-        (builtins.map (color: define-colors color colors.${color}) color-names)
-
-        (builtins.readFile ./style.css)
-
-        ''
-          /* Font configuration */
-          * {
-            font-family: ${config.stylix.fonts.monospace.name};
-            font-size: 12pt;
-          }
-        ''
-      ]
-    );
+    style = (builtins.readFile ./style.css);
   };
 }
