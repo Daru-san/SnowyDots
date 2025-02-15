a6253c950e6fc9c601bac7822dd9853fb1f8d24a
Daru <mica.tui@proton.me>
2025-02-15 08:31:05 +0200

    wayland/hyprland: remove env variables

--- modules/home/wayland/hyprland/default.nix
+++ modules/home/wayland/hyprland/default.nix
@@ -1,8 +1,6 @@
 {
   lib,
   config,
-  inputs,
-  system,
   ...
 }:
 let
@@ -12,9 +10,6 @@ in
   imports = [ ./config ];
   config = lib.mkIf cfg.enable {
     wayland.windowManager.hyprland = {
-      settings.env = lib.mapAttrsToList (
-        name: value: "${name},${builtins.toString value}"
-      ) config.home.sessionVariables;
       systemd = {
         enable = true;
         enableXdgAutostart = true;
