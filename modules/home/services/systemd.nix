d3e784b7937bfebbcd1c9f5f0dcd2c0f84f822d9
Daru <mica.tui@proton.me>
2025-02-22 18:10:57 +0200

    home/services: add seanime server

--- modules/home/services/systemd.nix
+++ modules/home/services/systemd.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }:
 {
   systemd.user.services = {
+    seanime-server = {
+      Service = {
+        ExecStart = "${pkgs.seanime}/bin/seanime";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
     rqbit-server = {
       Service = {
         ExecStart = "${pkgs.rqbit}/bin/rqbit server start Downloads/Torrents";
