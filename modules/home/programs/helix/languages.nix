356a5dc566b1839028b1f4c361119fe77da34c41
Daru <mica.tui@proton.me>
2025-03-04 19:17:04 +0200

    programs/helix: remove unneeded pathlin import in lldb-rust-primer

--- modules/home/programs/helix/languages.nix
+++ modules/home/programs/helix/languages.nix
@@ -23,27 +23,20 @@ let
     ];
   };
 
-  lldb-rust-primer =
-    pkgs.writers.writePython3 "rust-primer.py"
-      {
-        libraries = with pkgs.pythonPackages; [
-          pathlib
-        ];
-      }
-      ''
-        import subprocess
-        import pathlib
-        import lldb
+  lldb-rust-primer = pkgs.writers.writePython3 "rust-primer.py" ''
+    import subprocess
+    import pathlib
+    import lldb
 
-        # Determine the sysroot for the active Rust interpreter
-        rustlib_etc = pathlib.Path(subprocess.getoutput('rustc --print sysroot')) / 'lib' / 'rustlib' / 'etc'
-        if not rustlib_etc.exists():
-            raise RuntimeError('Unable to determine rustc sysroot')
+    # Determine the sysroot for the active Rust interpreter
+    rustlib_etc = pathlib.Path(subprocess.getoutput('rustc --print sysroot')) / 'lib' / 'rustlib' / 'etc'
+    if not rustlib_etc.exists():
+        raise RuntimeError('Unable to determine rustc sysroot')
 
-            # Load lldb_lookup.py and execute lldb_commands with the correct path
-            lldb.debugger.HandleCommand(f"""command script import "{rustlib_etc / 'lldb_lookup.py'}" """)
-            lldb.debugger.HandleCommand(f"""command source -s 0 "{rustlib_etc / 'lldb_commands'}" """)")")
-      '';
+        # Load lldb_lookup.py and execute lldb_commands with the correct path
+        lldb.debugger.HandleCommand(f"""command script import "{rustlib_etc / 'lldb_lookup.py'}" """)
+        lldb.debugger.HandleCommand(f"""command source -s 0 "{rustlib_etc / 'lldb_commands'}" """)")")
+  '';
 
   lldb-rust = {
     inherit (lldb-dap) command port-arg transport;
