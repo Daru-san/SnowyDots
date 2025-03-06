4bfc69640fbbd989714a33597ed415ea1261e288
Daru <mica.tui@proton.me>
2025-03-06 05:49:50 +0200

    programs/helix: fix lldb-rust-primer build

--- modules/home/programs/helix/languages.nix
+++ modules/home/programs/helix/languages.nix
@@ -23,7 +23,7 @@ let
     ];
   };
 
-  lldb-rust-primer = pkgs.writers.writePython3 "rust-primer.py" ''
+  lldb-rust-primer = pkgs.writers.writePython3 "rust-primer.py" { doCheck = false; } ''
     import subprocess
     import pathlib
     import lldb
@@ -35,7 +35,7 @@ let
 
         # Load lldb_lookup.py and execute lldb_commands with the correct path
         lldb.debugger.HandleCommand(f"""command script import "{rustlib_etc / 'lldb_lookup.py'}" """)
-        lldb.debugger.HandleCommand(f"""command source -s 0 "{rustlib_etc / 'lldb_commands'}" """)")")
+        lldb.debugger.HandleCommand(f"""command source -s 0 "{rustlib_etc / 'lldb_commands'}" """)
   '';
 
   lldb-rust = {
