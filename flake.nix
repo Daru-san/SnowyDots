14f90ca27f7980b4da4cb60ae43dd003353777e0
Daru <mica.tui@proton.me>
2025-03-12 18:00:20 +0200

    flake: update zen-browser input to youwen5/zen-browser-flake

--- flake.nix
+++ flake.nix
@@ -42,7 +42,10 @@
     vigiland.url = "github:Jappie3/vigiland";
 
     # Zen browser
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     zenix = {
       url = "github:anders130/zenix";
