5bc3e51cec8a5d9ab566a0da40987bd94218df4c
Daru <mica.tui@proton.me>
2025-03-22 22:59:46 +0200

    wayland/ashell: use yml config file

--- modules/home/wayland/common/ashell/default.nix
+++ modules/home/wayland/common/ashell/default.nix
@@ -6,28 +6,6 @@ in
   home.packages = [ pkgs.ashell ];
   xdg.configFile.ashell = {
     target = "ashell.yml";
-    source = yaml.generate "ashell.yml" {
-      logLevel = "WARN";
-      outputs = "All";
-      position = "bottom";
-      workspaces = {
-        visibilityMode = "All";
-      };
-      clock = {
-        format = "%a %d %b %X";
-      };
-      modules = {
-        left = [
-          "Workspaces"
-          "MediaPlayer"
-        ];
-        center = [ "WindowTitle" ];
-        right = [
-          "SystemInfo"
-          "Tray"
-          ''[Clock, Privacy, Settings]''
-        ];
-      };
-    };
+    source = ./ashell.yml;
   };
 }
