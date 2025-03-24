280bba014e5aa168825349922053bd186c81e8ba
Daru <mica.tui@proton.me>
2025-03-24 20:03:22 +0200

    home/packages: add neovide config

--- home/daru/packages.nix
+++ home/daru/packages.nix
@@ -4,6 +4,7 @@
   inputs,
   system,
   lib,
+  config,
   ...
 }:
 {
@@ -105,12 +106,31 @@
 
       # The craft
       prismlauncher
+      neovide
     ])
     (with inputs; [
       # rimi.packages.${system}.default
       color-picker.packages.${system}.default
       (vim.packages.${system}.default.extend {
         nixpkgs.overlays = [ snowpkgs.overlays.default ];
+        extraConfigLua = ''
+          if vim.g.neovide then
+            vim.keymap.set('n', '<D-s>', ':w<CR>')
+            vim.keymap.set('v', '<D-c>', '"+y')
+            vim.keymap.set('n', '<D-v>', '"+P')
+            vim.keymap.set('v', '<D-v>', '"+P')
+            vim.keymap.set('c', '<D-v>', '<C-R>+')
+            vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli')
+            vim.g.neovide_position_animation_length = 0
+            vim.g.neovide_cursor_animation_length = 0.00
+            vim.g.neovide_cursor_trail_size = 0
+            vim.g.neovide_cursor_animate_in_insert_mode = false
+            vim.g.neovide_cursor_animate_command_line = false
+            vim.g.neovide_scroll_animation_far_lines = 0
+            vim.g.neovide_scroll_animation_length = 0.00
+            vim.o.guifont = "${config.stylix.fonts.monospace.name}:${toString config.stylix.fonts.sizes.terminal}"
+          end
+        '';
       })
     ])
   ];
