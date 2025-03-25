bcf522515d861a7788da7fa38678e379a443749f
Daru <mica.tui@proton.me>
2025-03-26 01:31:06 +0200

    system: remove hyprland cache

--- systems/Aurorus/default.nix
+++ systems/Aurorus/default.nix
@@ -39,14 +39,12 @@
     builders-use-substitutes = true;
     substituters = [
       "https://anyrun.cachix.org"
-      "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://snowy-cache.cachix.org"
     ];
 
     trusted-public-keys = [
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "snowy-cache.cachix.org-1:okWl5IF/yzdZ+p/eRhDFvcanQo/y0ta80dvfdGgy28U="
     ];
