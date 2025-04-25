0d904f09dd013ef643d2cb0ca488b3e65db470ac
Daru <mica.tui@proton.me>
2025-04-25 15:54:12 +0200

    wayland/niri: update package

--- modules/home/wayland/niri/default.nix
+++ modules/home/wayland/niri/default.nix
@@ -1,14 +1,19 @@
-{ inputs, ... }:
+{ inputs, pkgs,... }:
 {
   imports = [
     inputs.niri.homeModules.niri
+    inputs.niri.homeModules.stylix
     ./config
+  ];
+  nixpkgs.overlays = [
+    inputs.niri.overlays.niri
   ];
   wayland = {
     systemd.target = "niri-session.target";
   };
   programs.niri = {
     enable = true;
+    package = pkgs.niri-unstable;
     settings.environment = {
       QT_QPA_PLATFORM = "wayland";
       NIXOS_OZONE_WL = "true";
