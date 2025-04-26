27c1889c80ebf149538c109d715b2470a6c4caf5
Daru <mica.tui@proton.me>
2025-04-26 16:29:00 +0200

    programs/neovide: disable animations

--- modules/home/programs/nvim/default.nix
+++ modules/home/programs/nvim/default.nix
@@ -34,6 +34,12 @@
             vim.g.neovide_cursor_trail_size = 0
             vim.g.neovide_cursor_animate_in_insert_mode = false
             vim.g.neovide_cursor_animate_command_line = false
+            vim.g.neovide_scroll_animation_far_lines = 0
+            vim.g.neovide_scroll_animation_length = 0.00
+
+            vim.g.neovide_cursor_trail_size = 0
+            vim.g.neovide_cursor_animate_in_insert_mode = false
+            vim.g.neovide_cursor_animate_command_line = false
             vim.o.guifont = "${config.stylix.fonts.monospace.name}:${toString config.stylix.fonts.sizes.terminal}"
           end
         '';
