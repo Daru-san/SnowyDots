7b9c554662369de845db3e8fc864ef00f7261d7b
Daru <mica.tui@proton.me>
2025-04-27 20:52:04 +0200

    wayland/sway: remove papersway

--- modules/home/wayland/sway/config/autostart.nix
+++ modules/home/wayland/sway/config/autostart.nix
@@ -6,11 +6,8 @@
 }:
 {
   wayland.windowManager.sway.config.startup = [
-    { command = lib.getExe pkgs.autotiling-rs; }
-    { command = lib.getExe pkgs.sway-assign-cgroups; }
     { command = lib.getExe pkgs.ghostty + " --title=btop -e " + lib.getExe pkgs.btop; }
     { command = lib.getExe config.services.clipse.package + " --listen-shell"; }
-    { command = lib.getExe' pkgs.perl540Packages.Apppapersway "papersway"; }
     { command = lib.getExe pkgs.flameshot; }
   ];
 }
