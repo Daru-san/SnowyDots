45f8a6b994a791d9becb61d4cb607c903ff3f41d
Daru <mica.tui@proton.me>
2025-04-27 17:18:28 +0200

    wayland/sway: update startup

--- modules/home/wayland/sway/config/autostart.nix
+++ modules/home/wayland/sway/config/autostart.nix
@@ -8,8 +8,9 @@
   wayland.windowManager.sway.config.startup = [
     { command = lib.getExe pkgs.autotiling-rs; }
     { command = lib.getExe pkgs.sway-assign-cgroups; }
-    { command = lib.getExe pkgs.kitty + " --app-id btop --hold " + lib.getExe pkgs.btop; }
-    { command = lib.getExe config.programs.clipse.package + "--listen-shell"; }
+    { command = lib.getExe pkgs.ghostty + " --title=btop -e " + lib.getExe pkgs.btop; }
+    { command = lib.getExe config.services.clipse.package + " --listen-shell"; }
     { command = lib.getExe' pkgs.perl540Packages.Apppapersway "papersway"; }
+    { command = lib.getExe pkgs.flameshot; }
   ];
 }
