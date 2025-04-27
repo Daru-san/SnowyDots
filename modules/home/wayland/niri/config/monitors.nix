18cc2917921e939b550f4ba98e1fcf1c377d7bf7
Daru <mica.tui@proton.me>
2025-04-27 16:53:35 +0200

    wayland/niri: enable if selected

--- modules/home/wayland/niri/config/monitors.nix
+++ modules/home/wayland/niri/config/monitors.nix
@@ -2,7 +2,7 @@
   programs.niri.settings = {
     outputs = {
       "HDMI-A-2" = {
-        variable-refresh-rate = true;
+        variable-refresh-rate = false;
         mode = {
           height = 1080;
           width = 1920;
--- modules/home/wayland/niri/default.nix
+++ modules/home/wayland/niri/default.nix
@@ -1,23 +1,24 @@
-{ inputs, pkgs,... }:
+{ inputs, pkgs,lib,config,... }:
 {
   imports = [
     inputs.niri.homeModules.niri
     inputs.niri.homeModules.stylix
     ./config
   ];
-  nixpkgs.overlays = [
-    inputs.niri.overlays.niri
-  ];
-  wayland = {
-    systemd.target = "niri-session.target";
-  };
-  programs.niri = {
-    enable = true;
-    package = pkgs.niri-unstable;
-    settings.environment = {
-      QT_QPA_PLATFORM = "wayland";
-      NIXOS_OZONE_WL = "true";
-      DISPLAY = ":0";
+  config = lib.mkIf config.programs.niri.enable {
+    nixpkgs.overlays = [
+      inputs.niri.overlays.niri
+    ];
+    wayland = {
+      systemd.target = "niri-session.target";
+    };
+    programs.niri = {
+      package = pkgs.niri-unstable;
+      settings.environment = {
+        QT_QPA_PLATFORM = "wayland";
+        NIXOS_OZONE_WL = "true";
+        DISPLAY = ":0";
+      };
     };
   };
 }
