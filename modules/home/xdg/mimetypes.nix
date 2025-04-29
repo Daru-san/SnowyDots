7ee1ae2d70273d8d19e19ec46ee30c661c9e5456
Daru <mica.tui@proton.me>
2025-04-29 13:06:05 +0200

    home/xdg: expand subtypes for video and audio default applications

--- modules/home/xdg/mimetypes.nix
+++ modules/home/xdg/mimetypes.nix
@@ -1,12 +1,45 @@
+{ lib, ... }:
 {
-  defaultApplications = {
-    "application/pdf" = [ "zathura.desktop" ];
-    "image/*" = [ "oculante.desktop" ];
-    "video/*" = [ "mpv.desktop" ];
-    "audio/*" = [ "qmmp.desktop" ];
-    "audio/mpeg" = [ "qmmp.desktop" ];
-    "audio/mp4" = [ "qmmp.desktop" ];
-    "*.ogg" = [ "qmmp.desktop" ];
-    "text/plain" = [ "Helix.desktop" ];
-  };
+  defaultApplications = lib.zipAttrsWith (_: values: values) (
+    let
+      subtypes =
+        type: program: subtype:
+        builtins.listToAttrs (
+          builtins.map (x: {
+            name = type + "/" + x;
+            value = program;
+          }) subtype
+        );
+    in
+    [
+      {
+        "application/pdf" = "org.pwmt.zathura-pdf-mupdf.desktop";
+      }
+      (subtypes "image" "oculante.desktop" [
+        "png"
+        "jpeg"
+        "gif"
+        "apng"
+        "avif"
+        "bmp"
+        "vnd.microsoft.icon"
+        "svg+xml"
+        "tiff"
+        "webp"
+        "svg"
+      ])
+      (subtypes "video" "mpv.desktop" [
+        "3gpp"
+        "h261"
+        "jpeg"
+        "mp4"
+        "mpeg"
+        "ogg"
+        "quicktime"
+        "webm"
+        "x-matroska"
+      ])
+      { "text/plain" ="neovide.desktop"; }
+    ]
+  );
 }
