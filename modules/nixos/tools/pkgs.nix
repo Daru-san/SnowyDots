e76244a58a2a2bade85946ddcb298e9ee765ea90
Daru <mica.tui@proton.me>
2025-05-19 19:02:19 +0200

    format

--- modules/nixos/tools/pkgs.nix
+++ modules/nixos/tools/pkgs.nix
@@ -1,9 +1,10 @@
-{inputs,...}:{
-    imports = [inputs.chaotic.nixosModules.default];
-    chaotic.nyx = {
-        cache.enable =true;
-        nixPath.enable = true;
-        overlay.enable = true;
-        registry.enable = true;
-    };
+{ inputs, ... }:
+{
+  imports = [ inputs.chaotic.nixosModules.default ];
+  chaotic.nyx = {
+    cache.enable = true;
+    nixPath.enable = true;
+    overlay.enable = true;
+    registry.enable = true;
+  };
 }
