4fa705c5f24e98b7df9fe60eaf69e021b0cf3e6d
Daru <mica.tui@proton.me>
2025-05-19 22:18:02 +0200

    home/xdg: remove unused directories

--- modules/home/xdg/dirs.nix
+++ modules/home/xdg/dirs.nix
@@ -17,11 +17,6 @@ rec {
   extraConfig = rec {
     XDG_SCREENSHOTS_DIR = "${pictures}/Screenshots";
     XDG_WALLPAPER_DIR = "${hm}/Wallpapers";
-    XDG_MISC_DIR = "${hm}/Misc";
-    XDG_REPO_DIR = "${XDG_DEV_DIR}/Repos";
-    XDG_PROJECTS_DIR = "${XDG_DEV_DIR}/Projects";
-    XDG_NOTES_DIR = "${hm}/Notes";
-    XDG_DEV_DIR = "${hm}/Development";
     XDG_SECRET_DIR = "${hm}/.secrets";
   };
 }
