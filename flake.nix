643a055f5c723b34769da646309b7e8578d34c41
Daru <mica.tui@proton.me>
2025-05-19 19:51:27 +0200

    flake: rollback stable to 24.11

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Nixpkgs repos
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
     snow-nixpkgs.url = "github:Daru-san/nixpkgs/ashell-update";
 
