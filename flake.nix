999990fa16b37110787959a4e7f64fd1af0a7d3f
Daru <mica.tui@proton.me>
2025-05-28 20:50:13 +0200

    yazi: v25.4.8 -> v25.5.28

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     #secrets
     sops-nix.url = "github:Mic92/sops-nix";
 
-    yazi.url = "github:sxyazi/yazi/v25.4.8";
+    yazi.url = "github:sxyazi/yazi/v25.5.28";
 
     # Theme manager
     stylix.url = "github:nix-community/stylix";
