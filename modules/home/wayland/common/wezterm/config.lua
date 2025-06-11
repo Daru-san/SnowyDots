ac4f8b6cc09339c04cc0a047d266ac6e50d9db9d
Daru <mica.tui@proton.me>
2025-06-11 20:38:46 +0200

    wezterm: update config

--- modules/home/wayland/common/wezterm/config.lua
+++ modules/home/wayland/common/wezterm/config.lua
@@ -1,5 +1,16 @@
-return {
-    config({
-        window_background_opacity = 0.6,
-    }),
-}
+local gpus = wezterm.gui.enumerate_gpus()
+
+local config = {}
+
+for _, gpu in ipairs(gpus) do
+    if gpu.backend == "Vulkan" and gpu.device_type == "IntegratedGpu" then
+        config.front_end = "WebGpu"
+        config.webgpu_preferred_adapter = gpu
+    end
+end
+
+config.window_background_opacity = 0.6
+config.hide_tab_bar_if_only_one_tab = true
+config.font_locator = "FontConfig"
+
+return config
