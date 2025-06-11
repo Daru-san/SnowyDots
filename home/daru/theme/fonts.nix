d1f86c61c9290ef740a95be97ac2e4308bbeee17
Daru <mica.tui@proton.me>
2025-06-11 22:39:16 +0200

    home: explicitly enable fontconfig stylix target

--- home/daru/theme/fonts.nix
+++ home/daru/theme/fonts.nix
@@ -4,20 +4,26 @@
   ...
 }:
 {
-  stylix.fonts = {
-    serif = {
-      package = pkgs.rubik;
-      name = "Rubik";
+  stylix = {
+    targets = {
+      fontconfig.enable = true;
+      font-packages.enable = true;
     };
-    sansSerif = config.stylix.fonts.serif;
-    monospace = {
-      package = pkgs.nerd-fonts.iosevka;
-      name = "Iosevka Nerd Font";
-    };
-    sizes = {
-      desktop = 12;
-      applications = 12;
-      terminal = 14;
+    fonts = {
+      serif = {
+        package = pkgs.rubik;
+        name = "Rubik";
+      };
+      sansSerif = config.stylix.fonts.serif;
+      monospace = {
+        package = pkgs.nerd-fonts.iosevka;
+        name = "Iosevka Nerd Font";
+      };
+      sizes = {
+        desktop = 12;
+        applications = 12;
+        terminal = 14;
+      };
     };
   };
   home.packages = with pkgs; [
