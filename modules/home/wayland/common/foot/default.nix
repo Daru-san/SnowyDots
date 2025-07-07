813f13aa0185e72b00a54f447ddff07ea983692d
Daru <mica.tui@proton.me>
2025-07-07 16:47:26 +0200

    programs/foot: update configuration

--- modules/home/wayland/common/foot/default.nix
+++ modules/home/wayland/common/foot/default.nix
@@ -1,5 +1,25 @@
 {
-    programs.foot = {
-        server.enable = true;
+  programs.foot = {
+    server.enable = true;
+    settings = {
+      app-id = "footerm";
+      mouse = {
+        hide-when-typing = true;
+      };
+      cursor = {
+        style = "block";
+        blink = true;
+      };
+      scrollback = {
+        lines = 100 * 10000;
+      };
+      bell = {
+        system = true;
+        notify = true;
+      };
+      security = {
+        osc52 = "enabled";
+      };
     };
+  };
 }
