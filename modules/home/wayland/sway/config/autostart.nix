f02b904317ae29855b4c3b64008830d60f0bfc92
Daru <mica.tui@proton.me>
2025-09-16 20:00:38 +0200

    refactor: update sway autostarts

--- modules/home/wayland/sway/config/autostart.nix
+++ modules/home/wayland/sway/config/autostart.nix
@@ -5,9 +5,8 @@
 }:
 {
   wayland.windowManager.sway.config.startup = [
-    { command = lib.getExe pkgs.ghostty + " --title=btop -e " + lib.getExe pkgs.btop; }
+    { command = lib.getExe pkgs.foot + " -e " + lib.getExe pkgs.btop; }
     { command = lib.getExe pkgs.copyq; }
-    { command = lib.getExe pkgs.flameshot; }
-    { command = lib.getExe pkgs.stable.planify + " -b"; }
+    { command = lib.getExe pkgs.planify + " -b"; }
   ];
 }
