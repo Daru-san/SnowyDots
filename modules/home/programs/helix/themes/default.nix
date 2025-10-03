033bd419e49877325e0c97003375c38481ba7c6c
Daru <mica.tui@proton.me>
2025-10-03 18:58:11 +0200

    programs/helix: read theme files

--- modules/home/programs/helix/themes/default.nix
+++ modules/home/programs/helix/themes/default.nix
@@ -3,12 +3,12 @@ let
   themes = import ./npins/default.nix;
 in
 {
-  aegir = builtins.toPath "${themes.aegir.outPath}/themes/aegir";
-  aegir_transparent = builtins.toPath "${themes.aegir.outPath}/themes/aegir_transparent.toml";
-  darkvoid =  builtins.toPath "${themes.darkvoid.outPath}/darkvoid.toml";
-  grayscale = builtins.toPath "${themes.grayscale.outPath}/themes/grayscale.toml";
-  oxocarbon =  builtins.toPath "${themes.oxocarbon.outPath}/oxocarbon.toml";
-  rose_pine =  builtins.toPath  "${themes.rose-pine.outPath}/rose_pine.toml";
-  rose_pine_dawm = builtins.toPath "${themes.rose-pine.outPath}/rose_pine_dawn.toml";
-  rose_pine_moon = builtins.toPath "${themes.rose-pine.outPath}/rose_pine_moon.toml";
+  aegir = builtins.readFile "${themes.aegir.outPath}/themes/aegir";
+  aegir_transparent = builtins.readFile "${themes.aegir.outPath}/themes/aegir_transparent.toml";
+  darkvoid = builtins.readFile "${themes.darkvoid.outPath}/darkvoid.toml";
+  grayscale = builtins.readFile "${themes.grayscale.outPath}/themes/grayscale.toml";
+  oxocarbon = builtins.readFile "${themes.oxocarbon.outPath}/oxocarbon.toml";
+  rose_pine = builtins.readFile "${themes.rose-pine.outPath}/rose_pine.toml";
+  rose_pine_dawm = builtins.readFile "${themes.rose-pine.outPath}/rose_pine_dawn.toml";
+  rose_pine_moon = builtins.readFile "${themes.rose-pine.outPath}/rose_pine_moon.toml";
 }
