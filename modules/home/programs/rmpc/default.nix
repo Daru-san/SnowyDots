82d26f0d72a92ce4a6135705f04ab3ac91d55063
Daru <mica.tui@proton.me>
2025-10-03 18:58:27 +0200

    programs/rmpc: fix config file import

--- modules/home/programs/rmpc/default.nix
+++ modules/home/programs/rmpc/default.nix
@@ -2,10 +2,13 @@
 {
   programs.rmpc = {
     enable = true;
-    config = pkgs.runCommand "config" { } ''
-      substituteInPlace ${./config.ron} $out \
-        --replace-fail "MPD_ADDR" ${config.services.mpd.network.listenAddress} \
-        --replace-fail "MPD_PORT" ${toString config.services.mpd.network.port}
-    '';
+    config = builtins.readFile (
+      pkgs.runCommand "config" { } ''
+        cat ${./config.ron} > $out
+        substituteInPlace $out \
+          --replace-fail "MPD_ADDR" ${config.services.mpd.network.listenAddress} \
+          --replace-fail "MPD_PORT" ${toString config.services.mpd.network.port}
+      ''
+    );
   };
 }
