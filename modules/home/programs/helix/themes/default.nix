254ce13f18ec8b5bdc4709c93d0af663e92da3c3
Daru <mica.tui@proton.me>
2025-10-03 18:56:05 +0200

    programs/helix: import themes as paths

--- modules/home/programs/helix/themes/default.nix
+++ modules/home/programs/helix/themes/default.nix
@@ -3,12 +3,12 @@ let
   themes = import ./npins/default.nix;
 in
 {
-  aegir = "${themes.aegir.outPath}/themes/aegir";
-  aegir_transparent = "${themes.aegir.outPath}/themes/aegir_transparent.toml";
-  darkvoid = "${themes.darkvoid.outPath}/darkvoid.toml";
-  grayscale = "${themes.grayscale.outPath}/themes/grayscale.toml";
-  oxocarbon = "${themes.oxocarbon.outPath}/oxocarbon.toml";
-  rose_pine = "${themes.rose-pine.outPath}/rose_pine.toml";
-  rose_pine_dawm = "${themes.rose-pine.outPath}/rose_pine_dawn.toml";
-  rose_pine_moon = "${themes.rose-pine.outPath}/rose_pine_moon.toml";
+  aegir = builtins.toPath "${themes.aegir.outPath}/themes/aegir";
+  aegir_transparent = builtins.toPath "${themes.aegir.outPath}/themes/aegir_transparent.toml";
+  darkvoid =  builtins.toPath "${themes.darkvoid.outPath}/darkvoid.toml";
+  grayscale = builtins.toPath "${themes.grayscale.outPath}/themes/grayscale.toml";
+  oxocarbon =  builtins.toPath "${themes.oxocarbon.outPath}/oxocarbon.toml";
+  rose_pine =  builtins.toPath  "${themes.rose-pine.outPath}/rose_pine.toml";
+  rose_pine_dawm = builtins.toPath "${themes.rose-pine.outPath}/rose_pine_dawn.toml";
+  rose_pine_moon = builtins.toPath "${themes.rose-pine.outPath}/rose_pine_moon.toml";
 }
