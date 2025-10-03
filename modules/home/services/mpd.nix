123c0656f892919d0f48593efaa79114cc31c3bc
Daru <mica.tui@proton.me>
2025-10-03 15:17:22 +0200

    programs/rmpc: add cava support

--- modules/home/services/mpd.nix
+++ modules/home/services/mpd.nix
@@ -15,6 +15,13 @@
               type    "pipewire"
               name    "PipeWire Sound Server"
           }
+
+          audio_output {
+            type "fifo"
+            name "rmpc-fifo"
+            path "/tmp/mpd.fifo"
+            format "44100:16:2"
+          }
         '';
     };
     mpd-mpris = {
