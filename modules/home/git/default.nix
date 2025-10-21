becaa81fe9aeef44f3e2beccdedfeb261d26d1ec
Daru <mica.tui@proton.me>
2025-10-21 13:54:19 +0200

    home/git: fix config

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -3,43 +3,48 @@
     ./lazygit.nix
     ./gh.nix
   ];
-  programs.git-cliff = {
-    enable = true;
-  };
-  programs.git = {
-    enable = true;
-    userName = "Daru";
-    userEmail = "mica.tui@proton.me";
-
-    signing.format = "ssh";
-
-    extraConfig = {
-      core = {
-        editor = "vi";
-        fileMode = false;
-        autocrlf = false;
-      };
+  programs = {
+    git-cliff = {
+      enable = true;
     };
-    # Git highlighting
-    diff-so-fancy.enable = true;
+    diff-so-fancy = {
+      enable = true;
+      enableGitIntegration = true;
+    };
+    git = {
+      enable = true;
+      signing.format = "ssh";
+      settings = {
+        user = {
+          name = "Daru";
+          email = "mica.tui@proton.me";
+        };
 
-    # Git aliases
-    aliases = {
-      ci = "commit";
-      co = "checkout";
-      cl = "clone";
-      st = "status";
-      ad = "add";
-      ps = "push";
-      pl = "pull";
-      re = "remote";
-      br = "branch";
-      fe = "fetch";
-      cp = "cherry-pick";
-      rs = "reset";
-      df = "diff";
-      "in" = "init";
-      sw = "switch";
+        core = {
+          editor = "vi";
+          fileMode = false;
+          autocrlf = false;
+        };
+
+        # Git aliases
+        aliases = {
+          ci = "commit";
+          co = "checkout";
+          cl = "clone";
+          st = "status";
+          ad = "add";
+          ps = "push";
+          pl = "pull";
+          re = "remote";
+          br = "branch";
+          fe = "fetch";
+          cp = "cherry-pick";
+          rs = "reset";
+          df = "diff";
+          "in" = "init";
+          sw = "switch";
+        };
+      };
     };
   };
 }
