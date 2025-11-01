3d19280fb91aa1b1abb306b3fb9d68fd74953b8d
Daru <mica.tui@proton.me>
2025-11-01 14:36:52 +0200

    system/input: update fcitx5 packages

--- modules/nixos/system/input.nix
+++ modules/nixos/system/input.nix
@@ -7,7 +7,9 @@
       fcitx5 = {
         addons = with pkgs; [
           fcitx5-mozc
-          fcitx5-gtk
+          qt6Packages.fcitx5-configtool
+          fcitx5-material-color
+          qt6Packages.fcitx5-unikey
         ];
         waylandFrontend = true;
       };
