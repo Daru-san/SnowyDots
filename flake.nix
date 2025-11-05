b8ed5fad7538d950bf1c108fc7949e0eed5eeb16
Daru <mica.tui@proton.me>
2025-11-05 18:31:04 +0200

    flake: switch zen-browser flake

--- flake.nix
+++ flake.nix
@@ -51,13 +51,8 @@
 
     # Zen browser
     zen-browser = {
-      url = "github:youwen5/zen-browser-flake";
+      url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    snow-zen = {
-      url = "git+https://codeberg.org/Daru-san/snow-zen";
-      inputs.zen-browser.follows = "zen-browser";
     };
 
     zen-findbar = {
