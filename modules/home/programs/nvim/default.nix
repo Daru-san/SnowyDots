7a9f05a5863e63f02ba3b7a647f9bd5833fd8801
Daru <mica.tui@proton.me>
2025-11-05 18:30:54 +0200

    programs/nvim: use extended nixvim configuration

--- modules/home/programs/nvim/default.nix
+++ modules/home/programs/nvim/default.nix
@@ -1,50 +1,21 @@
 {
   inputs,
   config,
-  pkgs,
-  system,
   ...
 }:
 {
-  env = {
-    editor = {
-      extraPackages = [ pkgs.neovide ];
-      defaultEditor = false;
-      package = inputs.vim.packages.${system}.default.extend {
-        nixpkgs.overlays = [ inputs.snowpkgs.overlays.default ];
-        dependencies = {
-          yazi.package = pkgs.yazi;
-          lazygit.package = pkgs.lazygit;
-          go.package = pkgs.go;
-          rust-analyzer.package = pkgs.rust-analyzer;
-          direnv.package = pkgs.direnv;
-          git.package = pkgs.go;
-          ripgrep.package = pkgs.ripgrep;
-          gcc.package = pkgs.gcc;
-        };
-        extraConfigLua = ''
-          if vim.g.neovide then
-            vim.keymap.set('n', '<D-s>', ':w<CR>')
-            vim.keymap.set('v', '<D-c>', '"+y')
-            vim.keymap.set('n', '<D-v>', '"+P')
-            vim.keymap.set('v', '<D-v>', '"+P')
-            vim.keymap.set('c', '<D-v>', '<C-R>+')
-            vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli')
-            vim.g.neovide_position_animation_length = 0
-            vim.g.neovide_cursor_animation_length = 0.00
-            vim.g.neovide_cursor_trail_size = 0
-            vim.g.neovide_cursor_animate_in_insert_mode = false
-            vim.g.neovide_cursor_animate_command_line = false
-            vim.g.neovide_scroll_animation_far_lines = 0
-            vim.g.neovide_scroll_animation_length = 0.00
-
-            vim.g.neovide_cursor_trail_size = 0
-            vim.g.neovide_cursor_animate_in_insert_mode = false
-            vim.g.neovide_cursor_animate_command_line = false
-            vim.o.guifont = "${config.stylix.fonts.monospace.name}:${toString config.stylix.fonts.sizes.terminal}"
-          end
-        '';
-      };
-    };
+  imports = [
+    inputs.vim.homeModules.default
+  ];
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    luaLoader.enable = true;
+    imports = [
+      config.lib.stylix.nixvim.config
+      inputs.vim.nixvimModules.default
+    ];
   };
 }
