697cbbfaac1cdfff134c9dbc4bdf0136e12c1d97
Daru <mica.tui@proton.me>
2025-11-11 20:31:47 +0200

    programs/neovim: fleshify pascal configuration

--- modules/home/programs/nvim/default.nix
+++ modules/home/programs/nvim/default.nix
@@ -1,11 +1,14 @@
 {
   inputs,
   system,
+  pkgs,
+  lib,
   ...
 }:
 let
   pascal-tools = inputs.pascal-tools.packages.${system};
   pascal-lsp = inputs.pascal-lsp.packages.${system};
+  pascal-gtk4-bindings = inputs.pascal-gtk-bindings;
 in
 {
   imports = [
@@ -22,7 +25,52 @@ in
     ];
     lsp.servers.pasls = {
       enable = true;
-      package = pascal-lsp.pasls;
+      package = pkgs.symlinkJoin {
+        pname = "${pascal-lsp.pasls.pname}-env";
+        version = lib.getVersion pascal-lsp.pasls;
+        paths = [ pascal-lsp.pasls ];
+        preferLocalBuild = true;
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/pasls \
+            --set PP ${pkgs.fpc}/bin/fpc \
+            --set LAZARUSDIR ${pkgs.lazarus-qt6}/share/lazarus \
+            --set FPCDIR ${pkgs.lazarus-qt6}/share/fpcsrc \
+            --prefix PATH ${
+              lib.strings.makeBinPath (
+                with pkgs;
+                [
+                  fpc
+                  lazarus-qt6
+                ]
+              )
+            }
+        '';
+      };
+      config = {
+        cmd = [ "pasls" ];
+        filetypes = [
+          "pascal"
+          "fpc"
+          ".lpi"
+          ".lpr"
+        ];
+        settings = {
+          fpcOptions = [
+            "-Fu${pascal-gtk4-bindings}/bindings"
+            "-FU${pascal-gtk4-bindings}/bindings"
+          ];
+          insertCompletionsAsSnippets = true;
+          insertCompletionProcedureBrackets = true;
+          includeWorkspaceFoldersAsUnitPaths = true;
+          includeWorkspaceFoldersAsIncludePaths = true;
+          checkSyntax = true;
+          publishDiagnostics = true;
+          workspaceSymbols = true;
+          documentSymbols = true;
+          showSyntaxErrors = false;
+        };
+      };
     };
     extraPackages = [
       pascal-tools.pasfmt
