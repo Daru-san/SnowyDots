11baab65fa5395f9f29b097ab958350070cb7c61
Daru <mica.tui@proton.me>
2025-11-19 17:00:41 +0200

    home/theme/gtk: use WhiteSur-Dark-grey

--- home/daru/theme/gtk.nix
+++ home/daru/theme/gtk.nix
@@ -31,7 +31,7 @@ rec {
     enable = true;
 
     theme = {
-      name = "WhiteSur-Dark-alt-grey";
+      name = "WhiteSur-Dark-grey";
       package = pkgs.whitesur-gtk-theme.override {
         colorVariants = [ "dark" ];
         themeVariants = [ "grey" ];
