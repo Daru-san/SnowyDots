711e813ef7bfb94da6ccf1f00563ac34ee5b02f6
Daru <mica.tui@proton.me>
2025-11-20 14:19:31 +0200

    system/env: implement fix for dolphin

--- modules/nixos/tools/env.nix
+++ modules/nixos/tools/env.nix
@@ -6,5 +6,7 @@
       libheif.out
     ];
     pathsToLink = [ "share/thumbnailers" ];
+    etc."xdg/menus/applications.menu".source =
+      "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
   };
 }
