ed3e9b26d4e9bc320cdccc980c6906c12487c8ce
Daru <mica.tui@proton.me>
2025-11-20 17:35:34 +0200

    wayland/niri: use kde file picker

--- modules/home/xdg/portal.nix
+++ modules/home/xdg/portal.nix
@@ -4,6 +4,7 @@
   extraPortals = with pkgs; [
     xdg-desktop-portal-gtk
     xdg-desktop-portal-wlr
+    kdePackages.xdg-desktop-portal-kde
   ];
   config = {
     sway = {
@@ -11,6 +12,16 @@
         "gtk"
         "wlr"
       ];
+    };
+    niri = {
+      default = [
+        "gnome"
+        "gtk"
+      ];
+      "org.freedesktop.impl.portal.FileChooser" = "kde";
+      "org.freedesktop.impl.portal.Access" = "gtk";
+      "org.freedesktop.impl.portal.Notification" = "gtk";
+      "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
     };
     hyprland = {
       default = [
