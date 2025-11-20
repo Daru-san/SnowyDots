2f1debefb72324eb45ec556faa3fc4ac4ee7a0ca
Daru <mica.tui@proton.me>
2025-11-20 12:03:53 +0200

    wayland/niri: support f-key workspace navigation

--- modules/home/wayland/niri/config/binds.nix
+++ modules/home/wayland/niri/config/binds.nix
@@ -104,40 +104,33 @@
 
           "Mod+v".action.toggle-window-floating = [ ];
         }
-        {
-          "Mod+1".action.focus-workspace = 1;
-          "Mod+2".action.focus-workspace = 2;
-          "Mod+3".action.focus-workspace = 3;
-          "Mod+4".action.focus-workspace = 4;
-          "Mod+5".action.focus-workspace = 5;
-          "Mod+6".action.focus-workspace = 6;
-          "Mod+7".action.focus-workspace = 7;
-          "Mod+8".action.focus-workspace = 8;
-          "Mod+9".action.focus-workspace = 9;
-          "Mod+0".action.focus-workspace = 10;
-
-          "Mod+Ctrl+1".action.move-column-to-workspace = 1;
-          "Mod+Ctrl+2".action.move-column-to-workspace = 2;
-          "Mod+Ctrl+3".action.move-column-to-workspace = 3;
-          "Mod+Ctrl+4".action.move-column-to-workspace = 4;
-          "Mod+Ctrl+5".action.move-column-to-workspace = 5;
-          "Mod+Ctrl+6".action.move-column-to-workspace = 6;
-          "Mod+Ctrl+7".action.move-column-to-workspace = 7;
-          "Mod+Ctrl+8".action.move-column-to-workspace = 8;
-          "Mod+Ctrl+9".action.move-column-to-workspace = 8;
-          "Mod+Ctrl+0".action.move-column-to-workspace = 10;
-
-          "Mod+Shift+1".action.move-window-to-workspace = 1;
-          "Mod+Shift+2".action.move-window-to-workspace = 2;
-          "Mod+Shift+3".action.move-window-to-workspace = 3;
-          "Mod+Shift+4".action.move-window-to-workspace = 4;
-          "Mod+Shift+5".action.move-window-to-workspace = 5;
-          "Mod+Shift+6".action.move-window-to-workspace = 6;
-          "Mod+Shift+7".action.move-window-to-workspace = 7;
-          "Mod+Shift+8".action.move-window-to-workspace = 8;
-          "Mod+Shift+9".action.move-window-to-workspace = 8;
-          "Mod+Shift+0".action.move-window-to-workspace = 10;
-        }
+        (
+          let
+            keys = lib.range 0 9;
+            mkBinds =
+              keys:
+              map (
+                key:
+                (
+                  let
+                    key_str = builtins.toString key;
+                    fkey_str = if key == 0 then "F10" else "F${key_str}";
+                    ws = if key == 0 then 10 else key;
+                    fws = if key == 0 then 20 else key + 10;
+                  in
+                  {
+                    "Mod+${key_str}".action.focus-workspace = ws;
+                    "Mod+Ctrl+${key_str}".action.move-column-to-workspace = ws;
+                    "Mod+Shift+${key_str}".action.move-window-to-workspace = ws;
+                    "Mod+${fkey_str}".action.focus-workspace = fws;
+                    "Mod+Ctrl+${fkey_str}".action.move-column-to-workspace = fws;
+                    "Mod+Shift+${fkey_str}".action.move-window-to-workspace = fws;
+                  }
+                )
+              ) keys;
+          in
+          lib.mergeAttrsList (mkBinds keys)
+        )
         (
           let
             terminal = getExe config.programs.foot.package;
