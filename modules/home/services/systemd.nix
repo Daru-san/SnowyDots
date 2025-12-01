ef2df19eca670e37fe96e31677d3bc0a6728ffe3
Daru <mica.tui@proton.me>
2025-12-01 22:24:34 +0200

    home/systemd: disable rqbit server

--- modules/home/services/systemd.nix
+++ modules/home/services/systemd.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   inputs,
   system,
   ...
@@ -9,14 +8,6 @@
     seanime-server = {
       Service = {
         ExecStart = "${inputs.seanime.packages.${system}.seanime}/bin/seanime";
-      };
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
-    };
-    rqbit-server = {
-      Service = {
-        ExecStart = "${pkgs.rqbit}/bin/rqbit server start Downloads/Torrents";
       };
       Install = {
         WantedBy = [ "default.target" ];
