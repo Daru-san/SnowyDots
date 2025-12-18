b93528ff19aeda2aaa3244926a5d2836e21488eb
Daru <mica.tui@proton.me>
2025-12-18 22:56:38 +0200

    refactor: configure scarlet popups with home-manager

--- modules/home/wayland/common/scarlet/default.nix
+++ modules/home/wayland/common/scarlet/default.nix
@@ -8,8 +8,37 @@
     source = ./styles;
     recursive = true;
   };
+
   programs.scarlet = {
-    config = ./config.kdl;
+    extraConfig = ./config.kdl;
+    settings = {
+      popups = {
+        notifications = {
+          enable = true;
+          timeout = 3000;
+          anchors = [ "top" ];
+          layer = "overlay";
+        };
+
+        osd = {
+          enable = true;
+          timeout = 2000;
+
+          layer = "overlay";
+
+          orientation = "horizontal";
+
+          margins = {
+            bottom = 80;
+            top = 10;
+            left = 12;
+            right = 13;
+          };
+
+          anchors = [ "bottom" ];
+        };
+      };
+    };
     systemd = {
       enable = true;
       styles-watch-directory = "${config.xdg.configHome}/scarlet";
