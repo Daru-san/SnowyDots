0e84040c772c25ddabf9d62cffdc6c8ff75377be
Daru <mica.tui@proton.me>
2025-12-25 19:24:06 +0200

    wayland/niri: remove custom monitor mode

--- modules/home/wayland/niri/config/monitors.nix
+++ modules/home/wayland/niri/config/monitors.nix
@@ -4,7 +4,6 @@
       "HDMI-A-2" = {
         variable-refresh-rate = false;
         mode = {
-          custom = true;
           height = 1080;
           width = 1920;
           refresh = 70.0;
