5af42b5fe8aa0472bf75f2bb9e1b447713d45c42
Daru <mica.tui@proton.me>
2026-01-06 02:07:32 +0200

    wayland/scarlet: update configuration

--- modules/home/wayland/common/scarlet/default.nix
+++ modules/home/wayland/common/scarlet/default.nix
@@ -4,17 +4,17 @@
     inputs.scarlet.homeModules.default
   ];
 
-  xdg.configFile."scarlet" = {
-    source = ./styles;
-    recursive = true;
-  };
+  # xdg.configFile."scarlet" = {
+  #   source = ./styles;
+  #   recursive = true;
+  # };
 
   programs.scarlet = {
     extraConfig = ./config.kdl;
     settings = import ./settings.nix;
     systemd = {
       enable = true;
-      styles-watch-directory = "${config.xdg.configHome}/scarlet";
+      # styles-watch-directory = "${config.xdg.configHome}/scarlet";
     };
   };
 }
